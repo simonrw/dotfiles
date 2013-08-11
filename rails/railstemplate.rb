3066d08be8f56ef1672982e3f3b81d84b5e1d534
Simon Walker <s.r.walker101@googlemail.com>
2013-08-11 10:58:56 +0100

    Adds some more rails initialisation
    Taken from
    https://github.com/dennybritz/rails_startup_template/blob/master/template.rb

--- rails/railstemplate.rb
+++ rails/railstemplate.rb
@@ -1,16 +1,42 @@
 gem_group :development, :test do
+    # Nice repl
     gem 'pry'
     gem 'pry-rails'
+
+    # Better testing dsl
     gem 'rspec'
     gem 'rspec-rails'
+
+    # Automated tasks
     gem 'guard'
     gem 'guard-rspec'
+
+    # Integration tests with a real headless browser
     gem 'capybara'
 end
 
 gem_group :development do
+    # Nice debugging information on request timings
     gem 'rack-mini-profiler'
 end
 
+# For encrypted passwords
+gem 'bcrypt-ruby'
+
 # Remove the secret token from the tracking
 run "echo 'config/initializers/secret_token.rb' >> .gitignore"
+
+# Set up foreman
+run "echo 'web: bundle exec rails s -p $PORT' >> Procfile"
+run "echo PORT=3000 >> .env"
+run "echo '.env' >> .gitignore"
+# disable output buffering
+run "echo 'STDOUT.sync = true' >> config/environments/development.rb"
+
+# Set up guard
+run "bundle exec guard init rspec"
+
+# Initialize git
+git :init
+git add: '.'
+git commit: %Q{ -m 'Initial commit' }
