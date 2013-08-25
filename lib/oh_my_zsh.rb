71982d501fafa5a39272aead631ac0aa9caa43d1
Simon Walker <s.r.walker101@googlemail.com>
2013-08-25 20:10:19 +0100

    Adds install plugin

---
+++ lib/oh_my_zsh.rb
@@ -0,0 +1,48 @@
+class OhMyZSH
+    def initialize(options = {})
+        @location = options['location'] || File.join(ENV['HOME'], '.oh-my-zsh')
+        @file_directory = options['directory'] || File.join(File.dirname(__FILE__), '..')
+    end
+
+    def install
+        (exists? || fetch) && update_custom
+    end
+
+    def uninstall
+        exists? && remove
+    end
+
+    private
+
+    def exists?
+        File.exist? @location
+    end
+
+    def fetch
+        system %Q{git clone https://github.com/robbyrussell/oh-my-zsh.git #{@location}}
+    end
+
+    def remove
+        system %Q{rm -rf "#{@location}"}
+    end
+
+    def source_location
+        File.expand_path(File.join(@file_directory, 'oh-my-zsh', 'custom'))
+    end
+
+    def custom_location
+        File.expand_path(File.join(@location, 'custom'))
+    end
+
+    def update_custom
+        remove_custom && link_custom
+    end
+
+    def remove_custom
+        system %Q{rm -rf "#{custom_location}"}
+    end
+
+    def link_custom
+        system %Q{ln -sv #{source_location} #{custom_location}}
+    end
+end
