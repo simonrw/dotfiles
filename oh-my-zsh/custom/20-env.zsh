74dfa6047bdd00f2974c9355956ed277b0072a89
Simon Walker <s.r.walker101@googlemail.com>
2013-08-26 14:48:09 +0100

    Setting visual to EDITOR

--- oh-my-zsh/custom/20-env.zsh
+++ oh-my-zsh/custom/20-env.zsh
@@ -1,6 +1,6 @@
 # vim: ft=zsh
 export PATH=${HOME}/.bin:${HOME}/prefix/bin:$PATH
 export EDITOR=vim_nox
-export VISUAL=gvim_fg
+export VISUAL=${EDITOR}
 export BIBINPUTS=${HOME}/work/central-bibliography:${BIBINPUTS}
 export TERM=screen-256color
