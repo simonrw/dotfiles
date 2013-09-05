ac82d0b47b7d2ea5226526ffbb036682af3da2d5
Simon Walker <s.r.walker101@googlemail.com>
2013-09-05 16:16:26 +0100

    Adds homebrew sbin to mac path

--- oh-my-zsh/custom/29-env-macosx.zsh
+++ oh-my-zsh/custom/29-env-macosx.zsh
@@ -1,6 +1,6 @@
 # vim: ft=zsh
 case $OSTYPE in darwin*)
-    export PATH=${PATH}:/usr/texbin:/usr/local/share/npm/bin
+    export PATH=${PATH}:/usr/texbin:/usr/local/share/npm/bin:/usr/local/sbin
     if [[ -f /usr/local/opt/autoenv/activate.sh ]]; then
         source /usr/local/opt/autoenv/activate.sh
     fi
