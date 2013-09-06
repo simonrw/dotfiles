411600d711e0db91d09790a82d57e948c6d91050
Simon Walker <s.r.walker101@googlemail.com>
2013-09-06 22:18:44 +0100

    Removes autoenv

--- zsh/zsh/29-env-macosx.zsh
+++ zsh/zsh/29-env-macosx.zsh
@@ -1,7 +1,4 @@
 # vim: ft=zsh
 case $OSTYPE in darwin*)
     export PATH=${PATH}:/usr/texbin:/usr/local/share/npm/bin:/usr/local/sbin
-    if [[ -f /usr/local/opt/autoenv/activate.sh ]]; then
-        source /usr/local/opt/autoenv/activate.sh
-    fi
 ;; esac
