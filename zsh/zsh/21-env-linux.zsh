5ac9ae959362ef58548733690f4dfac67a51cddf
Simon Walker <s.r.walker101@googlemail.com>
2013-09-13 13:55:12 +0100

    Adds leicester setup to zsh init

--- zsh/zsh/21-env-linux.zsh
+++ zsh/zsh/21-env-linux.zsh
@@ -1,4 +1,9 @@
 # vim: ft=zsh
 case $OSTYPE in linux*)
     export LD_LIBRARY_PATH=${HOME}/prefix/lib:${LD_LIBRARY_PATH}
+
+    # If the server is at Leicester, source the intel compiler variables
+    if [[ `dnsdomainname` == "star.le.ac.uk" ]]; then
+        source /opt/intel/composerxe-2011.0.084/bin/compilervars.sh intel64 2>/dev/null
+    fi
 ;; esac
