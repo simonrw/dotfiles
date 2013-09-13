f8a71ac90dbfc781a5649231ede13561632fc49c
Simon Walker <s.r.walker101@googlemail.com>
2013-09-13 13:59:13 +0100

    Adds sge sourcing at leicester

--- zsh/zsh/21-env-linux.zsh
+++ zsh/zsh/21-env-linux.zsh
@@ -5,5 +5,6 @@ case $OSTYPE in linux*)
     # If the server is at Leicester, source the intel compiler variables
     if [[ `dnsdomainname` == "star.le.ac.uk" ]]; then
         source /opt/intel/composerxe-2011.0.084/bin/compilervars.sh intel64 2>/dev/null
+        source /usr/local/sge/default/common/settings.sh
     fi
 ;; esac
