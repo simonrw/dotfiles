455751b29ec932915d886376e2c82a2e78f5c303
Simon Walker <s.r.walker101@googlemail.com>
2013-09-17 23:50:42 +0100

    Adds mac autoenv startup

--- zsh/zsh/29-env-macosx.zsh
+++ zsh/zsh/29-env-macosx.zsh
@@ -1,4 +1,8 @@
 # vim: ft=zsh
 case $OSTYPE in darwin*)
     export PATH=${PATH}:/usr/texbin:/usr/local/share/npm/bin:/usr/local/sbin
+    local autoenv_file=/usr/local/opt/autoenv/activate.sh
+    if [ -f $autoenv_file ]; then
+        source $autoenv_file
+    fi
 ;; esac
