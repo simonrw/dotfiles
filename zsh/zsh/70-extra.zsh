0ad5f76cc14b2180758b4713aa06fb989db82736
Simon Walker <s.r.walker101@googlemail.com>
2013-09-22 00:12:22 +0100

    Removes marks as they were unused

--- zsh/zsh/70-extra.zsh
+++ zsh/zsh/70-extra.zsh
@@ -14,30 +14,6 @@ function gi() {
     curl ${URL}${ARGS}
 }
 
-# Enable marking of directories for quick jumping around
-# Source: http://jeroenjanssens.com/2013/08/16/quickly-navigate-your-filesystem-from-the-command-line.html
-export MARKPATH=$HOME/.marks
-function jump { 
-    cd -P $MARKPATH/$1 2>/dev/null || echo "No such mark: $1"
-}
-function mark { 
-    mkdir -p $MARKPATH; ln -s $(pwd) $MARKPATH/$1
-}
-function unmark { 
-    rm -i $MARKPATH/$1 
-}
-function marks {
-    \ls -l "$MARKPATH" | tail -n +2 | sed 's/  / /g' | cut -d' ' -f9- | awk -F ' -> ' '{printf "%-10s -> %s\n", $1, $2}'
-}
-
-function _completemarks {
-    reply=($(ls $MARKPATH))
-}
-compctl -K _completemarks jump
-compctl -K _completemarks unmark
-
-# Add an alias for the jump command
-alias j=jump
 
 # Source the custom zshrc.local file in the system
 if [[ -f ${HOME}/.zshrc.local ]]; then
