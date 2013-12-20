e6b4c7b3b15c9ddec636b2d0cf91a1e62d7d9186
Simon Walker <s.r.walker101@googlemail.com>
2013-12-20 18:09:33 +0000

    Adds auto module reloading by default

--- ipython/ipython/profile_default/ipython_config.py
+++ ipython/ipython/profile_default/ipython_config.py
@@ -24,7 +24,10 @@ c = get_config()
 # c.InteractiveShellApp.code_to_run = ''
 
 # lines of code to run at IPython startup.
-# c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines = []
+c.InteractiveShellApp.exec_lines.append('%load_ext autoreload')
+c.InteractiveShellApp.exec_lines.append('%autoreload 2')
+
 
 # Enable GUI event loop integration ('qt', 'wx', 'gtk', 'glut', 'pyglet',
 # 'osx').
