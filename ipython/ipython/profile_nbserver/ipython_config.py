93e06cc19f42e98a9c68c89657f7244883a4dae9
Simon Walker <s.r.walker101@googlemail.com>
2014-04-07 00:23:42 +0100

    disables auto pylab

--- ipython/ipython/profile_nbserver/ipython_config.py
+++ ipython/ipython/profile_nbserver/ipython_config.py
@@ -2,7 +2,7 @@
 
 c = get_config()
 
-c.IPKernelApp.pylab = 'inline'
+c.IPKernelApp.pylab = None # 'inline'
 c.NotebookApp.certfile = u'/storage/astro2/phsnag/ipython-notebook/mycert.pem'
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
