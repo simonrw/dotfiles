66613507411ff514a903e62e4401c3e695396ffa
Simon Walker <s.r.walker101@googlemail.com>
2014-04-26 19:08:36 +0100

    Removes scrollbar

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -31,7 +31,16 @@
 (setq multi-term-program "zsh")
 
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  )
