0a0e9005646b39b94f611dd9b9aaf756d69fa010
Simon Walker <s.r.walker101@googlemail.com>
2014-04-26 17:22:22 +0100

    Adds comments for emacs file

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -2,6 +2,7 @@
       '(clojure-mode
 	graphviz-dot-mode
 	multi-term
+	magit
 	))
 
 
@@ -12,11 +13,14 @@
 
 (package-initialize)
 
+; Automatically install packages that are missing
+; Note unwanted packages must be removed from ~/.emacs.d/elpa for the time being
 (unless package-archive-contents
   (package-refresh-contents))
 
 (dolist (package package-list)
   (unless (package-installed-p package)
-    (package-install package)))    
-			   
+    (package-install package)))
+
+; Set the colour scheme
 (load "~/.emacs.d/colourscheme/base16-default-theme.el" nil t)
