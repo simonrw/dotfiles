3ccda6f4978bd0f2f8173b6d7bc10f33967adf83
Simon Walker <s.r.walker101@googlemail.com>
2014-04-27 16:25:29 +0100

    Re-enabling menu bar in emacs

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -30,7 +30,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
