3241e63f2fccff3a87caae749af267f699367fe0
Simon Walker <s.r.walker101@googlemail.com>
2014-04-28 11:56:38 +0100

    Only showing menu bar on mac os x

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -16,46 +16,9 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-; Set the colour scheme
-(load "~/.emacs.d/colourscheme/base16-default-theme.el" nil t)
-
-; Configure some plugins
-(setq multi-term-program "zsh")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-; My custom settings
-(setq inhibit-startup-message t
-      inhibit-startup-echo-area-message 1) ; cleaner startup
-(define-key global-map (kbd "RET") 'newline-and-indent) ; autoindent by default
-(global-subword-mode t)
-
-
-(global-set-key (kbd "\C-x\C-z") 'magit-status)
-
-;; no tabs
-(setq-default indent-tabs-mode nil)
-(setq-default c-basic-offset 4)
-(setq-default default-tab-width 4)
-
-;; if running in terminal, disable the menu bar
-(when (equal window-system nil)
+(if
+    (equal window-system 'ns)
+    (menu-bar-mode 1)
   (menu-bar-mode 0))
 
 (global-set-key (kbd "C-o") 'other-window)
