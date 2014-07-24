b05e879791a17dd3cb3dce1fa5a22b8205dc7c08
Simon Walker <s.r.walker101@googlemail.com>
2014-07-24 22:44:14 +0100

    Starting emacs config again

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,80 +1,37 @@
-(setq package-list
-      '(multi-term
-        clojure-mode
-        python-mode
-        evil
-        cider
-        markdown-mode
-        graphviz-dot-mode
-        magit))
+;; Use meta key on linux
+(setq x-super-keysym 'meta)
 
-(setq package-archives
-      '(("elpa" . "http://tromey.com/elpa/")
-	("gnu" . "http://elpa.gnu.org/packages/")
-	("marmalade" . "http://marmalade-repo.org/packages/")))
-
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-
-(package-initialize)
-
-;; Disable bells completely
+;; Disable all bells
 (setq ring-bell-function 'ignore)
 
-; Automatically install packages that are missing
-; Note unwanted packages must be removed from ~/.emacs.d/elpa for the time being
-(unless package-archive-contents
-  (package-refresh-contents))
+;; Disable the startup pane
+(setq inhibit-startup-message t)
 
-(dolist (package package-list)
-  (unless (package-installed-p package)
-    (package-install package)))
+;; Add the melpa repositories
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives
+	       '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (package-initialize)
+  )
 
-; Configure some plugins
-(setq multi-term-program "zsh")
+;; add auto indentation
+(define-key global-map (kbd "RET") 'newline-and-indent)
 
-; Set ido mode
-(ido-mode)
-(ido-everywhere)
+;; Set the path from the shell
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(when window-system (set-exec-path-from-shell-PATH))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("38c4fb6c8b2625f6307f3dde763d5c61d774d854ecee9c5eb9c5433350bc0bef" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default)))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(visible-bell nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
-; My custom settings
-(setq inhibit-startup-message t
-      inhibit-startup-echo-area-message 1) ; cleaner startup
-(define-key global-map (kbd "RET") 'newline-and-indent) ; autoindent by default
-(global-subword-mode t)
-
-
-(global-set-key (kbd "\C-x\C-z") 'magit-status)
-
-;; no tabs
-(setq-default indent-tabs-mode nil)
-(setq-default c-basic-offset 4)
-(setq-default default-tab-width 4)
-
-;; if running in terminal, disable the menu bar
-(if (equal window-system 'ns)
-    (menu-bar-mode 1)
-  (menu-bar-mode 0))
-
-(global-set-key (kbd "C-o") 'other-window)
-;; Re-enable hash key on mac os x
-(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
-
-;; Use menlo font where possible
-(when (member "Menlo" (font-family-list))
-  (set-face-attribute 'default nil :font "Menlo"))
+;; List packages
+(defvar my-packages '(better-defaults
+		      clojure-mode
+		      clojure-test-mode
+                      evil
+		      cider
+                      multi-term))
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
