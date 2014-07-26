b1c2251f07175541f2b7e8d199b57990cff3d4a5
Simon Walker <s.r.walker101@googlemail.com>
2014-07-26 09:13:04 +0100

    Updates emacs config

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,3 +1,37 @@
+;; Add the melpa repositories
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives
+	       '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (package-initialize)
+
+  (unless package-archive-contents
+    (package-refresh-contents))
+
+  ;; List packages
+  (defvar my-packages '(better-defaults
+                        clojure-mode
+                        clojure-test-mode
+                        evil
+                        evil-leader
+                        cider
+                        projectile
+                        multi-term))
+
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p)))
+
+  )
+
+;; Always use utf8
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Use y or n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 ;; Use meta key on linux
 (setq x-super-keysym 'meta)
 
@@ -6,15 +40,6 @@
 
 ;; Disable the startup pane
 (setq inhibit-startup-message t)
-
-;; Add the melpa repositories
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list 'package-archives
-	       '("marmalade" . "http://marmalade-repo.org/packages/"))
-  (package-initialize)
-  (package-refresh-contents)
-  )
 
 ;; add auto indentation
 (define-key global-map (kbd "RET") 'newline-and-indent)
@@ -26,13 +51,14 @@
     (setq exec-path (split-string path-from-shell path-separator))))
 (when window-system (set-exec-path-from-shell-PATH))
 
-;; List packages
-(defvar my-packages '(better-defaults
-		      clojure-mode
-		      clojure-test-mode
-                      evil
-		      cider
-                      multi-term))
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(require 'projectile)
+(projectile-global-mode)
+
+(require 'evil)
+(evil-mode t)
+
+(require 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-leader ",")
+(evil-leader/set-key
+  "," 'projectile-find-file)
