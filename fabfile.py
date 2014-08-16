87f1c7372fc4f234937296283d1a53053c9d101f
Simon Walker <s.r.walker101@googlemail.com>
2014-08-16 13:29:30 +0100

    Removes fabfile
    i no longer really use this

--- fabfile.py
+++
@@ -1,8 +0,0 @@
-from fabric.api import run, env, cd, local
-
-env.use_ssh_config = True
-env.hosts = ['chain.astro', 'ngtshead.astro']
-
-def sync():
-    with cd("~/dotfiles"):
-        run('git sync')
