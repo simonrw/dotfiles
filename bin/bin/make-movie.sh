4ab0dd30d7cbe35eab06e2a1611cbe93c2f83612
Simon Walker <s.r.walker101@googlemail.com>
2014-10-09 16:51:57 +0100

    Getting cpu count from python

--- bin/bin/make-movie.sh
+++ bin/bin/make-movie.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+cpu_count() {
+    python -c 'import multiprocessing; print multiprocessing.cpu_count()'
+}
+
 main() {
-    mencoder 'mf://*.png' -mf w=800:h=600:fps=15:type=png -ovc x264 -x264encopts crf=18:nofast_pskip:nodct_decimate:nocabac:global_header:threads=4 -of lavf -lavfopts format=mp4 -o output.mp4
+    mencoder 'mf://*.png' -mf w=800:h=600:fps=15:type=png -ovc x264 -x264encopts crf=18:nofast_pskip:nodct_decimate:nocabac:global_header:threads=$(cpu_count) -of lavf -lavfopts format=mp4 -o output.mp4
 }
 
 main
