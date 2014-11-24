ebbcb3fa6c0efb1c637b661af3aad277cf77c60d
Simon Walker <s.r.walker101@googlemail.com>
2014-11-24 12:01:13 +0000

    Add quick eshell opening

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -64,6 +64,32 @@
 ;; ido mode
 (ido-mode)
 
+;; Quick eshell running
+(defun eshell-here ()
+  "Opens a new shell in the directory associated with the
+current buffer's file. The eshell is renamed to match that
+directory to make multiple eshell windows easier."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+(defun eshell/x ()
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+                 
+
 ;; Evil mode
 (require-package 'evil)
 (evil-mode t)
