2a4f24384a883a4f4f785986a7e348f5efee0540
Simon Walker <s.r.walker101@googlemail.com>
2014-11-27 11:59:30 +0000

    Set emacs path from shell

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -14,6 +14,15 @@
       (package-refresh-contents))
     (package-install package)))
 
+(require-package 'better-defaults)
+
+;; Fix the PATH variable
+(defun set-exec-path-from-shell-PATH ()
+  (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+(when window-system (set-exec-path-from-shell-PATH))
+
 ;; Themes
 (load-theme 'wombat t)
 
