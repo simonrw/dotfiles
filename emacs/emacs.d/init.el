fd02707389d811f16180167137456e1d6cf2f59e
Simon Walker <s.r.walker101@googlemail.com>
2014-11-27 11:59:22 +0000

    Update melpa location

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,7 +1,8 @@
+(require 'package)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
 			 ("org" . "http://orgmode.org/elpa/")
 			 ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("melpa" . "http://melpa.milkbox.net/packages/")))
+			 ("melpa" . "http://melpa.org/packages/")))
 
 (package-initialize)
 
