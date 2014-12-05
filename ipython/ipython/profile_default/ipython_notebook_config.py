48a7b2833f99a7d62a86293e5578d842abae0f7d
Simon Walker <s.r.walker101@googlemail.com>
2014-12-05 17:22:47 +0000

    Ipython notebook remove default styling and set png backend

--- ipython/ipython/profile_default/ipython_notebook_config.py
+++ ipython/ipython/profile_default/ipython_notebook_config.py
@@ -1,10 +1,4 @@
 c = get_config()
 
 # Subset of matplotlib rcParams that should be different for the inline backend.
-c.InlineBackend.rc = {'font.size': 10, 
-'figure.figsize': (10, 8), 
-'figure.facecolor': 'white', 
-'savefig.dpi': 72, 
-'figure.subplot.bottom': 0.125, 
-'figure.edgecolor': 'white'}
-c.InlineBackend.figure_format = 'svg'
+c.InlineBackend.rc = {'figure.figsize': (10, 8)}
