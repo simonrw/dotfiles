2da3f6a339c6bcdf37d0fe46561b3a1352c93f35
Simon Walker <s.r.walker101@googlemail.com>
2014-12-11 20:31:33 +0000

    Add comment enabling svg if needed in the future

--- ipython/ipython/profile_default/ipython_notebook_config.py
+++ ipython/ipython/profile_default/ipython_notebook_config.py
@@ -2,3 +2,4 @@ c = get_config()
 
 # Subset of matplotlib rcParams that should be different for the inline backend.
 c.InlineBackend.rc = {'figure.figsize': (10, 8)}
+# c.InlineBackend.figure_format = 'svg'
