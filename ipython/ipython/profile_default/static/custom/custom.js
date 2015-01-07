1af7e12173c33021efd1b8d674c7485b886c191a
Simon Walker <s.r.walker101@googlemail.com>
2015-01-07 14:51:44 +0000

    Hide main ipython notebook toolbar also

--- ipython/ipython/profile_default/static/custom/custom.js
+++ ipython/ipython/profile_default/static/custom/custom.js
@@ -1,5 +1,6 @@
 // Hide the header by default
 $([IPython.events]).on('notebook_loaded.Notebook', function(){
-    $('div#header').hide()
+    $('div#header').hide();
+    $('div#maintoolbar').hide();
     IPython.layout_manager.do_resize();
 });
