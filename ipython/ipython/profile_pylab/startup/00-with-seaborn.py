3b226cba678dde5a33e4c959e332228061b1e143
Simon Walker <s.r.walker101@googlemail.com>
2015-01-18 20:20:05 +0000

    Add seaborn startup to pylab profile

---
+++ ipython/ipython/profile_pylab/startup/00-with-seaborn.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+
+try:
+    print('Loading seaborn...', end=' ')
+    import seaborn as sns
+except ImportError:
+    print('Cannot load seaborn')
+else:
+    print('Done')
