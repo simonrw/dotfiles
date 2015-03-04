c363a035f5236318b25e6c5e0cba3356f32f7c19
Simon Walker <s.r.walker101@googlemail.com>
2015-03-04 17:48:24 +0000

    Add emacs setup code

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -12,53 +12,19 @@
 (when window-system (set-exec-path-from-shell-PATH))
 (require 'cl)
 
-;; Package management
-
-(load "package")
-(package-initialize)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(setq package-archive-enable-alist '(("melpa" deft magit)))
-
-;; Default packages
-(defvar srwalker101/packages
-  '(haskell-mode
-    smex
-    ir-black-theme
-    markdown-mode
-    auto-complete
-    evil
-    evil-leader
-    writeroom-mode
-    magit
-    auctex
-    writegood-mode
-    clojure-mode))
-
-;; Install default packages
-
-(defun srwalker101/packages-installed-p ()
-  (loop for pkg in srwalker101/packages
-	when (not (package-installed-p pkg)) do (return nil)
-	finally (return t)))
-
-(unless (srwalker101/packages-installed-p)
-  (message "%s" "Refreshing package database...")
-  (package-refresh-contents)
-  (dolist (pkg srwalker101/packages)
-    (when (not (package-installed-p pkg))
-      (package-install pkg))))
-
-;; Start up options
-
 ;; Font
 (set-face-attribute 'default nil
                     :family "Inconsolata"
                     :height 160
                     :weight 'normal
                     :width 'normal)
+
+;; Packages
+
+(defun load-config (file)
+  (load-file (expand-file-name file "~/.emacs.d/config")))
+
+(load-config "packages.el")
 
 ;; Splash screen
 
@@ -68,9 +34,9 @@
 ;; Scroll/tool/menu bars
 
 (when window-system
-    (scroll-bar-mode -1))
-(tool-bar-mode -1)
-(menu-bar-mode -1)
+  (scroll-bar-mode -1)
+  (tool-bar-mode -1)
+  (menu-bar-mode -1))
 
 ;; Marking text
 
@@ -109,17 +75,7 @@
 (global-set-key (kbd "C-c C-k") 'compile)
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; Evil
-
-; (require 'evil)
-; (evil-mode t)
-
-;; Evil leader
-
-; (require 'evil-leader)
-; (evil-leader/set-leader ",")
-
-;; Make the hash key work for OSX
+;; make the hash key work for OSX
 (if (eq system-type 'darwin)
     (global-set-key (kbd "M-3") '(lambda() (interactive) (insert "#"))))
 
@@ -130,42 +86,8 @@
       visible-bell t)
 (show-paren-mode t)
 
-;; Packages
-
-;; Smex
-
-(setq smex-save-file (expand-file-name ".smex-items" user-emacs-directory))
-(smex-initialize)
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-
-;; Ido
+; ;; Ido
 
 (ido-mode t)
 (setq ido-enable-flex-matching t
       ido-use-virtual-buffers t)
-
-;; Auto-complete
-
-(require 'auto-complete-config)
-(ac-config-default)
-
-;; AucTeX
-
-(setq-default TeX-master nil) ; Query for master file
-
-;; Language settings
-
-;; Markdown
-
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-(add-hook 'markdown-mode-hook
-	  (lambda ()
-	    (visual-line-mode t)
-	    (writegood-mode t)))
-(setq markdown-command "pandoc --smart --standalone -f markdown -t html")
-
-;; Theme
-
-(load-theme 'wombat t)
