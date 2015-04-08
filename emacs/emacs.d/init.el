0426dcddb0a1d9607940fea10197989ddd24deb6
Simon Walker <s.r.walker101@googlemail.com>
2015-04-08 10:46:40 +0100

    Simplify emacs

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,29 +1,41 @@
-;; User details
-(setq user-full-name "Simon Walker")
-(setq user-email-address "s.r.walker101@googlemail.com")
-
-;; Environment
-
-;; Fix the PATH variable
-(defun set-exec-path-from-shell-PATH ()
-  (let ((path-from-shell (shell-command-to-string "TERM=vt100 $SHELL -i -c 'echo $PATH'")))
-    (setenv "PATH" path-from-shell)
-    (setq exec-path (split-string path-from-shell path-separator))))
-(when window-system (set-exec-path-from-shell-PATH))
-(require 'cl)
-
 ;; Packages
 
-(defun load-config (file)
-  (load-file (expand-file-name file "~/.emacs.d/config")))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries
+  (add-to-list 'package-archives
+               '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize)
 
-(load-config "keys.el")
-(load-config "visual.el")
-(load-config "misc.el")
-(load-config "packages.el")
+;; Install packages
+(defvar srw/my-packages '(better-defaults
+                          paredit
+                          magit
+                          smex
+                          ido-ubiquitous
+                          idle-highlight-mode
+                          find-file-in-project
+                          multi-term
+                          monokai-theme))
 
-; ;; Ido
+(dolist (p srw/my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
 
-(ido-mode t)
-(setq ido-enable-flex-matching t
-      ido-use-virtual-buffers t)
+;; Theming
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+
+ '(custom-enabled-themes (quote (monokai)))
+ '(custom-safe-themes (quote ("a041a61c0387c57bb65150f002862ebcfe41135a3e3425268de24200b82d6ec9" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#272822" :foreground "#F8F8F2" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "apple" :family "Menlo")))))
