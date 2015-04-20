c1e55eaba5f999c0f64d539f6dbf8145edb92efb
Simon Walker <s.r.walker101@googlemail.com>
2015-04-20 11:48:40 +0100

    Verbose logging

--- bin/bin/ngts/info-from-imageid.py
+++ bin/bin/ngts/info-from-imageid.py
@@ -7,14 +7,17 @@ import logging
 import pymysql
 
 logging.basicConfig(
-    level='DEBUG', format='%(levelname)7s %(message)s')
+    level='INFO', format='%(levelname)7s %(message)s')
 logger = logging.getLogger(__name__)
 
 
 def main(args):
+    if args.verbose:
+        logger.setLevel('DEBUG')
     logger.debug(args)
     with pymysql.connect(user=args.user, host=args.host, db='ngts_ops',
             cursorclass=pymysql.cursors.DictCursor) as cursor:
+        logger.debug('Database connection open')
         nrows = cursor.execute('select * from raw_image_list where image_id = %s',
                 (args.imageid, ))
         if nrows != 1:
@@ -23,7 +26,7 @@ def main(args):
         result = cursor.fetchone()
 
     for key in args.keys:
-        print('{key:>20s} {value}'.format( key=key, value=result[key]))
+        print('{key:} {value}'.format( key=key, value=result[key]))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,4 +34,5 @@ if __name__ == '__main__':
     parser.add_argument('-u', '--user', required=False, default='sw')
     parser.add_argument('imageid')
     parser.add_argument('-k', '--keys', help='Keys to return', nargs='+', required=True)
+    parser.add_argument('-v', '--verbose', action='store_true')
     main(parser.parse_args())
