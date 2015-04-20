a109be05f032eeb4197b2102a6de99d5add5381c
Simon Walker <s.r.walker101@googlemail.com>
2015-04-20 11:46:16 +0100

    Printing image info

--- bin/bin/ngts/info-from-imageid.py
+++ bin/bin/ngts/info-from-imageid.py
@@ -20,10 +20,15 @@ def main(args):
         if nrows != 1:
             raise RuntimeError('Number of rows found: {nrows}, should be 1'.format(
                 nrows=nrows))
+        result = cursor.fetchone()
+
+    for key in args.keys:
+        print('{key:>20s} {value}'.format( key=key, value=result[key]))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-H', '--host', required=False, default='ngtsdb')
     parser.add_argument('-u', '--user', required=False, default='sw')
     parser.add_argument('imageid')
+    parser.add_argument('-k', '--keys', help='Keys to return', nargs='+', required=True)
     main(parser.parse_args())
