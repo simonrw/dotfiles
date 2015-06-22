ed5785516c8fd77721a8bdba3acbcc2ea0a982de
Simon Walker <s.r.walker101@googlemail.com>
2015-06-22 10:11:39 +0100

    Hide ipython notebook toolbar

--- ipython/ipython/profile_default/static/custom/custom.js
+++ ipython/ipython/profile_default/static/custom/custom.js
@@ -2,6 +2,9 @@ require(['base/js/namespace', 'base/js/events'], function(IPython, events) {
   events.on('app_initialized.NotebookApp', function() {
     // Do not autoclose brackets
     IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
+
+    // Hide the toolbar
+    $('div#maintoolbar').hide();
   });
 });
 
