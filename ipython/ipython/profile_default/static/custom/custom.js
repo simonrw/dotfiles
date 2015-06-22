1905ebf5564773b99a7fab462a5c86d9b514bd9c
Simon Walker <s.r.walker101@googlemail.com>
2015-06-22 10:11:31 +0100

    Add comment for autoclose brackets

--- ipython/ipython/profile_default/static/custom/custom.js
+++ ipython/ipython/profile_default/static/custom/custom.js
@@ -1,5 +1,6 @@
 require(['base/js/namespace', 'base/js/events'], function(IPython, events) {
   events.on('app_initialized.NotebookApp', function() {
+    // Do not autoclose brackets
     IPython.CodeCell.options_default.cm_config.autoCloseBrackets = false;
   });
 });
