d9e7b2bc9adfb86abdd8c92f737f270f29bafa55
Simon Walker <s.r.walker101@googlemail.com>
2015-06-30 08:59:25 +0000

    Update scripts to run in current terminal

--- i3/i3/scripts.sh
+++ i3/i3/scripts.sh
@@ -1,19 +1,31 @@
 ssh-term() {
-    urxvt -e bash -c "TERM=xterm ssh -t $1 $2"
+    bash -c "TERM=xterm ssh -t $1 $2"
 }
 
 ngtshead-term() {
     ssh-term ngtshead.astro "$*"
 }
 
+_node-loadavg() {
+    echo watch -n 120 /home/sw/.bin/ngts/node-loadavg
+}
+
+_watchjobs() {
+    echo /home/sw/.bin/watchjobs
+}
+
+_copywatcher() {
+    echo /home/sw/.local/bin/tmux attach -t copy
+}
+
 node-loadavg() {
-    ngtshead-term watch -n 120 /home/sw/.bin/ngts/node-loadavg
+    ngtshead-term "$(_node-loadavg)"
 }
 
 watchjobs() {
-    ngtshead-term /home/sw/.bin/watchjobs
+    ngtshead-term "$(_watchjobs)"
 }
 
 copywatcher() {
-    ngtshead-term /home/sw/.local/bin/tmux attach -t copy
+    ngtshead-term "$(_copywatcher)"
 }
