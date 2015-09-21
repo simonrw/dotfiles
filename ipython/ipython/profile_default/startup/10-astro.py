f9a20fceb82a980ec5962d1d418d4b55b6287eaa
Simon Walker <s.r.walker101@googlemail.com>
2015-09-21 18:08:17 +0100

    Add astro magic command to import astropy

---
+++ ipython/ipython/profile_default/startup/10-astro.py
@@ -0,0 +1,15 @@
+try:
+    import numpy as np
+except ImportError:
+    pass
+else:
+
+    ip = get_ipython()
+
+    def import_astropy(self, arg):
+        ip.ex('import astropy')
+        ip.ex('from astropy import coordinates as coords')
+        ip.ex('import astropy.units as u')
+        ip.ex('from astropy.io import fits')
+
+    ip.define_magic('astro', import_astropy)
