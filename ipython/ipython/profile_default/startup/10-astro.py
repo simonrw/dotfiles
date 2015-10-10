6a16443781898a357caf8e3ce0a9de5a6cc119f5
Simon Walker <s.r.walker101@googlemail.com>
2015-10-10 20:29:07 +0100

    Remove importing of numpy in all ipython sessions

--- ipython/ipython/profile_default/startup/10-astro.py
+++ ipython/ipython/profile_default/startup/10-astro.py
@@ -1,15 +1,9 @@
-try:
-    import numpy as np
-except ImportError:
-    pass
-else:
+ip = get_ipython()
 
-    ip = get_ipython()
+def import_astropy(self, arg):
+    ip.ex('import astropy')
+    ip.ex('from astropy import coordinates as coords')
+    ip.ex('import astropy.units as u')
+    ip.ex('from astropy.io import fits')
 
-    def import_astropy(self, arg):
-        ip.ex('import astropy')
-        ip.ex('from astropy import coordinates as coords')
-        ip.ex('import astropy.units as u')
-        ip.ex('from astropy.io import fits')
-
-    ip.define_magic('astro', import_astropy)
+ip.define_magic('astro', import_astropy)
