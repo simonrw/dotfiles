e30bc2449ca4bef2eac9c0626dad8666f37cc8d3
Simon Walker <s.r.walker101@googlemail.com>
2015-11-12 11:48:01 +0000

    Jupyter hide ui elements

--- jupyter/jupyter/custom/custom.js
+++ jupyter/jupyter/custom/custom.js
@@ -1,4 +1,9 @@
-require(['notebook/js/codecell', 'notebook/js/cell'], function(code_cell, cell) {
+require(['jquery', 'base/js/namespace', 'notebook/js/codecell', 'notebook/js/cell'], function($, IPython, code_cell, cell) {
+    $([IPython.events]).on('kernel_idle.Kernel',function () {
+        /* Hide ui elements */
+        $('div#maintoolbar').hide();
+        $('div#header-container').hide();
+    });
     code_cell.CodeCell.options_default.cm_config.autoCloseBrackets = false;
     cell.Cell.options_default.cm_config.lineWrapping = true;
 });
