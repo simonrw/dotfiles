5556c5d4ce4e8a50b36735f04e7341cc301ffc80
Simon Walker <s.r.walker101@googlemail.com>
2015-11-20 13:14:34 +0000

    UI elements hidden on proper ready event

--- jupyter/jupyter/custom/custom.js
+++ jupyter/jupyter/custom/custom.js
@@ -1,5 +1,5 @@
 require(['jquery', 'base/js/namespace', 'notebook/js/codecell', 'notebook/js/cell'], function($, IPython, code_cell, cell) {
-    $([IPython.events]).on('kernel_idle.Kernel',function () {
+    $([IPython.events]).on('notebook_loaded.Notebook',function () {
         /* Hide ui elements */
         $('div#maintoolbar').hide();
         $('div#header-container').hide();
