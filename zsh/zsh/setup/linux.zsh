963cc09a31e6aeca1477673ba0f94232a52ffda5
Simon Walker <s.r.walker101@googlemail.com>
2016-04-05 14:45:01 +0100

    Use colour with ls by default

--- zsh/zsh/setup/linux.zsh
+++ zsh/zsh/setup/linux.zsh
@@ -5,7 +5,7 @@ case $OSTYPE in
         export PATH=${PATH}:${HOME}/.gem/ruby/1.8/bin
         export LD_LIBRARY_PATH=${BUILD_PREFIX}/lib:${LD_LIBRARY_PATH}
         export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
-        export LS_OPTIONS=--color=never
+        alias ls='ls --color=auto'
 
         # Set up the module command
         function module() { eval `modulecmd zsh $*`; }
@@ -14,7 +14,6 @@ case $OSTYPE in
         alias open='xdg-open 2>/dev/null'
         alias pbcopy='xsel --clipboard --input'
         alias pbpaste='xsel --clipboard --output'
-        alias lsc='ls --color=auto'
 
         alias configurevim='./configure --prefix=$BUILD_PREFIX --with-features=huge --enable-pythoninterp --with-compiledby="Simon Walker" --disable-gui --without-x'
         ;;
