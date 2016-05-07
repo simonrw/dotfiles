a49ed9ff0111faa88bdb6c93041101abaefac544
Simon Walker <s.r.walker101@googlemail.com>
2016-05-07 10:47:31 +0100

    Swap tmux switcher alias for fs and improve

--- zsh/zsh/setup/60fzf.zsh
+++ zsh/zsh/setup/60fzf.zsh
@@ -1,1 +1,13 @@
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+fs() {
+    local session
+    session=$(tmux list-sessions -F "#{session_name}" | \
+        fzf-tmux --query="$1" --select-1 --exit-0)
+
+    if [ -z ${TMUX} ]; then
+        tmux attach -t "$session"
+    else
+        tmux switch-client -t "$session"
+    fi
+}
