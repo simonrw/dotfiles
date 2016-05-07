671321eb13ee222e3f8df9625bdc461eeeb48d69
Simon Walker <s.r.walker101@googlemail.com>
2016-05-07 11:32:15 +0100

    Add function to checkout git branch

--- zsh/zsh/setup/60fzf.zsh
+++ zsh/zsh/setup/60fzf.zsh
@@ -11,3 +11,17 @@ fs() {
         tmux switch-client -t "$session"
     fi
 }
+
+gco() {
+    local tags branches target
+    tags=$(
+    git tag | awk '{print "\x1b[31;1mtag\x1b[m\t" $1}') || return
+    branches=$(
+    git branch --all | grep -v HEAD             |
+    sed "s/.* //"    |
+    sort -u          | awk '{print "\x1b[34;1mbranch\x1b[m\t" $1}') || return
+    target=$(
+    (echo "$tags"; echo "$branches") |
+    fzf-tmux -- --no-hscroll --ansi +m -d "\t" -n 2) || return
+    git checkout $(echo "$target" | awk '{print $2}')
+}
