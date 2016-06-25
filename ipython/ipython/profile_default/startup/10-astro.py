89f264df207e82e7926f236c1856f0751ad69ceb
Simon Walker <s.r.walker101@googlemail.com>
2016-06-25 22:33:11 +0100

    Remove astropy setup file from ipython

--- ipython/ipython/profile_default/startup/10-astro.py
+++
@@ -1,9 +0,0 @@
-ip = get_ipython()
-
-def import_astropy(self, arg):
-    ip.ex('import astropy')
-    ip.ex('from astropy import coordinates as coords')
-    ip.ex('import astropy.units as u')
-    ip.ex('from astropy.io import fits')
-
-ip.define_magic('astro', import_astropy)
