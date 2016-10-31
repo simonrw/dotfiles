9dc969c9e9099dbe34c1b26acee271f94ba27776
Simon Walker <s.r.walker101@googlemail.com>
2016-10-31 10:52:36 +0000

    Add alias ag => rg if available

--- zsh/zsh/setup/20aliases.zsh
+++ zsh/zsh/setup/20aliases.zsh
@@ -50,3 +50,7 @@ alias par-ds='ssh par-ds'
 alias ngts_ops='ngtsdb ngts_ops'
 alias ngts_pipe='ngtsdb ngts_pipe'
 alias irc='ssh -t raspberrypi tmux attach -t irc'
+
+if has_executable rg; then
+    alias ag=rg
+fi
