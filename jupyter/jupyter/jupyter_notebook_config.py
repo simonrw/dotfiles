a5814f77eb4e0137d03dddbb2df5df905d97ff20
Simon Walker <s.r.walker101@googlemail.com>
2016-11-09 12:00:50 +0000

    Remove custom jupyter config

--- jupyter/jupyter/jupyter_notebook_config.py
+++ jupyter/jupyter/jupyter_notebook_config.py
@@ -1,5 +1,1 @@
 c = get_config()
-
-# Subset of matplotlib rcParams that should be different for the inline backend.
-c.InlineBackend.rc = {'figure.figsize': (10, 8)}
-# c.InlineBackend.figure_format = 'svg'
