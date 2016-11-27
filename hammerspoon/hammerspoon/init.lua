a584e2d760383b8931eaef8034ddb43fd8de7171
Simon Walker <s.r.walker101@googlemail.com>
2016-11-27 22:18:31 +0000

    Track hammerspoon config

---
+++ hammerspoon/hammerspoon/init.lua
@@ -0,0 +1,56 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "w", function()
+    hs.notify.new({title="Hammerspoon", informativeText="Hello world"}):send()
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + (max.w / 2)
+    f.y = max.y
+    f.w = max.w / 2
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "f", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+-- Reload the config on file change
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end):start()
+hs.alert.show("Config reloaded")
