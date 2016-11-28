79f23fa5fa5215d1d3dfa535ba515746fb1dad98
Simon Walker <s.r.walker101@googlemail.com>
2016-11-28 10:22:49 +0000

    Add window resize controls to hammerspoon

--- hammerspoon/hammerspoon/init.lua
+++ hammerspoon/hammerspoon/init.lua
@@ -41,6 +41,31 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "f", function()
     win:setFrame(f)
 end)
 
+-- constant holding the window enlargement/shrinkage factor
+WINDOW_SIZE_CHANGE = 10
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "-", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+
+    f.w = f.w - WINDOW_SIZE_CHANGE
+    f.h = f.h - WINDOW_SIZE_CHANGE
+    f.x = f.x + WINDOW_SIZE_CHANGE / 2
+    f.y = f.y + WINDOW_SIZE_CHANGE / 2
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "=", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+
+    f.w = f.w + WINDOW_SIZE_CHANGE
+    f.h = f.h + WINDOW_SIZE_CHANGE
+    f.x = f.x - WINDOW_SIZE_CHANGE / 2
+    f.y = f.y - WINDOW_SIZE_CHANGE / 2
+    win:setFrame(f)
+end)
+
 -- Reload the config on file change
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", function(files)
     doReload = false
