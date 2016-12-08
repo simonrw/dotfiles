c42011117cbfdc9e7378d4e3726c16cd01ab19f5
Simon Walker <s.r.walker101@googlemail.com>
2016-12-08 16:02:39 +0000

    Add caffeine functionality

--- hammerspoon/hammerspoon/init.lua
+++ hammerspoon/hammerspoon/init.lua
@@ -114,6 +114,25 @@ hs.hotkey.bind({'cmd', 'shift'}, 'Space', function()
     hs.hints.windowHints()
 end)
 
+-- Caffeine
+local caffeine = hs.menubar.new()
+function setCaffeineDisplay(displayIdleAllowed)
+    if displayIdleAllowed then
+        caffeine:setIcon("~/.hammerspoon/caffeine-icons/inactive@2x.png");
+    else
+        caffeine:setIcon("~/.hammerspoon/caffeine-icons/active@2x.png");
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
 -- Reload the config on file change
 hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', function(files)
     doReload = false
