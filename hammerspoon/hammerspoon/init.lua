f974551f8ab9f620e2ab941c20ffdf14fb545a2a
Simon Walker <s.r.walker101@googlemail.com>
2016-12-11 10:31:14 +0000

    Switch to firefox

--- hammerspoon/hammerspoon/init.lua
+++ hammerspoon/hammerspoon/init.lua
@@ -11,7 +11,7 @@ end)
 
 -- Browser
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
-    hs.application.launchOrFocus('Google Chrome')
+    hs.application.launchOrFocus('FirefoxDeveloperEdition')
 end)
 
 -- Window manipulation
