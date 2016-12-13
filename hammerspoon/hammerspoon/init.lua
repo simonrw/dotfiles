b9a382a813ff04d6fdb9db6f183b734aae0af3ad
Simon Walker <s.r.walker101@googlemail.com>
2016-12-13 22:55:05 +0000

    Reduce window borders

--- hammerspoon/hammerspoon/init.lua
+++ hammerspoon/hammerspoon/init.lua
@@ -17,7 +17,7 @@ end)
 -- Window manipulation
 
 -- constant holding the window enlargement/shrinkage factor
-WINDOW_SIZE_CHANGE = 20
+WINDOW_SIZE_CHANGE = 5
 
 -- Move window to the next screen
 hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'o', function()
