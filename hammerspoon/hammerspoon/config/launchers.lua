efeffa2aa5601e004efec5600bf965532cc01cdc
Simon Walker <s.r.walker101@googlemail.com>
2016-12-15 10:19:25 +0000

    Break out hammerspoon launchers and window manipulation

---
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -0,0 +1,15 @@
+-- Terminal
+hs.hotkey.bind({'cmd', 'alt'}, 't', function()
+    hs.application.launchOrFocus('iTerm')
+end)
+
+-- Browser
+hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
+    hs.application.launchOrFocus('Safari')
+end)
+
+-- Hammerspoon window
+hs.hotkey.bind({'cmd', 'alt'}, 'h', function()
+    hs.application.launchOrFocus('Hammerspoon')
+end)
+
---
+++ hammerspoon/hammerspoon/config/window.lua
@@ -0,0 +1,92 @@
+-- constant holding the window enlargement/shrinkage factor
+local WINDOW_SIZE_CHANGE = 20
+local WINDOW_BORDER = 0
+
+-- Move window to the next screen
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'o', function()
+    local win = hs.window.focusedWindow()
+    local nextScreen = win:screen():next()
+    win:moveToScreen(nextScreen)
+end)
+
+-- Center window
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'c', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local screenFrame = screen:frame()
+
+    f.x = (screenFrame.w / 2) - (f.w / 2)
+    f.y = (screenFrame.h / 2) - (f.h / 2)
+    f.w = f.w
+    f.h = f.h
+    win:setFrame(f)
+end)
+
+-- Move window to left half
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Left', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + WINDOW_BORDER / 2
+    f.y = max.y + WINDOW_BORDER / 2
+    f.w = max.w / 2 - WINDOW_BORDER / 2
+    f.h = max.h - WINDOW_BORDER
+    win:setFrame(f)
+end)
+
+-- Move window to the right half
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Right', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + (max.w / 2)
+    f.y = max.y + WINDOW_BORDER / 2
+    f.w = max.w / 2 - WINDOW_BORDER / 2
+    f.h = max.h - WINDOW_BORDER
+    win:setFrame(f)
+end)
+
+-- Maximise window
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'f', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + WINDOW_BORDER / 2
+    f.y = max.y + WINDOW_BORDER / 2
+    f.w = max.w - WINDOW_BORDER
+    f.h = max.h - WINDOW_BORDER
+    win:setFrame(f)
+end)
+
+-- Make window smaller
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, '-', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+
+    f.w = f.w - WINDOW_SIZE_CHANGE
+    f.h = f.h - WINDOW_SIZE_CHANGE
+    f.x = f.x + WINDOW_SIZE_CHANGE / 2
+    f.y = f.y + WINDOW_SIZE_CHANGE / 2
+    win:setFrame(f)
+end)
+
+-- Make window larger
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, '=', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+
+    f.w = f.w + WINDOW_SIZE_CHANGE
+    f.h = f.h + WINDOW_SIZE_CHANGE
+    f.x = f.x - WINDOW_SIZE_CHANGE / 2
+    f.y = f.y - WINDOW_SIZE_CHANGE / 2
+    win:setFrame(f)
+end)
+
+
