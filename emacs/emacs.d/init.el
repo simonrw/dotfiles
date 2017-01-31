f8242a1bd47d805625d9c3ffe376301496a89455
Simon Walker <s.r.walker101@googlemail.com>
2017-01-31 13:17:54 +0000

    Add rust mode package

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -11,7 +11,8 @@
 
 (defvar srw/my-packages
   '(better-defaults
-      molokai-theme))
+	rust-mode
+	molokai-theme))
 
 (mapc #'(lambda (package)
 	  (unless (package-installed-p package)
@@ -66,3 +67,15 @@
 
 ;; Theming
 (load-theme 'molokai t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (rust-mode molokai-theme better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
