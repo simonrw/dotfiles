e9108ba29fb971626750909a50cdaf78a3ec218e
Simon Walker <s.r.walker101@googlemail.com>
2017-01-31 11:25:45 +0000

    Bind key for notational velocity

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -13,6 +13,11 @@ hs.hotkey.bind({'cmd', 'alt'}, 'e', function()
     hs.application.launchOrFocus('Mail')
 end)
 
+hs.hotkey.bind({'cmd', 'alt'}, 'n', function()
+    hs.application.launchOrFocus('Notational Velocity')
+end)
+
+
 -- Hammerspoon window
 hs.hotkey.bind({'cmd', 'shift', 'alt'}, 'h', function()
     hs.application.launchOrFocus('Hammerspoon')
