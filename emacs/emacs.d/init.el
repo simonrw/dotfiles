0b5ec343c05e7b70ef6b17ef77a49d735e072606
Simon Walker <s.r.walker101@googlemail.com>
2017-01-31 13:14:25 +0000

    Clean up emacs config and remove spacemacs

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -1,3 +1,4 @@
+
 ;; Packages
 (require 'package)
 
@@ -10,27 +11,7 @@
 
 (defvar srw/my-packages
   '(better-defaults
-    flycheck ;; syntax checker
-    py-autopep8
-	yaml-mode
-	solarized-theme
-	smex
-    markdown-mode
-	adoc-mode
-	fzf
-    multi-term
-	clojure-mode
-	auto-complete
-	cider
-	paredit
-	base16-theme
-	magit
-	ansible
-	editorconfig
-	jinja2-mode
-	rust-mode
-	company
-	ir-black-theme))
+      molokai-theme))
 
 (mapc #'(lambda (package)
 	  (unless (package-installed-p package)
@@ -44,10 +25,6 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (global-visual-line-mode t)
 
-;; Hooks
-(add-hook 'markdown-mode-hook
-		  (auto-fill-mode))
-
 ;; Disable blinking cursor
 (blink-cursor-mode 0)
 
@@ -60,49 +37,15 @@
     (normal-top-level-add-subdirs-to-load-path))
 
   ;; Toggle fullscreen mode
-  (global-set-key [s-return] 'toggle-frame-fullscreen)
-
-  ;; Allow hash command
-  (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#"))))
+  (global-set-key [s-return] 'toggle-frame-fullscreen))
 
 ;; Language specifics
 (setq-default c-basic-offset 4 c-default-style "linux")
 (setq-default tab-width 4 indent-tabs-mode t)
 
-;; Org mode settings
-(define-key global-map "\C-cl" 'org-store-link)
-(define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-
-(setq org-agenda-files (list "~/org/ngts.org"
-							 "~/org/home.org"))
-
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '((python . t)))
-
-
-
 ;; Make sure everything is in utf-8
 (set-language-environment "UTF-8")
 (set-buffer-file-coding-system 'utf-8)
-
-;; Use auto-complete mode always
-(global-auto-complete-mode)
-
-;; Set up fzf
-;; Add the fzf dir to the PATH
-;; (setenv "PATH" (concat
-				;; (getenv "HOME") "/.fzf/bin:" (getenv "PATH")))
-;; (setq exec-path
-	  ;; (append
-	   ;; '((concat (getenv "HOME") "/.fzf/bin") exec-path)))
-
-;; Smex
-(autoload 'smex "smex"
-  "Smex is a M-x enhancement for Emacs")
-
-(global-set-key (kbd "M-x") 'smex)
 
 ;; Open files in an existing frame instead of a new frame each time
 (setq ns-pop-up-frames nil)
@@ -121,15 +64,5 @@
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
 
-;; Set up rust and racer
-(setq racer-rust-src-path "~/.cargo/rust-src/src/")
-(add-hook 'rust-mode-hook #'racer-mode)
-(add-hook 'racer-mode-hook #'eldoc-mode)
-(add-hook 'racer-mode-hook (lambda ()
-							 #'company-mode
-							 (define-key rust-mode-map (kbd "TAB") #'company-indent-or-complete-common)
-							 (setq company-tooltip-align-annotations t)))
-
-
 ;; Theming
-(load-theme 'ir-black t)
+(load-theme 'molokai t)
