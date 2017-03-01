a5a8790b9d3baab2956029d5a7c1dffa6b7bc4ac
Simon Walker <s.r.walker101@googlemail.com>
2017-03-01 10:00:39 +0000

    Disable custom hiding from juptyer notebook

--- jupyter/jupyter/custom/custom.js
+++ jupyter/jupyter/custom/custom.js
@@ -2,8 +2,11 @@
 require(['jquery', 'base/js/namespace', 'notebook/js/codecell', 'notebook/js/cell'], function($, IPython, code_cell, cell) {
     $([IPython.events]).on('notebook_loaded.Notebook',function () {
         /* Hide ui elements */
+        /* This causes visual bugs which I don't like. Hence I'm
+         * disabling this behaviour - SRW 2017/03/01
         $('div#maintoolbar').hide();
         $('div#header-container').hide();
+        */
     });
     code_cell.CodeCell.options_default.cm_config.autoCloseBrackets = false;
     cell.Cell.options_default.cm_config.lineWrapping = true;
