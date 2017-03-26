5ae259bf8dcd5430b6a00a53dbefaf57952ba754
Simon Walker <s.r.walker101@googlemail.com>
2017-03-26 11:37:15 +0100

    Make left/right window splits non-equal ratio

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -2,6 +2,7 @@
 local WINDOW_SIZE_CHANGE = 16
 local WINDOW_BORDER = 0
 local FULLSCREEN_BORDER = 0
+local LEFTRIGHT_FRACTION = 1. / 1.61803398875 -- golden ratio reciprocal
 
 -- Move window to the next screen
 hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'o', function()
@@ -24,7 +25,7 @@ hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'c', function()
     win:setFrame(f)
 end)
 
--- Move window to left half
+-- Move window to left two thirds
 hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Left', function()
     local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -33,7 +34,7 @@ hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Left', function()
 
     f.x = max.x + WINDOW_BORDER / 2
     f.y = max.y + WINDOW_BORDER / 2
-    f.w = max.w / 2 - WINDOW_BORDER / 2
+    f.w = LEFTRIGHT_FRACTION * max.w
     f.h = max.h - WINDOW_BORDER
     win:setFrame(f)
 end)
@@ -45,9 +46,9 @@ hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Right', function()
     local screen = win:screen()
     local max = screen:frame()
 
-    f.x = max.x + (max.w / 2)
+    f.x = max.x + LEFTRIGHT_FRACTION * max.w
     f.y = max.y + WINDOW_BORDER / 2
-    f.w = max.w / 2 - WINDOW_BORDER / 2
+    f.w = (1 - LEFTRIGHT_FRACTION) * max.w --  - WINDOW_BORDER / 2
     f.h = max.h - WINDOW_BORDER
     win:setFrame(f)
 end)
