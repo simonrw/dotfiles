3ba7ac83829e8eacac661b94567701d00299c767
Simon Walker <s.r.walker101@googlemail.com>
2017-04-25 13:25:29 +0100

    Set up variables

---
+++ fish/conf.d/rust.fish
@@ -0,0 +1,4 @@
+# Rust configuration
+set CARGO_HOME {$HOME}/.cargo
+set CARGO_INCREMENTAL 1
+set RUST_SRC_PATH {$HOME}/.cargo/rust-src/src
--- fish/conf.d/vars.fish
+++ fish/conf.d/vars.fish
@@ -1,2 +1,11 @@
 # Set up environment variables here
-set PATH $PATH $HOME/.cargo/bin
+set BUILD_PREFIX $HOME/.local
+set PATH $HOME/.bin $HOME/.bin/ngts $BUILD_PREFIX/bin $HOME/.cargo/bin $PATH
+set EDITOR vim
+set VISUAL $EDITOR
+
+# Set up fzf
+set FZF_DEFAULT_OPTS "--color dark --tac --ansi --no-mouse --tabstop 4 --inline-info --tiebreak=begin,length"
+
+# General settings
+set TREE_CHARSET ascii               # For rendering with the `tree` command
