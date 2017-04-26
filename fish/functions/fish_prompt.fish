fcddc07f144b16dcc45b973c3ebdb21ac9c7fd0e
Simon Walker <s.r.walker101@googlemail.com>
2017-04-26 11:20:32 +0100

    Return prompt to my classic one

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,3 +1,12 @@
 function fish_prompt
-	printf "\n\$ "
+	set -l status_color
+	if test $status -eq 0
+		set status_color green
+	else
+		set status_color red
+	end
+
+	set -l prompt_symbol (set_color $status_color)"\$"(set_color normal)
+
+	printf "\n%s " $prompt_symbol
 end
