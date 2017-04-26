7bd6f0d58971f7cf59297402d1c30fdff65ca5ae
Simon Walker <s.r.walker101@googlemail.com>
2017-04-26 10:53:35 +0100

    Simplify prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,13 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-	# Save our status
-    set -l last_status $status
-
-    set -l last_status_string ""
-    if [ $last_status -ne 0 ]
-        printf "%s(%d)%s " (set_color red --bold) $last_status (set_color normal)
-    end
-
-    set -l color_cwd
+	set -l color_cwd
     set -l suffix
     switch $USER
         case root toor
