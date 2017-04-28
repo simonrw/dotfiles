9e6c941b7fc943fb3f853035cf0d2c80ac73ca75
Simon Walker <s.r.walker101@googlemail.com>
2017-04-28 10:03:43 +0100

    Switch to chrome

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -5,7 +5,7 @@ end)
 
 -- Browser
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
-    hs.application.launchOrFocus('FirefoxNightly')
+    hs.application.launchOrFocus('Google Chrome')
 end)
 
 -- Email
