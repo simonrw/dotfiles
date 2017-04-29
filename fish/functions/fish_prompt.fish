5ca54747db28662fbe55430be74ad03281cf7879
Simon Walker <s.r.walker101@googlemail.com>
2017-04-29 17:36:38 +0100

    Add suspended indicator for fish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,12 +1,22 @@
 function fish_prompt
+	set -l njobs (jobs | wc -l | sed 's/ //g')
 	set -l status_color
+
 	if test $status -eq 0
 		set status_color green
 	else
 		set status_color red
 	end
 
-	set -l prompt_symbol (set_color $status_color)"\$"(set_color normal)
+	set -l prompt_njobs_char
+	if test $njobs -eq 0
+		set prompt_njobs_char ''
+	else
+		set prompt_njobs_char '= '
+	end
 
-	printf "\n%s " $prompt_symbol
+	set -l prompt_symbol (set_color $status_color)"\$"(set_color normal)
+	set -l prompt_njobs_symbol (set_color yellow)$prompt_njobs_char(set_color normal)
+
+	printf "\n%s%s " $prompt_njobs_symbol $prompt_symbol
 end
