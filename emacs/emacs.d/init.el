dfaae7cb49b7e1f1b10e784eff2392fe142bbff4
Simon Walker <s.r.walker101@googlemail.com>
2017-07-22 11:38:05 +0100

    Add nice eshell mapping
    Taken from http://www.howardism.org/Technical/Emacs/eshell-fun.html

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -208,6 +208,31 @@
 
 (use-package cider)
 
+;; Use emacs shell
+(defun eshell-here ()
+  "Opens a new eshell in the current buffers cwd."
+  (interactive)
+  (let* ((parent (if (buffer-file-name)
+                     (file-name-directory (buffer-file-name))
+                   default-directory))
+         (height (/ (window-total-height) 3))
+         (name (car (last (split-string parent "/" t)))))
+    (split-window-vertically (- height))
+    (other-window 1)
+    (eshell "new")
+    (rename-buffer (concat "*eshell: " name "*"))
+    (insert (concat "ls"))
+    (eshell-send-input)))
+
+(global-set-key (kbd "C-!") 'eshell-here)
+
+(defun eshell/x ()
+  "Close the eshell from eshell."
+  (interactive)
+  (insert "exit")
+  (eshell-send-input)
+  (delete-window))
+
 ;; Theming
 (load-theme 'wombat)
 
