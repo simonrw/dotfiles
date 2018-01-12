70535dc620b3d651ba212d36320169339bddc65d
Simon Walker <s.r.walker101@googlemail.com>
2018-01-12 16:52:56 +0000

    Remove pyenv support

--- zsh/zsh/setup/70pyenv.zsh
+++
@@ -1,9 +0,0 @@
-export PATH=${HOME}/.pyenv/bin:${PATH}
-
-if ! has_executable "pyenv"; then
-    # Install pyenv using pyenv-installer
-    echo "Installing pyenv"
-    curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-fi
-    # Shim to set up pyenv
-eval "$(command pyenv init - --no-rehash)"
