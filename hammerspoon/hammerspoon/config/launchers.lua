7e937d9b38c8d81af71dd0409f6aa96d11ac0da7
Simon Walker <s.r.walker101@googlemail.com>
2018-02-10 11:26:58 +0000

    Switch to safari

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -5,7 +5,7 @@ end)
 
 -- Browser
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
-    hs.application.launchOrFocus('Google Chrome')
+    hs.application.launchOrFocus('Safari')
 end)
 
 -- Notational velocity
