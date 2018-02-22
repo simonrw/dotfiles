5abd7b0bba0ae3f5d9b3ced8eaa0475e107bfe1b
Simon Walker <s.r.walker101@googlemail.com>
2018-02-22 22:23:07 +0000

    Switch to google chrome

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -5,7 +5,7 @@ end)
 
 -- Browser
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
-    hs.application.launchOrFocus('FirefoxNightly')
+    hs.application.launchOrFocus('Google Chrome')
 end)
 
 hs.hotkey.bind({'cmd', 'alt'}, 'e', function()
