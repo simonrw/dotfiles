d8af6f7f5e07f15bb363ff5eb0476aeadb4be3e4
Simon Walker <s.r.walker101@googlemail.com>
2018-04-10 19:52:10 +0100

    Switch to Safari

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -1,13 +1,9 @@
 -- Terminal
 hs.hotkey.bind({'cmd', 'alt'}, 't', function()
-    hs.application.launchOrFocus('kitty')
+    hs.application.launchOrFocus('Kitty')
 end)
 
 -- Browser
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
-    hs.application.launchOrFocus('Firefox')
-end)
-
-hs.hotkey.bind({'cmd', 'alt'}, 'e', function()
-    hs.application.launchOrFocus('Mail')
+    hs.application.launchOrFocus('Safari')
 end)
