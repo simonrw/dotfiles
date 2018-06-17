9986a8ca9305f90f47b9b82da5f2612be9d9d2f5
Simon Walker <s.r.walker101@googlemail.com>
2018-06-17 19:43:54 +0100

    Add method of switching between editor and terminal

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -1,9 +1,29 @@
--- Terminal
-hs.hotkey.bind({'cmd', 'alt'}, 't', function()
+function launchTerminal()
     hs.application.launchOrFocus('Kitty')
-end)
+end
+
+function launchEditor()
+    hs.application.launchOrFocus('Visual Studio Code')
+end
+
+-- Terminal
+hs.hotkey.bind({'cmd', 'alt'}, 't', launchTerminal)
 
 -- Browser
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
-    hs.application.launchOrFocus('FirefoxDeveloperEdition')
+    hs.application.launchOrFocus('Google Chrome')
+end)
+
+-- Editor
+hs.hotkey.bind({'cmd', 'alt'}, 'e', launchEditor)
+
+-- Switch between editor and terminal
+hs.hotkey.bind({'cmd', 'alt'}, 's', function()
+    local currentApplication = hs.application.frontmostApplication()
+    local currentApplicationName = currentApplication:name():lower()
+    if currentApplicationName == 'code' then
+        hs.application.launchOrFocus('Kitty')
+    elseif currentApplicationName == 'kitty' then
+        hs.application.launchOrFocus('Visual Studio Code')
+    end
 end)
