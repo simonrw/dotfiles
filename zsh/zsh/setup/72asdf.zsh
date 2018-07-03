2b207c51ee7c631647efea2996de9ab9e5fbbcae
Simon Walker <s.r.walker101@googlemail.com>
2018-07-03 23:29:12 +0100

    Install asdf if missing

--- zsh/zsh/setup/72asdf.zsh
+++ zsh/zsh/setup/72asdf.zsh
@@ -1,2 +1,11 @@
+if [[ ! -d ${HOME}/.asdf ]]; then
+    if has_executable "git"; then
+        echo "Fetching asdf"
+        git clone https://github.com/asdf-vm/asdf.git ${HOME}/.asdf
+    else
+        echo "Cannot install asdf without git :(" >&2
+    fi
+fi
+
 . ${HOME}/.asdf/asdf.sh
 . ${HOME}/.asdf/completions/asdf.bash
