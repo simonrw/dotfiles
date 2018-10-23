83beac65bc039c64163348644039a86a070cb9a9
Simon Walker <s.r.walker101@googlemail.com>
2018-10-23 19:19:36 +0100

    Unify history support

--- zsh/zsh/setup/60fzf.zsh
+++ zsh/zsh/setup/60fzf.zsh
@@ -48,3 +48,22 @@ function fpass() {
 
   lpass ls | fzf-tmux | =grep -oE '\d+' | xargs lpass show -c --password
 }
+
+# Overload the default history widget which reverses the order of the entries
+function fzf-history-widget () {
+    local selected num
+    setopt localoptions noglobsubst noposixbuiltins pipefail 2> /dev/null
+    selected=($(fc -l 1 |
+        FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort $FZF_CTRL_R_OPTS --query=${(qqq)LBUFFER} +m" $(__fzfcmd)))
+    local ret=$?
+    if [ -n "$selected" ]
+    then
+        num=$selected[1]
+        if [ -n "$num" ]
+        then
+            zle vi-fetch-history -n $num
+        fi
+    fi
+    zle reset-prompt
+    return $ret
+}
