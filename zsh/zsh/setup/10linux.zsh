36dc53c599083d6e186bd895d2db71f6bb3c3ea3
Simon Walker <s.r.walker101@googlemail.com>
2018-11-14 18:38:59 +0000

    Remove some linux specific code

--- zsh/zsh/setup/10linux.zsh
+++ zsh/zsh/setup/10linux.zsh
@@ -1,17 +1,7 @@
 # Linux setup
 case $OSTYPE in
     linux*)
-        export LD_LIBRARY_PATH=${BUILD_PREFIX}/lib:${LD_LIBRARY_PATH}
-        export PKG_CONFIG_PATH=${BUILD_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}
-
-        # Set up the module command
-        function module() { eval `modulecmd zsh $*`; }
-
-        export work=${HOME}/work/
         alias open=xdg-open
         alias pbcopy='xsel --clipboard --input'
         alias pbpaste='xsel --clipboard --output'
-
-
-        alias configurevim='./configure --prefix=$BUILD_PREFIX --with-features=huge --enable-pythoninterp --with-compiledby="Simon Walker" --disable-gui --without-x'
 esac
