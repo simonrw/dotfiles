2eadb1f63d29e25e432d273cc585db32dd007237
Simon Walker <s.r.walker101@googlemail.com>
2018-12-11 13:23:37 +0000

    Reinstate evil mode
    Following guide: https://blog.aaronbieber.com/2016/01/23/living-in-evil.html

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -127,6 +127,24 @@
 (use-package toml-mode
   :mode (("\\.toml\\'" . toml-mode)))
 
+(use-package evil
+  :ensure t
+  :config
+  (evil-mode 1)
+
+  (use-package evil-leader
+	:ensure t
+	:config
+	(global-evil-leader-mode))
+
+  (use-package evil-surround
+	:ensure t
+	:config
+	(global-evil-surround-mode))
+
+  (use-package evil-indent-textobject
+	:ensure t))
+
 ;; Configure C family of xlanguages
 (add-hook 'c-mode-common-hook
 	  (lambda ()
