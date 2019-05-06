e7e28537f6318db75eed9e67a9a7cd343509e9da
Simon Walker <s.r.walker101@googlemail.com>
2019-05-06 10:24:01 +0100

    Simplify launcher configuration

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -1,41 +1,15 @@
 terminalEmulator = "Kitty"
-browser = "Firefox Developer Edition"
+browser = "Google Chrome"
+email = "Thunderbird"
 
-function launchTerminal()
-    hs.application.launchOrFocus(terminalEmulator)
-end
-
-function launchEditor()
-    hs.application.launchOrFocus('Visual Studio Code')
-end
-
-function launchEmail()
-    hs.application.launchOrFocus('Thunderbird')
-end
-
-function launchTerminal()
-    hs.application.launchOrFocus(terminalEmulator)
-end
-
--- Browser
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
     hs.application.launchOrFocus(browser)
 end)
 
-hs.hotkey.bind({'cmd', 'alt'}, 't', launchTerminal)
+hs.hotkey.bind({'cmd', 'alt'}, 't', function()
+    hs.application.launchOrFocus(terminal)
+end)
 
-hs.hotkey.bind({'cmd', 'alt'}, 'e', launchEmail)
-
--- Editor
--- hs.hotkey.bind({'cmd', 'alt'}, 'e', launchEmail)
-
--- Switch between editor and terminal
-hs.hotkey.bind({'cmd', 'alt'}, 's', function()
-    local currentApplication = hs.application.frontmostApplication()
-    local currentApplicationName = currentApplication:name():lower()
-    if currentApplicationName == 'code' then
-        hs.application.launchOrFocus(terminalEmulator)
-    elseif currentApplicationName == terminalEmulator then
-        hs.application.launchOrFocus('Visual Studio Code')
-    end
+hs.hotkey.bind({'cmd', 'alt'}, 'e', function()
+    hs.application.launchOrFocus(email)
 end)
