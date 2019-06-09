bec0d2b793dd75c71dd57381b0a613827e92bfb9
Simon Walker <s.r.walker101@googlemail.com>
2019-06-09 15:36:33 +0100

    Reduce window border on fullscreen windows

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -1,7 +1,7 @@
 -- constant holding the window enlargement/shrinkage factor
 local WINDOW_SIZE_CHANGE = 16
 local WINDOW_BORDER = 0
-local FULLSCREEN_BORDER = 35
+local FULLSCREEN_BORDER = 15
 local LEFTRIGHT_FRACTION = 0.55
 
 -- Move window to the next screen
