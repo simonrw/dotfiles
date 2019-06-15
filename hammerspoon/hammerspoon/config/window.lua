bb0d8ef2d9115be19250f880b005dae3f4287e35
Simon Walker <s.r.walker101@googlemail.com>
2019-06-15 16:36:59 +0100

    Revert "Remove most window management commands"
    This reverts commit ebb5b1efc37226c4de231f5932b26a3b1d460afe.

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -10,3 +10,46 @@ hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'o', function()
     local nextScreen = win:screen():next()
     win:moveToScreen(nextScreen)
 end)
+
+-- Move window to left two thirds
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Left', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = WINDOW_BORDER / 2
+    f.y = max.y + WINDOW_BORDER / 2
+    f.w = LEFTRIGHT_FRACTION * max.w - WINDOW_BORDER / 2.0
+    f.h = max.h - WINDOW_BORDER
+    win:setFrame(f)
+end)
+
+-- Move window to the right half
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Right', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + LEFTRIGHT_FRACTION * max.w
+    f.y = max.y + WINDOW_BORDER / 2
+    f.w = (1 - LEFTRIGHT_FRACTION) * max.w - WINDOW_BORDER / 2.0
+    f.h = max.h - WINDOW_BORDER
+    win:setFrame(f)
+end)
+
+function maximizeWindow()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + FULLSCREEN_BORDER / 2
+    f.y = max.y + FULLSCREEN_BORDER / 2
+    f.w = max.w - FULLSCREEN_BORDER
+    f.h = max.h - FULLSCREEN_BORDER
+    win:setFrame(f)
+end
+
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'f', maximizeWindow)
