c37b3566c9f3c2c52d30519a78a794f537d294e9
Simon Walker <s.r.walker101@googlemail.com>
2019-06-23 13:42:42 +0100

    Do not start a tmux session on virtual consoles

--- zsh/zsh/setup/05tmux.zsh
+++ zsh/zsh/setup/05tmux.zsh
@@ -21,8 +21,23 @@ _not_inside_neovim() { [[ -z "$NVIM_LISTEN_ADDRESS" ]] }
 
 _not_inside_emacs() { [[ -z "$EMACS" ]] }
 
+# We only want tmux to run when were inside a X session i.e. not at a virtual
+# console. On macos we have to pretend that an Xsession is running so hard code
+# the result.
+_inside_x_session() {
+    case $OSTYPE in
+        darwin*)
+            # Hard code this to always return an x session
+            return 0
+            ;;
+        linux*)
+            pgrep Xorg 2>&1 >/dev/null
+            ;;
+    esac
+}
+
 ensure_tmux_is_running() {
-    if _not_inside_tmux && _not_inside_neovim && _not_inside_emacs; then
+    if _not_inside_tmux && _not_inside_neovim && _not_inside_emacs && _inside_x_session; then
         tat
     fi
 }
