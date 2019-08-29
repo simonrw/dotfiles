baba0c2d72c7a709133582cda0860c169a4d137e
Simon Walker <s.r.walker101@googlemail.com>
2019-08-29 19:15:37 +0100

    Remove email keyboard shortcut
    I will be using the web apps

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -9,7 +9,3 @@ end)
 hs.hotkey.bind({'cmd', 'alt'}, 't', function()
     hs.application.launchOrFocus(terminal)
 end)
-
-hs.hotkey.bind({'cmd', 'alt'}, 'e', function()
-    hs.application.launchOrFocus(email)
-end)
