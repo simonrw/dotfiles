89efd136057cb8bad8b05363b2d6da7dfc15a8eb
Simon Walker <s.r.walker101@googlemail.com>
2019-10-23 20:16:12 +0100

    Do not include asdf when inside a poetry shell

--- zsh/zsh/setup/73asdf.zsh
+++ zsh/zsh/setup/73asdf.zsh
@@ -1,4 +1,7 @@
 if [[ -e ${HOME}/.asdf ]]; then
-    . ${HOME}/.asdf/asdf.sh
-    . ${HOME}/.asdf/completions/asdf.bash
+    # Do not activate asdf if inside a poetry shell
+    if [[ -z "${POETRY_ACTIVE}" ]]; then
+        . ${HOME}/.asdf/asdf.sh
+        . ${HOME}/.asdf/completions/asdf.bash
+    fi
 fi
