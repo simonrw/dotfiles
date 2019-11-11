ba775608c60d250c4fb6a5f62f18e39450ba97ec
Simon Walker <s.r.walker101@googlemail.com>
2019-11-11 10:08:21 +0000

    Add single alias for vim updating

--- zsh/zsh/setup/20aliases.zsh
+++ zsh/zsh/setup/20aliases.zsh
@@ -13,8 +13,7 @@ alias es='exec $SHELL'
 alias sqlite3='rlwrap =sqlite3'
 alias k=kubectl
 alias sourceenv="source ./venv/bin/activate"
-alias vpi="vim +PlugClean! +PlugInstall +qa"
-alias vpu="vim +PlugUpdate"
+alias vup="vim +PlugClean! +PlugUpgrade +PlugUpdate +qa"
 
 if has_executable rg; then
     alias grep=rg
