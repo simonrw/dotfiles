6826de5d63c49fe7f8b91bf10f213f7fa30a116b
Simon Walker <s.r.walker101@googlemail.com>
2019-11-14 15:11:18 +0000

    hide asdf behind init helper

--- zsh/zsh/setup/73asdf.zsh
+++ zsh/zsh/setup/73asdf.zsh
@@ -1,7 +1,7 @@
-if [[ -e ${HOME}/.asdf ]]; then
+function asdf-init() {
     # Do not activate asdf if inside a poetry shell
     if [[ -z "${POETRY_ACTIVE}" ]]; then
         . ${HOME}/.asdf/asdf.sh
         . ${HOME}/.asdf/completions/asdf.bash
     fi
-fi
+}
