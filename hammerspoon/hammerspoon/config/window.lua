4e11c4ea300e5fe59f21005047da24be08438096
Simon Walker <s.r.walker101@googlemail.com>
2019-11-30 20:46:04 +0000

    Increase hammerspoon window border

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -1,7 +1,7 @@
 -- constant holding the window enlargement/shrinkage factor
 local WINDOW_SIZE_CHANGE = 0
 local WINDOW_BORDER = 4
-local FULLSCREEN_BORDER = 32
+local FULLSCREEN_BORDER = 64
 local LEFTRIGHT_FRACTION = 0.55
 
 -- Move window to the next screen
