b6d721fb00ed97fb3716d9a0f23b835aa192f88f
Simon Walker <s.r.walker101@googlemail.com>
2020-02-16 17:49:19 +0000

    Add keyboard shortcut to launch email

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -9,3 +9,8 @@ end)
 hs.hotkey.bind({'cmd', 'alt'}, 't', function()
     hs.application.launchOrFocus(terminal)
 end)
+
+hs.hotkey.bind({'cmd', 'alt'}, 'e', function()
+    hs.application.launchOrFocus(email)
+end)
+
