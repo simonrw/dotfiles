f0762d1c8bd1186cd54cc78168c9b50a1d91d4ae
Simon Walker <s.r.walker101@googlemail.com>
2020-03-11 01:00:27 +0000

    Revert "Include zoxide"
    This reverts commit e24c09b1ea9d22d6b0df182abe314cff3fde1021.

--- zsh/zsh/setup/75zoxide.zsh
+++
@@ -1,9 +0,0 @@
-# Set up zoxide
-#
-# https://github.com/ajeetdsouza/zoxide
-
-if has_executable zoxide; then
-    source ${HOME}/dotfiles/external/zoxide/zoxide.plugin.zsh
-else
-    printf "\u001b[30;1mZoxide not installed\u001b[0m\n"
-fi
