763b6b721cb68f02d6feda98951864ce5099a585
Simon Walker <s.r.walker101@googlemail.com>
2020-04-07 21:41:27 +0100

    Includes compilation of `external` packages

--- deploy.py
+++ deploy.py
@@ -5,6 +5,8 @@ import argparse
 from pathlib import Path
 import logging
 from typing import Optional
+import subprocess as sp
+import os
 
 
 logging.basicConfig(
@@ -19,6 +21,13 @@ class Deployer(object):
         parser = argparse.ArgumentParser()
         parser.add_argument("-n", "--dry-run", action="store_true", default=False)
         parser.add_argument("-f", "--force", action="store_true", default=False)
+        parser.add_argument(
+            "-c",
+            "--compile",
+            action="store_true",
+            default=False,
+            help="Compile packages under `external`",
+        )
         parser.add_argument("-v", "--verbose", action="count")
         args = parser.parse_args()
 
@@ -31,16 +40,23 @@ class Deployer(object):
         logger.info("deploying dotfiles")
         logger.debug("debug logging enabled")
 
-        self = cls(dry_run=args.dry_run, force=args.force)
+        self = cls(dry_run=args.dry_run, force=args.force, should_compile=args.compile)
         self.run()
 
-    def __init__(self, dry_run: bool, force: bool) -> None:
+    def __init__(self, dry_run: bool, force: bool, should_compile: bool) -> None:
         self.dry_run = dry_run
         self.force = force
+        self.compile = should_compile
 
     def run(self) -> None:
         self.deploy_standard_dirs()
         self.deploy_dotconfig_files()
+        if self.compile:
+            self.install_rust_packages()
+        else:
+            logger.warning(
+                "`-c/--compile` not supplied, not compiling packages under `external`"
+            )
 
     def deploy_standard_dirs(self, install_path: Optional[Path] = None) -> None:
         dirnames = [
@@ -105,6 +121,18 @@ class Deployer(object):
             dest = Path.home().resolve().joinpath(".config", subdir, src.name)
             self._deploy_single_file(src, dest)
 
+    def install_rust_packages(self):
+        if not self._binary_exists("cargo"):
+            logger.warning(
+                "cannot find rust installation, skipping installing rust binaries"
+            )
+            return
+
+        subdirs = ["external/git-identity-manager", "external/mkflashdriverepo"]
+        for subdir in subdirs:
+            cmd = ["cargo", "install", "--path", subdir]
+            sp.run(cmd)
+
     def _deploy_single_file(self, src, dest):
         dest.parent.mkdir(parents=True, exist_ok=True)
 
@@ -116,6 +144,20 @@ class Deployer(object):
 
         dest.symlink_to(src)
         logger.debug("--> linking complete")
+
+    def _binary_exists(self, name):
+        pathvar = os.environ["PATH"]
+        for component in pathvar.split(":"):
+            if not component:
+                continue
+            loc = Path(component)
+            if not loc.is_dir():
+                continue
+
+            if loc / name:
+                return True
+
+        return False
 
 
 if __name__ == "__main__":
