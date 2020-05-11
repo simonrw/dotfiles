ba705adfe5a804e0eaff9a825129f87a96c729d2
Simon Walker <s.r.walker101@googlemail.com>
2020-05-11 23:00:27 +0100

    Stop using unittest for tests

--- testing/test_deploy.py
+++ testing/test_deploy.py
@@ -5,54 +5,54 @@ import deploy
 from unittest import mock
 
 
-class TestDeployStandardDir(object):
-    def test_creates_symlink(self, tmp_path):
-        args = self.create_args()
-        deployer = deploy.Deployer(args)
+def test_creates_symlink(tmp_path):
+    args = create_args()
+    deployer = deploy.Deployer(args)
 
+    deployer.deploy_standard_dir("vim", install_path=tmp_path)
+
+    assert tmp_path.joinpath(".vim").exists()
+
+
+def test_does_not_overwrite(tmp_path):
+    tmp_path.joinpath(".vim").touch()
+    args = create_args()
+    deployer = deploy.Deployer(args)
+
+    with mock.patch("deploy.Path.symlink_to") as mock_symlink:
         deployer.deploy_standard_dir("vim", install_path=tmp_path)
 
-        assert tmp_path.joinpath(".vim").exists()
+    mock_symlink.assert_not_called()
 
-    def test_does_not_overwrite(self, tmp_path):
-        tmp_path.joinpath(".vim").touch()
-        args = self.create_args()
-        deployer = deploy.Deployer(args)
 
-        with mock.patch("deploy.Path.symlink_to") as mock_symlink:
-            deployer.deploy_standard_dir("vim", install_path=tmp_path)
+def test_overwrites_with_force(tmp_path):
+    tmp_path.joinpath(".vim").touch()
+    args = create_args(force=True)
+    deployer = deploy.Deployer(args)
 
-        mock_symlink.assert_not_called()
+    with mock.patch("deploy.Path.symlink_to") as mock_symlink:
+        deployer.deploy_standard_dir("vim", install_path=tmp_path)
 
-    def test_overwrites_with_force(self, tmp_path):
-        tmp_path.joinpath(".vim").touch()
-        args = self.create_args(force=True)
-        deployer = deploy.Deployer(args)
+    mock_symlink.assert_called_once()
 
-        with mock.patch("deploy.Path.symlink_to") as mock_symlink:
-            deployer.deploy_standard_dir("vim", install_path=tmp_path)
 
-        mock_symlink.assert_called_once()
+# helpers
 
-    # helpers
 
-    def create_args(self, **kwargs):
-        """Returns default arguments for usage.
+def create_args(**kwargs):
+    """Returns default arguments for usage.
 
-        Can have overrides.
-        """
+    Can have overrides.
+    """
 
-        args = mock.Mock()
+    args = mock.Mock()
 
-        defaults = {
-                "dry_run": False,
-                "force": False,
-                }
+    defaults = {"dry_run": False, "force": False}
 
-        for key in defaults:
-            setattr(args, key, defaults[key])
+    for key in defaults:
+        setattr(args, key, defaults[key])
 
-        for key in kwargs:
-            setattr(args, key, kwargs[key])
+    for key in kwargs:
+        setattr(args, key, kwargs[key])
 
-        return args
+    return args
