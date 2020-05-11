4eb979f55a757c969525398033a6428d22081fbc
Simon Walker <s.r.walker101@googlemail.com>
2020-05-11 09:56:41 +0100

    Updates tests to new API

--- testing/test_deploy.py
+++ testing/test_deploy.py
@@ -7,7 +7,8 @@ from unittest import mock
 
 class TestDeployStandardDir(object):
     def test_creates_symlink(self, tmp_path):
-        deployer = deploy.Deployer(dry_run=False, force=False)
+        args = self.create_args()
+        deployer = deploy.Deployer(args)
 
         deployer.deploy_standard_dir("vim", install_path=tmp_path)
 
@@ -15,7 +16,8 @@ class TestDeployStandardDir(object):
 
     def test_does_not_overwrite(self, tmp_path):
         tmp_path.joinpath(".vim").touch()
-        deployer = deploy.Deployer(dry_run=False, force=False)
+        args = self.create_args()
+        deployer = deploy.Deployer(args)
 
         with mock.patch("deploy.Path.symlink_to") as mock_symlink:
             deployer.deploy_standard_dir("vim", install_path=tmp_path)
@@ -24,9 +26,33 @@ class TestDeployStandardDir(object):
 
     def test_overwrites_with_force(self, tmp_path):
         tmp_path.joinpath(".vim").touch()
-        deployer = deploy.Deployer(dry_run=False, force=True)
+        args = self.create_args(force=True)
+        deployer = deploy.Deployer(args)
 
         with mock.patch("deploy.Path.symlink_to") as mock_symlink:
             deployer.deploy_standard_dir("vim", install_path=tmp_path)
 
         mock_symlink.assert_called_once()
+
+    # helpers
+
+    def create_args(self, **kwargs):
+        """Returns default arguments for usage.
+
+        Can have overrides.
+        """
+
+        args = mock.Mock()
+
+        defaults = {
+                "dry_run": False,
+                "force": False,
+                }
+
+        for key in defaults:
+            setattr(args, key, defaults[key])
+
+        for key in kwargs:
+            setattr(args, key, kwargs[key])
+
+        return args
