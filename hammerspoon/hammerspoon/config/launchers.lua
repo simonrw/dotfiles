8576d1ec7d70df81b08236bdab4f153d3ca7d18b
Simon Walker <s.r.walker101@googlemail.com>
2020-05-21 09:40:02 +0100

    Install dash

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -3,6 +3,7 @@ browser = "Firefox"
 email = "Mail"
 chat = "Slack"
 music = "Spotify"
+documentation = "Dash"
 
 hs.hotkey.bind({'cmd', 'alt'}, 'c', function()
     hs.application.launchOrFocus(browser)
@@ -24,3 +25,6 @@ hs.hotkey.bind({'cmd', 'alt'}, 'e', function()
     hs.application.launchOrFocus(email)
 end)
 
+hs.hotkey.bind({'cmd', 'alt'}, 'd', function()
+    hs.application.launchOrFocus(documentation)
+end)
