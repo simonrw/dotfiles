b698317345d1928ba346ee3bebc3f0cb286b5c46
Simon Walker <s.r.walker101@googlemail.com>
2020-06-17 18:28:40 +0100

    Use firefox

--- hammerspoon/hammerspoon/config/applications.lua
+++ hammerspoon/hammerspoon/config/applications.lua
@@ -8,9 +8,3 @@ applications = {
     notes = "Notable",
     editor = "Visual Studio Code",
 }
-
--- overrides
-
-if string.find(hs.host.names()[1], "pixmac516") then
-    applications.browser = "Google Chrome"
-end
