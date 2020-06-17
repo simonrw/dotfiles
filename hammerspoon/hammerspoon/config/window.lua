ed4782d8d3158fe1a4cab811e3f2cf63d40b24be
Simon Walker <s.r.walker101@googlemail.com>
2020-06-17 08:50:22 +0100

    Remove border from fullscreen (terminal) windows

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -1,6 +1,6 @@
 -- constant holding the window enlargement/shrinkage factor
 local WINDOW_SIZE_CHANGE = 0
-local FULLSCREEN_BORDER = 16
+local FULLSCREEN_BORDER = 0
 local WINDOW_BORDER = FULLSCREEN_BORDER
 local LEFTRIGHT_FRACTION = 0.5
 local ENABLE_SHORTCUTS = true
