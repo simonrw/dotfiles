1f2600639f5cdf3d18b0cb7c7d0ffb7d16b003a1
Simon Walker <s.r.walker101@googlemail.com>
2020-06-17 17:56:47 +0100

    updates window shortcuts

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -1,14 +1,9 @@
 require "config/applications"
 
 -- constant holding the window enlargement/shrinkage factor
-local WINDOW_SIZE_CHANGE = 0
 local FULLSCREEN_BORDER = 16
-local WINDOW_BORDER = FULLSCREEN_BORDER
-local LEFTRIGHT_FRACTION = 0.5
-local ENABLE_SHORTCUTS = true
 local ENABLE_FULLSCREEN_SHORTCUT = true
 local ENABLE_FULLSCREEN_FOR_APPS = { applications.terminal }
-local ENABLE_FULLSCREEN_SHORTCUT = true
 
 -- Move window to the next screen
 hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'o', function()
@@ -18,56 +13,26 @@ hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'o', function()
 end)
 
 
-if ENABLE_SHORTCUTS then
-    -- Move window to left two thirds
-    hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Left', function()
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x + WINDOW_BORDER / 2
-        f.y = max.y + WINDOW_BORDER / 2
-        f.w = LEFTRIGHT_FRACTION * max.w - WINDOW_BORDER
-        f.h = max.h - WINDOW_BORDER
-        win:setFrame(f)
-    end)
-
-    -- Move window to the right half
-    hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Right', function()
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x + LEFTRIGHT_FRACTION * max.w
-        f.y = max.y + WINDOW_BORDER / 2
-        f.w = (1 - LEFTRIGHT_FRACTION) * max.w - WINDOW_BORDER / 2.0
-        f.h = max.h - WINDOW_BORDER
-        win:setFrame(f)
-    end)
-
-    function maximizeWindow()
-        local app = hs.application.frontmostApplication()
-        for _, allowed_app in ipairs(ENABLE_FULLSCREEN_FOR_APPS) do
-            if app:title() ~= allowed_app then
-                return
-            end
+function maximizeWindow()
+    local app = hs.application.frontmostApplication()
+    for _, allowed_app in ipairs(ENABLE_FULLSCREEN_FOR_APPS) do
+        if app:title() ~= allowed_app then
+            return
         end
-
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x + FULLSCREEN_BORDER / 2
-        f.y = max.y + FULLSCREEN_BORDER / 2
-        f.w = max.w - FULLSCREEN_BORDER
-        f.h = max.h - FULLSCREEN_BORDER
-        win:setFrame(f)
     end
 
-    if ENABLE_FULLSCREEN_SHORTCUT then
-        hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'f', maximizeWindow)
-    end
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + FULLSCREEN_BORDER / 2
+    f.y = max.y + FULLSCREEN_BORDER / 2
+    f.w = max.w - FULLSCREEN_BORDER
+    f.h = max.h - FULLSCREEN_BORDER
+    win:setFrame(f)
+end
+
+if ENABLE_FULLSCREEN_SHORTCUT then
+    hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'f', maximizeWindow)
 end
