5b7381cf374045b4d389b8f58e8efaf3ce0e5574
Simon Walker <s.r.walker101@googlemail.com>
2020-06-18 09:08:09 +0100

    Support host overrides for applications

--- hammerspoon/hammerspoon/config/applications.lua
+++ hammerspoon/hammerspoon/config/applications.lua
@@ -8,3 +8,11 @@ applications = {
     notes = "Notable",
     editor = "Visual Studio Code",
 }
+
+-- overrides
+
+for _, hostname in ipairs(hs.host.names()) do
+    if string.find(hostname, "pixmac516") then
+        -- host overrides
+    end
+end
