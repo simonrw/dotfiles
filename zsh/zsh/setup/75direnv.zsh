80c2414b94d147f51988a07c5cdf975140181f00
Simon Walker <s.r.walker101@googlemail.com>
2020-06-22 12:08:04 +0100

    Do not show virtualenv prompt

--- zsh/zsh/setup/75direnv.zsh
+++ zsh/zsh/setup/75direnv.zsh
@@ -3,10 +3,3 @@ if ! has_executable direnv; then
 else
     eval "$(direnv hook zsh)"
 fi
-
-show_virtual_env() {
-  if [[ -n "$VIRTUAL_ENV" && -n "$DIRENV_DIR" ]]; then
-    echo "($(basename $VIRTUAL_ENV))"
-  fi
-}
-PS1='$(show_virtual_env)'$PS1
