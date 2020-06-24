f4529be930c566db71eb6f3ba5c1c3f4e782c276
Simon Walker <s.r.walker101@googlemail.com>
2020-06-24 19:52:51 +0100

    Includes left/right shortcuts back

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -4,12 +4,42 @@ require "config/applications"
 local FULLSCREEN_BORDER = 16
 local ENABLE_FULLSCREEN_SHORTCUT = true
 local ENABLE_FULLSCREEN_FOR_APPS = {}
+local WINDOW_BORDER = FULLSCREEN_BORDER
+local LEFTRIGHT_FRACTION = 0.5
 
 -- Move window to the next screen
 hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'o', function()
     local win = hs.window.focusedWindow()
     local nextScreen = win:screen():next()
     win:moveToScreen(nextScreen)
+end)
+
+-- Move window to left two thirds
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Left', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + WINDOW_BORDER / 2
+    f.y = max.y + WINDOW_BORDER / 2
+    f.w = LEFTRIGHT_FRACTION * max.w - WINDOW_BORDER
+    f.h = max.h - WINDOW_BORDER
+    win:setFrame(f)
+end)
+
+-- Move window to the right half
+hs.hotkey.bind({'cmd', 'alt', 'ctrl'}, 'Right', function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x + LEFTRIGHT_FRACTION * max.w
+    f.y = max.y + WINDOW_BORDER / 2
+    f.w = (1 - LEFTRIGHT_FRACTION) * max.w - WINDOW_BORDER / 2.0
+    f.h = max.h - WINDOW_BORDER
+    win:setFrame(f)
 end)
 
 
