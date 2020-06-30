17cb44eab42ccc2d4dc591a885a5b21beef4cc35
Simon Walker <s.r.walker101@googlemail.com>
2020-06-30 09:35:04 +0100

    Use chrome on pixmac

--- hammerspoon/hammerspoon/config/applications.lua
+++ hammerspoon/hammerspoon/config/applications.lua
@@ -14,5 +14,6 @@ applications = {
 for _, hostname in ipairs(hs.host.names()) do
     if string.find(hostname, "pixmac516") then
         -- host overrides
+        applications.browser = "Google Chrome"
     end
 end
