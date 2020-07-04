3041f9a81657d551d18857d602caec8f70c5df95
Simon Walker <s.r.walker101@googlemail.com>
2020-07-04 23:23:39 +0100

    Test both cases of backoff

--- testing/test_yt_download_playlist.py
+++ testing/test_yt_download_playlist.py
@@ -5,17 +5,21 @@ import pytest
 from yt_download_playlist import ExponentialBackoff
 
 
+class BadRetry:
+    def __init__(self, raise_error):
+        self.calls = 0
+        self.raise_error = raise_error
+
+    def __call__(self):
+        self.calls += 1
+        if self.raise_error:
+            raise ValueError("bad")
+
+
 class TestBackoff:
     def test_retries(self):
-        class BadRetry:
-            def __init__(self):
-                self.calls = 0
 
-            def __call__(self):
-                self.calls += 1
-                raise ValueError("bad")
-
-        r = BadRetry()
+        r = BadRetry(raise_error=True)
         backoff = ExponentialBackoff(r)
 
         # reduce the factor
@@ -25,3 +29,14 @@ class TestBackoff:
             backoff.run()
 
         assert r.calls == 6
+
+    def test_ok_case(self):
+        r = BadRetry(raise_error=False)
+        backoff = ExponentialBackoff(r)
+
+        # reduce the factor
+        backoff._sleep_time = 0.001
+
+        backoff.run()
+
+        assert r.calls == 1
