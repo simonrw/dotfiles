f2a3c250bcb3fa00a74741e5c13c851bae832a12
Simon Walker <s.r.walker101@googlemail.com>
2020-07-15 08:39:40 +0100

    Allow fullscreen (with borders) for terminal

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -2,7 +2,7 @@ require "config/applications"
 
 -- constant holding the window enlargement/shrinkage factor
 local FULLSCREEN_BORDER = 16
-local ENABLE_FULLSCREEN_SHORTCUT = false
+local ENABLE_FULLSCREEN_SHORTCUT = true
 local ENABLE_FULLSCREEN_FOR_APPS = {}
 local WINDOW_BORDER = FULLSCREEN_BORDER
 local LEFTRIGHT_FRACTION = 0.5
