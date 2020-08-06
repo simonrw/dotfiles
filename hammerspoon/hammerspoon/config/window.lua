3a5a27485c50d5fe184c912bb8c6f49f674d9ba6
Simon Walker <s.r.walker101@googlemail.com>
2020-08-06 13:53:53 +0100

    Add window borders back

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -1,7 +1,7 @@
 require "config/applications"
 
 -- constant holding the window enlargement/shrinkage factor
-local FULLSCREEN_BORDER = 0
+local FULLSCREEN_BORDER = 16
 local ENABLE_FULLSCREEN_SHORTCUT = true
 local ENABLE_FULLSCREEN_FOR_APPS = {}
 local WINDOW_BORDER = FULLSCREEN_BORDER
