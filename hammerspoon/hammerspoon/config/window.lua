cfe1437bdb62f68c75bd6d0419d093e2e9c54f4c
Simon Walker <s.r.walker101@googlemail.com>
2020-09-07 15:13:03 +0100

    Reenable fullscreen windows

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -2,7 +2,7 @@ require "config/applications"
 
 -- constant holding the window enlargement/shrinkage factor
 local FULLSCREEN_BORDER = 16
-local ENABLE_FULLSCREEN_SHORTCUT = False
+local ENABLE_FULLSCREEN_SHORTCUT = true
 local WINDOW_BORDER = FULLSCREEN_BORDER
 local LEFTRIGHT_FRACTION = 0.5
 local TERMINAL_NORMAL_SIZE = {1024, 768}
