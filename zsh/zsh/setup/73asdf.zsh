6fe10f1562552590c5eba9ee7d6fbccc84b6e58e
Simon Walker <s.r.walker101@googlemail.com>
2020-09-25 14:54:56 +0100

    Add asdf completion

--- zsh/zsh/setup/73asdf.zsh
+++ zsh/zsh/setup/73asdf.zsh
@@ -3,3 +3,9 @@ if test -f ${HOME}/.asdf/asdf.sh; then
 else
     muted_print "asdf not installed"
 fi
+
+# append completions to fpath
+fpath=(${ASDF_DIR}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit
+compinit
