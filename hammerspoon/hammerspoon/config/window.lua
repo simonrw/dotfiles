9398e0bfb133964b1a9231a2fef0552344b33294
Simon Walker <s.r.walker101@googlemail.com>
2020-10-05 13:45:45 +0100

    No window borders

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -1,5 +1,5 @@
 -- constant holding the window enlargement/shrinkage factor
-local FULLSCREEN_BORDER = 16
+local FULLSCREEN_BORDER = 0
 local ENABLE_FULLSCREEN_SHORTCUT = true
 local WINDOW_BORDER = FULLSCREEN_BORDER
 local LEFTRIGHT_FRACTION = 0.5
