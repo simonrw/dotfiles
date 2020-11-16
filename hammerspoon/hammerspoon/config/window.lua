5f2ff4b94cea4fafa063fd229ea77cdfe2a222ae
Simon Walker <s.r.walker101@googlemail.com>
2020-11-16 08:45:22 +0000

    Use firefox and disable fullscreen shortcut

--- hammerspoon/hammerspoon/config/window.lua
+++ hammerspoon/hammerspoon/config/window.lua
@@ -1,6 +1,6 @@
 -- constant holding the window enlargement/shrinkage factor
 local FULLSCREEN_BORDER = 0
-local ENABLE_FULLSCREEN_SHORTCUT = true
+local ENABLE_FULLSCREEN_SHORTCUT = false
 local WINDOW_BORDER = 0
 local LEFTRIGHT_FRACTION = 0.5
 local TERMINAL_NORMAL_SIZE = {1024, 768}
