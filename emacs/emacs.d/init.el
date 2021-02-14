f3df56a76be8bf79b3a4de9b15729a1170bcd8fa
Simon Walker <s.r.walker101@googlemail.com>
2021-02-14 14:53:03 +0000

    Use light theme

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -241,31 +241,11 @@
          :map minibuffer-local-map
          ("C-r" . 'counsel-minibuffer-history)))
 
-
-(use-package doom-themes
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-dracula t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-  (doom-themes-treemacs-config)
-
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
-
 (use-package ivy
   :config
   (ivy-mode))
 
+(load-theme 'whiteboard t)
 
 (provide 'init)
 ;;; init.el ends here
