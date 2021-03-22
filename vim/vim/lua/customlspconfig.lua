5f726a565f04039277153e583594083d99841a13
Simon Walker <s.r.walker101@googlemail.com>
2021-03-22 13:52:15 +0000

    Updates vim lsp hover command

--- vim/vim/lua/customlspconfig.lua
+++ vim/vim/lua/customlspconfig.lua
@@ -41,7 +41,7 @@ local on_attach = function(client, bufnr)
     local opts = { noremap=true, silent=true }
 
     buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "<leader>H", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    buf_set_keymap("n", "<leader>d", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
     buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
     buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
     buf_set_keymap("n", "]g", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
