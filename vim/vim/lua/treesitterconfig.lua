dcce3305c1b311ec4eef7dab4086cfc538993eec
Simon Walker <s.r.walker101@googlemail.com>
2021-04-16 09:46:23 +0100

    Includes treesitter

--- vim/vim/lua/treesitterconfig.lua
+++ vim/vim/lua/treesitterconfig.lua
@@ -1,7 +1,25 @@
 local ts = require('nvim-treesitter.configs')
 
 ts.setup {
-    ensure_installed = {'python', 'rust', 'lua', 'zig', 'go', 'c', 'html', 'javascript', 'typescript', 'toml'},
+    ensure_installed = {
+        'bash',
+        'c',
+        'go',
+        'graphql',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+        'php',
+        'python',
+        'rust',
+        'svelte',
+        'toml',
+        'tsx',
+        'typescript',
+        'yaml',
+        'zig',
+    },
     highlight = {
         enable = true
     },
