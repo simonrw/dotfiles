36a650ca9b06719e0b1573961e2b094c0fa00ad7
Simon Walker <s.r.walker101@googlemail.com>
2021-05-16 01:58:03 +0100

    set up direnv to be managed by asdf

--- zsh/zsh/setup/75direnv.zsh
+++ zsh/zsh/setup/75direnv.zsh
@@ -1,5 +1,9 @@
 if ! has_executable direnv; then
     muted_print "direnv not installed"
 else
-    eval "$(direnv hook zsh)"
+    eval "$(asdf exec direnv hook zsh)"
 fi
+
+direnv() {
+    asdf exec direnv "$@"
+}
