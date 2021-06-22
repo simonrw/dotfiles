ad538e4181ba54779a30a237a4e6cc684ff4ec83
Simon Walker <s.r.walker101@googlemail.com>
2021-06-22 13:42:42 +0100

    Adds gs alias
    Instead of ghostscript, alias to something useful.

--- zsh/zsh/setup/20aliases.zsh
+++ zsh/zsh/setup/20aliases.zsh
@@ -8,6 +8,7 @@ alias pydoc='=python -m pydoc'
 alias pylab='ipython --pylab'
 alias clear-pycs='find ${PWD} -name "*.pyc" -delete'
 alias g='git'
+alias gs="git status"
 alias ga='git commit --amend'
 alias es='exec $SHELL'
 alias sqlite3='rlwrap =sqlite3'
