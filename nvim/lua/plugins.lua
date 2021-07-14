c3a33df3e5d264eb32277aa415117b30c6e0828b
Simon Walker <s.r.walker101@googlemail.com>
2021-07-14 22:37:39 +0100

    Makes lots of plugins lazy loaded

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -7,7 +7,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
     execute('packadd packer.nvim')
 end
 
-return require('packer').startup(function(use)
+return require('packer').startup({function(use)
     use 'wbthomason/packer.nvim'
     use 'lotabout/skim'
     use 'lotabout/skim.vim'
@@ -21,25 +21,25 @@ return require('packer').startup(function(use)
     use 'tpope/vim-repeat'
     use 'christoomey/vim-tmux-runner'
     use 'christoomey/vim-conflicted'
-    use 'vim-test/vim-test'
+    use { 'vim-test/vim-test', cmd = {'TestSuite', 'TestFile', 'TestLast', 'TestNearest'} }
     use 'junegunn/vim-easy-align'
     use { 'junegunn/goyo.vim', cmd = {'Goyo'} }
     use 'shumphrey/fugitive-gitlab.vim'
     use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install', ft = {'markdown'} }
+    use 'airblade/vim-gitgutter' 
 
     -- language plugins
     use { 'cespare/vim-toml', ft = {'toml'} }
-    use 'jeetsukumaran/vim-pythonsense'
-    use 'evanleck/vim-svelte'
+    use { 'jeetsukumaran/vim-pythonsense', ft = {'python'} }
+    use { 'evanleck/vim-svelte', ft = {'svelte'} }
     use { 'ElmCast/elm-vim', ft = {'elm'} }
     use { 'leafgarland/typescript-vim', ft = {'typescript'} }
-    use 'hashivim/vim-terraform'
+    use { 'hashivim/vim-terraform', ft = {'terraform'} }
     use { 'ziglang/zig.vim', ft = {'zig'} }
-    use 'jparise/vim-graphql'
-    use 'jjo/vim-cue'
-    use 'vmchale/dhall-vim'
-    use 'pest-parser/pest.vim'
-    use 'airblade/vim-gitgutter'
+    use { 'jparise/vim-graphql', ft = {'graphql'} }
+    use { 'jjo/vim-cue', ft = {'cue'} }
+    use { 'vmchale/dhall-vim', ft = {'dhall'} }
+    use { 'pest-parser/pest.vim', ft = {'pest'} }
 
     local completion_framework = vim.api.nvim_get_var("completion_framework")
     if completion_framework == "coc" then
@@ -47,18 +47,19 @@ return require('packer').startup(function(use)
         use { 'rodrigore/coc-tailwind-intellisense', run = 'npm install' }
     elseif completion_framework == 'ale' then
         use 'dense-analysis/ale'
-        use 'fatih/vim-go'
+        use { 'fatih/vim-go', ft = {'go'} }
     elseif completion_framework == 'nvim' then
         use 'neovim/nvim-lspconfig'
         use 'hrsh7th/nvim-compe'
         use 'RishabhRD/popfix'
         use 'RishabhRD/nvim-lsputils'
         use 'nvim-lua/lsp-status.nvim'
-        use { 'psf/black', branch = 'stable' }
+        use { 'psf/black', branch = 'stable', ft = {'python'} }
     end
 
     if vim.api.nvim_get_var("include_treesitter") == 1 then
         use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
     end
-end)
+end,
+config = {}})
 
