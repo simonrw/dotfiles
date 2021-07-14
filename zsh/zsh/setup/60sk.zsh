cfdb48626c2159a485261bf66d337dc6d8a27873
Simon Walker <s.r.walker101@googlemail.com>
2021-07-14 17:35:22 +0100

    Use system managed skim

--- zsh/zsh/setup/60sk.zsh
+++ zsh/zsh/setup/60sk.zsh
@@ -1,9 +1,6 @@
-[ -f ${HOME}/.skim/bin/sk ] && export PATH=${HOME}/.skim/bin:${PATH}
-
 # Set up skim
 export SKIM_DEFAULT_OPTIONS="--tiebreak begin --color dark,matched_bg:-1 --ansi --no-mouse --tabstop 4 --inline-info"
 export SKIM_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,venv,node_modules}/*" 2> /dev/null'
-export MANPATH=${MANPATH}:${HOME}/.skim/man
 
 
 fs() {
