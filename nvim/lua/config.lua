9f6fc7885589082fbe31baa4fb50afaf864be9af
Simon Walker <s.r.walker101@googlemail.com>
2021-07-16 09:38:55 +0100

    Adds fugitive configuration

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -1,6 +1,9 @@
 require('settings')
 require('plugins')
 require('mappings')
+
+-- plugins
 require('fzf')
 require('vim-test')
 require('vtr')
+require('fugitive')
---
+++ nvim/lua/fugitive.lua
@@ -0,0 +1,8 @@
+nnoremap('<leader>gc', ':Git commit -v<cr>')
+nnoremap('<leader>gd', ':Gdiff<cr>')
+nnoremap('<leader>gw', ':Gwrite<cr>')
+nnoremap('<leader>gr', ':Gread<cr>')
+nnoremap('gs', ':Git<cr>')
+nnoremap('<leader>ga', ':Git commit -v --amend<cr>')
+
+
