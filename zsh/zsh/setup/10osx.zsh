39debb224cc147bfb9a2723e0ecebb0846259d4d
Simon Walker <s.r.walker101@googlemail.com>
2021-07-22 09:33:46 +0100

    Clean up path variables

--- zsh/zsh/setup/10osx.zsh
+++ zsh/zsh/setup/10osx.zsh
@@ -1,14 +1,14 @@
 # OSX setup
 case $OSTYPE in
     darwin*)
-        export PATH=/Library/TeX/texbin:${GOPATH}/bin:/usr/local/opt/coreutils/libexec/gnubin:/usr/local/sbin:/sbin:/usr/sbin:${HOME}/.cabal/bin:/usr/local/opt/python/libexec/bin:${PATH}
-
-        alias curl=/usr/local/opt/curl/bin/curl
+        export PATH=/usr/local/opt/coreutils/libexec/gnubin:/sbin:/usr/sbin:/usr/local/opt/curl/bin:${PATH}
 
         # Configure homebrew
         export HOMEBREW_NO_ANALYTICS=1
         # Only update every week
         export HOMEBREW_AUTO_UPDATE_SECS=604800
+
+        eval $(brew shellenv)
 
         export DYLD_LIBRARY_PATH=${BUILD_PREFIX}/lib:${DYLD_LIBRARY_PATH}
 
--- zsh/zsh/setup/74android.zsh
+++ zsh/zsh/setup/74android.zsh
@@ -1,5 +1,7 @@
-export ANDROID_HOME=$HOME/Library/Android/sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+setup-android() {
+    export ANDROID_HOME=$HOME/Library/Android/sdk
+    export PATH=$PATH:$ANDROID_HOME/emulator
+    export PATH=$PATH:$ANDROID_HOME/tools
+    export PATH=$PATH:$ANDROID_HOME/tools/bin
+    export PATH=$PATH:$ANDROID_HOME/platform-tools
+}
