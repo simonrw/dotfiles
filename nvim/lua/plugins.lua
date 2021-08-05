06766e49a6e5c382e3f3a29cc44153ef76baf55d
Simon Walker <s.r.walker101@googlemail.com>
2021-08-05 23:08:07 +0100

    Updates plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -20,33 +20,25 @@ require('packer').startup({function(use)
     use 'tpope/vim-rhubarb'
     use 'tpope/vim-repeat'
     use 'christoomey/vim-tmux-runner'
-    use 'christoomey/vim-conflicted'
-    use 'vim-test/vim-test'
-    use 'junegunn/vim-easy-align'
+    use { 'christoomey/vim-conflicted', cmd = {'Conflicted'} }
+    use { 'vim-test/vim-test', cmd = {'TestSuite', 'TestNearest', 'TestFile', 'TestLast', 'TestVisit' } }
+    use { 'junegunn/vim-easy-align', cmd = {'EasyAlign'} }
     use 'shumphrey/fugitive-gitlab.vim'
     use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
     use 'itchyny/lightline.vim'
     use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 
     -- language plugins
-    use 'cespare/vim-toml'
     use 'jeetsukumaran/vim-pythonsense'
     use 'evanleck/vim-svelte'
-    use 'ElmCast/elm-vim'
-    use 'leafgarland/typescript-vim'
     use 'hashivim/vim-terraform'
-    use 'ziglang/zig.vim'
-    use 'jparise/vim-graphql'
-    use 'jjo/vim-cue'
-    use 'vmchale/dhall-vim'
-    use 'pest-parser/pest.vim'
     use 'lepture/vim-velocity'
 
     if vim.g.completion_framework == "coc" then
         use { 'neoclide/coc.nvim', branch = 'release' }
     elseif vim.g.completion_framework == 'ale' then
         use 'dense-analysis/ale'
-        use { 'fatih/vim-go', ft = {'go'} }
+        -- use { 'fatih/vim-go', ft = {'go'} }
     elseif vim.g.completion_framework == 'nvim' then
         use 'neovim/nvim-lspconfig'
         use 'hrsh7th/nvim-compe'
