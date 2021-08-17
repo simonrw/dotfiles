071e20593146b136e775e38f23b0cbd0fc88f22d
Simon Walker <s.r.walker101@googlemail.com>
2021-08-17 12:52:26 +0100

    Fix lua completion
    No longer use snippets, and increase the minimum number of characters to
    type before completion kicks in.
    
    Also reduce the number of completion sources

--- nvim/lua/customlspconfig.lua
+++ nvim/lua/customlspconfig.lua
@@ -12,7 +12,7 @@ compe.setup {
     enabled = true;
     autocomplete = true;
     debug = false;
-    min_length = 1;
+    min_length = 3;
     preselect = 'enable';
     throttle_time = 80;
     source_timeout = 200;
@@ -26,13 +26,13 @@ compe.setup {
         path = true;
         buffer = true;
         calc = true;
-        vsnip = true;
+        vsnip = false;
         nvim_lsp = true;
         nvim_lua = true;
-        spell = true;
+        spell = false;
         tags = true;
-        snippets_nvim = true;
-        treesitter = true;
+        snippets_nvim = false;
+        treesitter = false;
     };
 }
 
@@ -66,22 +66,35 @@ local on_attach = function(client, bufnr)
     end
 end
 
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
 local t = function(str)
     return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
+
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+
 _G.s_tab_complete = function()
     if vim.fn.pumvisible() == 1 then
         return t "<C-p>"
-    elseif vim.fn.call("vsnip#jumpable", {-1}) == 1 then
-        return t "<Plug>(vsnip-jump-prev)"
     else
         return t "<S-Tab>"
     end
 end
 
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 -- lsp-utils
 vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
