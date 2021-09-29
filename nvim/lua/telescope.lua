a66c534ad492b315856e7f9a94c67039ac79c844
Simon Walker <s.r.walker101@googlemail.com>
2021-09-29 17:23:41 +0100

    Adds previews for buffers

--- nvim/lua/telescope.lua
+++ nvim/lua/telescope.lua
@@ -1,5 +1,5 @@
 nnoremap('<leader>f', [[<cmd>lua require('telescope.builtin').git_files({previewer = false})<Cr>]])
 nnoremap('<leader>F', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<Cr>]])
-nnoremap('gb', [[<cmd>lua require('telescope.builtin').buffers({previewer = false})<Cr>]])
+nnoremap('gb', [[<cmd>lua require('telescope.builtin').buffers()<Cr>]])
 nnoremap('gl', [[<cmd>lua require('telescope.builtin').live_grep()<Cr>]])
 nnoremap('<leader>gT', [[<cmd>lua require('telescope.builtin').tags()<Cr>]])
