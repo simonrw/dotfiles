13fa7640cc04fd08c0ffcd05ca87861ca9530c01
Simon Walker <s.r.walker101@googlemail.com>
2021-12-14 11:19:55 +0000

    Use hammerspoon to manage default browser

--- hammerspoon/hammerspoon/config/launchers.lua
+++ hammerspoon/hammerspoon/config/launchers.lua
@@ -2,7 +2,7 @@ local applications = require("config/applications")
 
 -- helper function to bind multiple keys to a single application
 local seen_hotkeys = {}
-function bindKey(application, ...)
+local function bindKey(application, ...)
     local arg = {...}
     for _, k in ipairs(arg) do
         -- check if the key has been bound already
@@ -28,3 +28,40 @@ bindKey(applications.notes, 'e')
 bindKey(applications.todo, 'n')
 bindKey(applications.chat, 's')
 bindKey(applications.music, 'm')
+
+local BundleCache = {}
+BundleCache.__index = BundleCache
+
+function BundleCache:new()
+    local props = {}
+    setmetatable(props, BundleCache)
+    props.bundles = {}
+    return props
+end
+
+function BundleCache:get(name)
+    if self.bundles[name] == nil then
+        local app = hs.application.get(name)
+        if app then
+            self.bundles[name] = app:bundleID()
+        end
+    end
+
+    local bundleId = self.bundles[name]
+    if bundleId then
+        return bundleId
+    else
+        hs.alert.show("Could not find bundle for application: " .. name)
+    end
+end
+
+local bc = BundleCache:new()
+
+-- set default browser
+local function handleBrowse(scheme, host, params, fullURL, senderPID)
+    local bundleId = bc:get(applications.browser.name)
+    hs.urlevent.openURLWithBundle(fullURL, bundleId)
+end
+
+hs.application.enableSpotlightForNameSearches(true)
+hs.urlevent.httpCallback = handleBrowse
