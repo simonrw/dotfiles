912f4816af2b3f49cfcb68bcc4a2de24b087c91c
Simon Walker <s.r.walker101@googlemail.com>
2022-01-12 12:03:14 +0000

    Use lsp-installer for managing paths to lsp servers

--- nvim/lua/customlspconfig.lua
+++ nvim/lua/customlspconfig.lua
@@ -1,65 +1,49 @@
+local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- mappings
+    local opts = { noremap=true, silent=true }
+
+    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+    buf_set_keymap("n", "<leader>d", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+    buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+    buf_set_keymap("n", "td", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+    buf_set_keymap("n", "]g", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+    buf_set_keymap("n", "[g", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+    buf_set_keymap("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
+
+    buf_set_keymap("n", "<C-Space>", [[<Plug>(completion_trigger)]], opts)
+
+    vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
+end
+
 local function setup()
     local lspconfig = require("lspconfig")
+    local lsp_installer = require("nvim-lsp-installer")
 
-    local on_attach = function(client, bufnr)
-        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    lsp_installer.on_server_ready(function(server)
+        local opts = {
+            on_attach = on_attach,
+            flags = {
+                debounce_text_changes = 150,
+            },
+        }
 
-        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+        vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+            vim.lsp.diagnostic.on_publish_diagnostics, {
+                virtual_text = true,
+                signs = true,
+                update_in_insert = true,
+            }
+        )
 
-        -- mappings
-        local opts = { noremap=true, silent=true }
-
-        buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-        buf_set_keymap("n", "<leader>d", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-        buf_set_keymap("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-        buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-        buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-        buf_set_keymap("n", "td", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-        buf_set_keymap("n", "]g", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-        buf_set_keymap("n", "[g", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-        buf_set_keymap("n", "<leader>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-
-        buf_set_keymap("n", "<C-Space>", [[<Plug>(completion_trigger)]], opts)
-
-        vim.cmd("command! LspFormatting lua vim.lsp.buf.formatting()")
-    end
-
-    -- lspconfig
-    lspconfig.pyright.setup {
-        on_attach = on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        },
-        capabilities = capabilities,
-        cmd = { vim.fn.stdpath("data") .. '/lsp_servers/python/node_modules/.bin/pyright-langserver', '--stdio' },
-    }
-
-    lspconfig.gopls.setup {
-        on_attach = on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        },
-        capabilities = capabilities,
-        cmd = { vim.fn.stdpath("data") .. '/lsp_servers/go/gopls' },
-    }
-
-    lspconfig.rust_analyzer.setup {
-        on_attach = on_attach,
-        flags = {
-            debounce_text_changes = 150,
-        },
-        capabilities = capabilities,
-        cmd = { vim.fn.stdpath("data") .. '/lsp_servers/rust/rust-analyzer' },
-    }
-
-    vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-      vim.lsp.diagnostic.on_publish_diagnostics, {
-          virtual_text = true,
-          signs = true,
-          update_in_insert = true,
-      }
-    )
+        server:setup(opts)
+    end)
 end
 
 if vim.g.completion_framework == 'nvim' then
