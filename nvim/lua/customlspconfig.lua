ef1a1bab1e0c1a137828f4755a928248c682d415
Simon Walker <s.r.walker101@googlemail.com>
2022-01-12 20:35:42 +0000

    Adds terraform lsp

--- nvim/lua/customlspconfig.lua
+++ nvim/lua/customlspconfig.lua
@@ -1,4 +1,4 @@
-local target_servers = {"pyright", "gopls", "rust_analyzer", "yamlls"}
+local target_servers = {"pyright", "gopls", "rust_analyzer", "yamlls", "terraformls"}
 
 local on_attach = function(client, bufnr)
     local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
