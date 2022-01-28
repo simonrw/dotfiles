8ec3e3b6908fe1e8617038e9ff481ee38cc35e7d
Simon Walker <s.r.walker101@googlemail.com>
2022-01-28 14:02:02 +0000

    Updates pdm to use pipx installation

--- zsh/zsh/setup/80pdm.zsh
+++ zsh/zsh/setup/80pdm.zsh
@@ -1,5 +1,5 @@
 if [ -n "$PYTHONPATH" ]; then
-    export PYTHONPATH='/usr/local/opt/pdm/libexec/lib/python3.10/site-packages/pdm/pep582':$PYTHONPATH
+    export PYTHONPATH="${HOME}/.local/pipx/venvs/pdm/lib/python3.10/site-packages/pdm/pep582":$PYTHONPATH
 else
-    export PYTHONPATH='/usr/local/opt/pdm/libexec/lib/python3.10/site-packages/pdm/pep582'
+    export PYTHONPATH="${HOME}/.local/pipx/venvs/pdm/lib/python3.10/site-packages/pdm/pep582"
 fi
