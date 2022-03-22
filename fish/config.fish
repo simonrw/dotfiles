71772465963e181f96403a43116ce1f048333e35
Simon Walker <s.r.walker101@googlemail.com>
2022-03-22 14:35:29 +0000

    Add per-host fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,8 @@ end
 
 # this needs to be set for tmux to pick up
 set -x SHELL (command -v fish)
+
+# host-specific configuration
+set host_config ~/.config/fish/conf.d/per-host/config.(hostname).fish
+test -r $host_config; and source $host_config
+set -e host_config
