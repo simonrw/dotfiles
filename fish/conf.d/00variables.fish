aa7d940c53c6a7b556eb9b7ea40e5d85d621842f
Simon Walker <s.r.walker101@googlemail.com>
2022-04-14 09:47:55 +0100

    Configure pytest

--- fish/conf.d/00variables.fish
+++ fish/conf.d/00variables.fish
@@ -3,3 +3,4 @@ set -x PATH {$BUILD_PREFIX}/bin {$HOME}/.bin {$HOME}/.poetry/bin /usr/local/bin 
 set -x GOPATH {$HOME}/dev/gocode
 set -x EDITOR nvim
 set -x REVIEW_BASE main
+set -x PYTEST_ADDOPTS "-p no:sugar"
