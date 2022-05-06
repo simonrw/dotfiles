dfa611faa34d812fec539455314e4859eb476885
Simon Walker <s.r.walker101@googlemail.com>
2022-05-06 15:24:38 +0100

    Revert "Remove treesitter context"
    This reverts commit 6440f1efb1a24e123de634bd32ef7365544417f0.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -88,6 +88,7 @@ require('packer').startup({function(use)
 
     if vim.g.include_treesitter == 1 then
         use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+        use 'romgrk/nvim-treesitter-context'
         use 'nvim-treesitter/playground'
         use 'nvim-treesitter/nvim-treesitter-textobjects'
     end
