0522f6eaafac21b47aa8b983445fbfe8328c3651
Simon Walker <s.r.walker101@googlemail.com>
2022-06-14 20:59:01 +0100

    Use global status

--- nvim/lua/lualineconfig.lua
+++ nvim/lua/lualineconfig.lua
@@ -23,7 +23,7 @@ lualine.setup({
         section_separators = { left = '', right = ''},
         disabled_filetypes = {},
         always_divide_middle = true,
-        globalstatus = false,
+        globalstatus = true,
     },
     sections = {
         lualine_a = {'mode'},
