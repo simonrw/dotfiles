b25805a87e7350f87db85c10d956a352e410d6d9
Simon Walker <s.r.walker101@googlemail.com>
2022-06-29 22:35:40 +0100

    wezterm: update configuration to use version from x2x laptop

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,31 +1,41 @@
 local wezterm = require("wezterm")
 
 return {
+    color_scheme = "Lucius",
     font = wezterm.font("Source Code Pro"),
+    automatically_reload_config = true,
+    freetype_load_target = "HorizontalLcd",
+    freetype_load_flags = "DEFAULT",
     enable_tab_bar = false,
-    font_size = 14.0,
-    colors = {
-        foreground = "#ffffff",
-        background = "181818",
-        ansi = {
-            '#616261',
-            '#fd8272',
-            '#b4fa73',
-            '#fefcc3',
-            '#a5d5fe',
-            '#fd8ffd',
-            '#d0d1fe',
-            '#f1f0f2',
-        },
-        brights = {
-            '#8d8e8d',
-            '#fec4bd',
-            '#d6fcb9',
-            '#fefdd5',
-            '#c1e3fe',
-            '#fdb1fe',
-            '#e5e6fe',
-            '#fefffe',
+
+    color_schemes = {
+        ["Lucius"] = {
+            foreground = "#ffffff",
+            background = "#181818",
+
+            cursor_bg = "#ffffff",
+            cursor_fg = "#000000",
+            cursor_border = "#ffffff",
+            ansi = {
+                '#616261',
+                '#fd8272',
+                '#b4fa73',
+                '#fefcc3',
+                '#a5d5fe',
+                '#fd8ffd',
+                '#d0d1fe',
+                '#f1f0f2',
+            },
+            brights = {
+                '#8d8e8d',
+                '#fec4bd',
+                '#d6fcb9',
+                '#fefdd5',
+                '#c1e3fe',
+                '#fdb1fe',
+                '#e5e6fe',
+                '#fefffe',
+            },
         },
     },
 }
