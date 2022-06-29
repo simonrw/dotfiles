4f28f312cbadbc9c507408da7c5da8c65fb50769
Simon Walker <s.r.walker101@googlemail.com>
2022-06-29 14:50:34 +0100

    Remove completion
    Completion is nice, but it's quite distracting, and feels like it's
    slowing down my editor. Let's go back to normal vim completion for a
    bit.
    
    I suspect this will be annoying for Rust.

--- nvim/lua/customlspconfig.lua
+++ nvim/lua/customlspconfig.lua
@@ -55,10 +55,7 @@ end
 local function setup()
     local lspconfig = require("lspconfig")
     local lsp_installer = require("nvim-lsp-installer")
-    local capabilities = vim.tbl_extend('keep',
-            require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-            lsp_status.capabilities
-            )
+    local capabilities = lsp_status.capabilities
 
     -- Install required servers
     for _, target_server in ipairs(target_servers) do
