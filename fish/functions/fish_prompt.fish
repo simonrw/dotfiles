91730508b488334f1c737a629dd439dc20bff747
Simon Walker <s.r.walker101@googlemail.com>
2022-07-01 14:52:41 +0100

    fish: update colours

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -53,5 +53,5 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     # echo -n -s (set_color blue) {$current_pwd} {$space} {$bgjobs_color} {$jobs_prompt} {$suffix_color} {$suffix} {$normal} " "
-    echo -n -s (set_color brblack) "[" (date "+%H:%M") "] " (set_color blue) (hostname) (set_color brblack) ":" (set_color yellow) (basename $PWD) (set_color green) " " {$space} {$bgjobs_color} {$jobs_prompt} {$suffix_color} '| ' (set_color normal)
+    echo -n -s (set_color brblack) "[" (date "+%H:%M") "] " (set_color blue) (hostname) (set_color brblack) ":" (set_color cyan) (basename $PWD) (set_color green) {$space} {$bgjobs_color} {$jobs_prompt} {$suffix_color} '| ' (set_color normal)
 end
