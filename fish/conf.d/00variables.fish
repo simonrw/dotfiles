e9780ccb6c80250543dd5262665bb1accdf5e2f7
Simon Walker <s.r.walker101@googlemail.com>
2022-07-04 10:27:59 +0100

    env: support cargo sparse registry

--- fish/conf.d/00variables.fish
+++ fish/conf.d/00variables.fish
@@ -7,3 +7,6 @@ set -x PYTEST_ADDOPTS "-p no:sugar"
 set -x LANG en_GB.UTF-8
 set -x CARGO_TARGET_DIR {$HOME}/.cargo/cache
 set -x GOPROXY direct
+
+# experimental: enable cargo sparse registry for faster downloads
+set -x CARGO_UNSTABLE_SPARSE_REGISTRY true
