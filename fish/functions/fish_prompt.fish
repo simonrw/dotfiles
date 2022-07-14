917208e590334b05bb9ed695c3a85576eac00cde
Simon Walker <s.r.walker101@googlemail.com>
2022-07-14 10:35:19 +0100

    fish: update prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -23,29 +23,28 @@ function fish_prompt --description 'Write out the prompt'
         set bold_flag
     end
     set __fish_prompt_status_generation $status_generation
-    set -l status_color (set_color $fish_color_status)
-    set -l statusb_color (set_color $bold_flag $fish_color_status)
-    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
-    set -l bgjobs_color (set_color yellow)
 
     set -l num_bg_jobs (count (jobs))
-    set -l jobs_prompt ''
-    set -l current_pwd (prompt_pwd)
-    set -l space ' '
-    if test $num_bg_jobs = 0
-        set jobs_prompt ''
-    else
-        set jobs_prompt "="
-        set space ''
-    end
-
     set -l suffix_color ''
     if test $__fish_last_status = 0
-        set suffix_color (set_color green)
+        if test $num_bg_jobs = 0
+            set suffix_color (set_color green)
+        else
+            set suffix_color (set_color -u green)
+        end
     else
-        set suffix_color (set_color red)
+        if test $num_bg_jobs = 0
+            set suffix_color (set_color red)
+        else
+            set suffix_color (set_color -u red)
+        end
     end
 
     echo
-    echo -n -s {$bgjobs_color} {$jobs_prompt} {$suffix_color} {$suffix} {$normal} " "
+    echo -s (set_color brblue) (prompt_pwd --dir-length 2 --full-length-dirs 1) (set_color reset)
+    echo -n -s (__present_time) ' ' {$bgjobs_color} {$jobs_prompt} {$suffix_color} {$suffix} {$normal} " "
+end
+
+function __present_time
+    echo -n -s (set_color black) '[' (date +%H:%M) ']' (set_color reset)
 end
