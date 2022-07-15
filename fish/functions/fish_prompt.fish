df41c01013b7b0f58a2b60090b4547c4f283a694
Simon Walker <s.r.walker101@googlemail.com>
2022-07-15 22:25:59 +0100

    fish: change colour of pwd

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -44,6 +44,6 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     echo
-    echo -s (prompt_pwd --dir-length 2 --full-length-dirs 1)
+    echo -s (set_color brblack) (prompt_pwd --dir-length 2 --full-length-dirs 1)
     echo -n -s {$suffix_color} {$suffix} {$normal} " "
 end
