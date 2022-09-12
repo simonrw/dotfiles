6f8127e236cb3332627f46a7a473618e8f823b95
Simon Walker <s.r.walker101@googlemail.com>
2022-09-13 00:13:39 +0100

    feat: include direnv configuration

--- home.nix
+++ home.nix
@@ -4,6 +4,15 @@
     username = "simon";
     homeDirectory = "/Users/simon";
     stateVersion = "22.05";
+
+    sessionVariables = {
+      LANG = "en_GB.UTF-8";
+      LC_CTYPE = "en_GB.UTF-8";
+      LC_ALL = "en_GB.UTF-8";
+      EDITOR = "nvim";
+      PAGER = "bat";
+      MANPAGER = "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'";
+    };
   };
 
   programs.bat = {
@@ -14,5 +23,14 @@
       style = "plain";
     };
   };
+
   programs.home-manager.enable = true;
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv = {
+      enable = true;
+    };
+  };
+
 }
