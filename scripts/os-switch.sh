58362c9aa81a03b752c07c7c6d22ee4dcb63fa86
Simon Walker <s.r.walker101@googlemail.com>
2022-09-16 21:30:35 +0100

    add os-switch script

---
+++ scripts/os-switch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+. "$(dirname $(readlink -f $0))/utils.sh"
+
+NIXARCH="$(nixarch)"
+HOSTNAME="$(hostname -s)"
+
+nix --extra-experimental-features "nix-command flakes" build ".#darwinConfigurations.${NIXARCH}.${HOSTNAME}.system"
+./result/sw/bin/darwin-rebuild switch --flake ".#${NIXARCH}.${HOSTNAME}" $*
