6e56ff907ec130b15745158997f82b80fa8dc2fd
Simon Walker <s.r.walker101@googlemail.com>
2022-09-19 19:01:29 +0100

    nvim: move most configuration to nix

--- home/home.nix
+++ home/home.nix
@@ -725,6 +725,54 @@ in
     plugins = with pkgs.vimPlugins; [
       (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
       vim-nix
+      # pkgs.vimPlugins.skim
+      # skim-vim
+      vim-surround
+      vim-unimpaired
+      vim-commentary
+      vim-eunuch
+      vim-fugitive
+      vim-rhubarb
+      vim-repeat
+      # vim-tmux-runner
+      # vim-conflicted
+      vim-test
+      vim-easy-align
+      fugitive-gitlab-vim
+      markdown-preview-nvim
+      vim-gitgutter
+      telescope-nvim
+      telescope-ui-select-nvim
+      vim-gutentags
+      editorconfig-nvim
+      lualine-nvim
+      zen-mode-nvim
+      vim-toml
+      vim-svelte
+      vim-terraform
+      rust-vim
+      # lsps
+      nvim-lspconfig
+      lsp_extensions-nvim
+      lsp-status-nvim
+      lsp-format-nvim
+      nvim-dap
+      nvim-dap-ui
+
+      # completion
+      cmp-nvim-lsp
+      cmp-buffer
+      nvim-cmp
+      cmp-vsnip
+      cmp-emoji
+      vim-vsnip
+      lspkind-nvim
+
+      # treesitter
+      nvim-treesitter-context
+      playground
+      nvim-treesitter-textobjects
+      plenary-nvim
     ];
   };
 
--- home/nvim/lua/plugins.lua
+++ home/nvim/lua/plugins.lua
@@ -9,87 +9,20 @@ end
 
 require('packer').startup({function(use)
     use 'wbthomason/packer.nvim'
-    use 'lotabout/skim'
-    use 'lotabout/skim.vim'
-    use 'tpope/vim-surround'
-    use 'tpope/vim-unimpaired'
-    use 'tpope/vim-commentary'
-    use 'tpope/vim-eunuch'
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-rhubarb'
-    use 'tpope/vim-repeat'
     use 'christoomey/vim-tmux-runner'
     use 'christoomey/vim-conflicted'
-    use 'vim-test/vim-test'
-    use 'junegunn/vim-easy-align'
-    use 'shumphrey/fugitive-gitlab.vim'
-    use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
-    use 'airblade/vim-gitgutter'
-    use { 'kana/vim-textobj-indent', requires = { 'kana/vim-textobj-user' } }
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
     use 'TC72/telescope-tele-tabby.nvim'
-    use {'nvim-telescope/telescope-ui-select.nvim' }
-    use 'ludovicchabant/vim-gutentags'
-    use 'editorconfig/editorconfig-vim'
-    use 'nvim-lualine/lualine.nvim'
-    use {
-        "folke/zen-mode.nvim",
-        config = function()
-            require("zen-mode").setup {}
-        end
-    }
 
     -- language plugins
-    use 'cespare/vim-toml'
-    use 'evanleck/vim-svelte'
-    use 'hashivim/vim-terraform'
     use 'lepture/vim-velocity'
-    use 'NoahTheDuke/vim-just'
-    use 'rust-lang/rust.vim'
     use 'tweekmonster/django-plus.vim'
 
-    if vim.g.completion_framework == 'coc' then
-        use { 'neoclide/coc.nvim', branch = 'release' }
-        use 'fannheyward/telescope-coc.nvim'
-    elseif vim.g.completion_framework == 'coq' then
-        use { 'ms-jpq/coq_nvim', branch = 'coq' }
-    elseif vim.g.completion_framework == 'ale' then
-        use 'dense-analysis/ale'
-    elseif vim.g.completion_framework == 'nvim' then
-        use 'neovim/nvim-lspconfig'
-        use 'nvim-lua/lsp_extensions.nvim'
-        use 'psf/black'
-        use "williamboman/mason.nvim" 
-        use 'williamboman/mason-lspconfig.nvim'
-        use 'nvim-lua/lsp-status.nvim'
-        use "lukas-reineke/lsp-format.nvim"
+    use "williamboman/mason.nvim" 
+    use 'williamboman/mason-lspconfig.nvim'
 
-        -- debugging
-        use { 'rcarriga/nvim-dap-ui', requires = {'mfussenegger/nvim-dap'} }
-        use 'leoluz/nvim-dap-go'
-        use 'mfussenegger/nvim-dap-python'
-
-        -- Completion
-        use 'hrsh7th/cmp-nvim-lsp'
-        use 'hrsh7th/cmp-buffer'
-        use 'hrsh7th/nvim-cmp'
-        use 'hrsh7th/cmp-vsnip'
-        use 'hrsh7th/cmp-emoji'
-        use 'hrsh7th/vim-vsnip'
-        use 'onsails/lspkind-nvim'
-    end
-
-    if vim.g.include_treesitter == 1 then
-        use 'romgrk/nvim-treesitter-context'
-        use 'nvim-treesitter/playground'
-        use 'nvim-treesitter/nvim-treesitter-textobjects'
-    end
-
-    -- for testing
-    use 'nvim-lua/plenary.nvim'
+    -- debugging
+    use 'leoluz/nvim-dap-go'
+    use 'mfussenegger/nvim-dap-python'
 
     -- my plugins
     use 'mindriot101/search-in-scope.vim'
