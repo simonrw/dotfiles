e52d95c0cdad74b3842fbb8d13bd4be193c601c1
Simon Walker <s.r.walker101@googlemail.com>
2022-09-26 21:39:20 +0100

    correct merging of attribute sets

--- flake.nix
+++ flake.nix
@@ -21,41 +21,44 @@
             ./system/nixos/${name}/configuration.nix
           ];
         };
-    in
-    {
-      nixosConfigurations.nixos = mkNixOSConfiguration "nixos" //
-        flake-utils.lib.eachDefaultSystem (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          in
-          {
-            homeConfigurations = {
-              simon = home-manager.lib.homeManagerConfiguration {
-                pkgs = pkgs;
-                modules = [
-                  ./home/home.nix
-                ];
-              };
-            };
-            darwinConfigurations = {
-              mba = darwin.lib.darwinSystem {
-                system = "aarch64-darwin";
-                modules = [
-                  ./system/darwin/configuration.nix
-                ];
-                inputs = { inherit darwin pkgs; };
-              };
-            };
-            devShells.default = pkgs.mkShell {
-              buildInputs = with pkgs; [
-                python310
-                python310Packages.black
+
+      # these definitions are per system
+      perSystemConfigurations = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          homeConfigurations = {
+            simon = home-manager.lib.homeManagerConfiguration {
+              pkgs = pkgs;
+              modules = [
+                ./home/home.nix
               ];
             };
-          }
-        );
-    };
+          };
+          darwinConfigurations = {
+            mba = darwin.lib.darwinSystem {
+              system = "aarch64-darwin";
+              modules = [
+                ./system/darwin/configuration.nix
+              ];
+              inputs = { inherit darwin pkgs; };
+            };
+          };
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              python310
+              python310Packages.black
+            ];
+          };
+        }
+      );
+    in
+    {
+      # nixos configurations
+      nixosConfigurations.nixos = mkNixOSConfiguration "nixos";
+    } // perSystemConfigurations;
 }
