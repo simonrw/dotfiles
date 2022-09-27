0f371415b9866f2f185d9a1eb756e0419e386c25
Simon Walker <s.r.walker101@googlemail.com>
2022-09-27 17:50:48 +0100

    vim tighten up vim config

--- home/nvim/lua/config.lua
+++ home/nvim/lua/config.lua
@@ -2,27 +2,25 @@ require('disables')
 require('settings')
 require('plugins')
 require('mappings')
-require('gutentags')
-if vim.g.completion_framework == 'nvim' then
-    require('customlspconfig')
-    require('completionconfig')
-    require('dapconfig') end
-if vim.g.include_treesitter == 1 then
-    require('treesitterconfig')
-end
-
-if vim.fn.exists('g:neovide') then
-    require('neovideconfig')
-end
-
--- plugins
-require('fzf')
-require('vim-test')
-require('vtr')
-require('fugitive')
-if vim.g.include_treesitter == 1 then
-    require('treesitter-unit-config')
-end
 require('telescopeconfig')
-require('lualineconfig')
-require('octoconfig')
+
+vim.defer_fn(function()
+    if vim.g.include_treesitter == 1 then
+        require('treesitterconfig')
+    end
+
+    require('gutentags')
+
+    if vim.g.completion_framework == 'nvim' then
+        require('customlspconfig')
+        require('completionconfig')
+        require('dapconfig')
+    end
+
+    -- plugins
+    require('fzf')
+    require('vim-test')
+    require('vtr')
+    require('fugitive')
+    require('octoconfig')
+end, 0)
