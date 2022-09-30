356b94662efe072ae9713bc9a40b21096da74f3a
Simon Walker <s.r.walker101@googlemail.com>
2022-09-30 17:09:45 +0100

    add additional nvim dependency

--- bin/neovim-packages-env.sh
+++ bin/neovim-packages-env.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-PACKAGES="unzip zip nodejs cargo rustc go gcc"
+PACKAGES="unzip zip nodejs cargo rustc go gcc libiconv"
 
 # set up a nix shell that's suitable for building my neovim lsp servers
 nix-shell -p ${PACKAGES} --run nvim
