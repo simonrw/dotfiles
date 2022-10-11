7ede59d09fa2b31ce00f0af3705acc4b0b3fe591
Simon Walker <s.r.walker101@googlemail.com>
2022-10-11 16:44:08 +0100

    vim: disable line numbering

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,8 +1,8 @@
 vim.opt.mouse = 'a'
 vim.opt.scrolloff = 5
 vim.opt.smartindent = true
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.number = false
+vim.opt.relativenumber = false
 vim.opt.guicursor = ""
 vim.opt.cursorline = false
 vim.opt.ttimeoutlen = 10
