389009bab51186c8216b00fbb060bf7bac37bca7
Simon Walker <s.r.walker101@googlemail.com>
2022-10-24 14:24:24 +0100

    Revert "remove global cargo target dir"
    This reverts commit 1bcc6116e63f771dbe4ba094aec83828dd665c96.

--- fish/conf.d/00variables.fish
+++ fish/conf.d/00variables.fish
@@ -5,6 +5,7 @@ set -x EDITOR nvim
 set -x REVIEW_BASE main
 set -x PYTEST_ADDOPTS "-p no:sugar"
 set -x LANG en_GB.UTF-8
+set -x CARGO_TARGET_DIR {$HOME}/.cargo/cache
 
 # experimental: enable cargo sparse registry for faster downloads
 set -x CARGO_UNSTABLE_SPARSE_REGISTRY true
