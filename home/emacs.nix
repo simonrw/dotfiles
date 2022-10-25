fe6733e315ad897f67b816a43c9fbd8b7a719eba
Simon Walker <s.r.walker101@googlemail.com>
2022-10-25 23:34:53 +0100

    fix: use gui emacs package

--- home/emacs.nix
+++ home/emacs.nix
@@ -1,29 +1,31 @@
 { pkgs ? import <nixpkgs> { } }:
 {
   enable = true;
-  extraPackages = epkgs: with epkgs; [
-    magit
-    exec-path-from-shell
-    markdown-mode
-    rust-mode
-    nix-mode
-    nixpkgs-fmt
-    cargo
-    toml-mode
-    cmake-mode
-    python-pytest
-    go-mode
-    dockerfile-mode
-    yaml-mode
-    which-key
-    speed-type
-    vterm
-    company
-    projectile
-    ripgrep
-    fzf
-    eglot
-  ];
+  package = pkgs.emacs-gtk;
+  extraPackages = epkgs: with epkgs;
+    [
+      magit
+      exec-path-from-shell
+      markdown-mode
+      rust-mode
+      nix-mode
+      nixpkgs-fmt
+      cargo
+      toml-mode
+      cmake-mode
+      python-pytest
+      go-mode
+      dockerfile-mode
+      yaml-mode
+      which-key
+      speed-type
+      vterm
+      company
+      projectile
+      ripgrep
+      fzf
+      eglot
+    ];
   extraConfig = (builtins.readFile ./emacs/init.el) + ''
     (setq explicit-shell-file-name "${pkgs.fish}/bin/fish")
     (setq shell-file-name "fish")
