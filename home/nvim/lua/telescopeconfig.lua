6ffbfb09f5fe94dd8a313325883a7a40da5710dd
Simon Walker <s.r.walker101@googlemail.com>
2022-11-09 12:56:21 +0000

    Make telescope look more like helix

--- home/nvim/lua/telescopeconfig.lua
+++ home/nvim/lua/telescopeconfig.lua
@@ -1,11 +1,18 @@
 local mappings = require('mappings')
 
-mappings.nnoremap('<leader>f', [[<cmd>lua require('telescope.builtin').git_files({previewer = false})<Cr>]])
-mappings.nnoremap('<leader>F', [[<cmd>lua require('telescope.builtin').find_files({previewer = false})<Cr>]])
+mappings.nnoremap('<leader>f', [[<cmd>lua require('telescope.builtin').git_files()<Cr>]])
+mappings.nnoremap('<leader>F', [[<cmd>lua require('telescope.builtin').find_files()<Cr>]])
 mappings.nnoremap('<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<Cr>]])
 mappings.nnoremap('<leader>/', [[<cmd>lua require('telescope.builtin').live_grep()<Cr>]])
 
 require("telescope").setup({
+    defaults = {
+        layout_strategy = 'horizontal',
+        layout_config = {
+            prompt_position = 'top',
+        },
+        sorting_strategy = 'ascending',
+    },
   extensions = {
     ["ui-select"] = {},
     ["tele_tabby"] = {},
