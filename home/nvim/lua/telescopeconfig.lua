cbde421c7711c21ae3dad8cf91d148fa331f3bf2
Simon Walker <s.r.walker101@googlemail.com>
2022-11-28 20:54:49 +0000

    feat: use g/ and gb for some telescope mappings

--- home/nvim/lua/telescopeconfig.lua
+++ home/nvim/lua/telescopeconfig.lua
@@ -2,8 +2,8 @@ local mappings = require('mappings')
 
 mappings.nnoremap('<leader>f', [[<cmd>lua require('telescope.builtin').git_files()<Cr>]])
 mappings.nnoremap('<leader>F', [[<cmd>lua require('telescope.builtin').find_files()<Cr>]])
-mappings.nnoremap('<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<Cr>]])
-mappings.nnoremap('<leader>/', [[<cmd>lua require('telescope.builtin').live_grep()<Cr>]])
+mappings.nnoremap('gb', [[<cmd>lua require('telescope.builtin').buffers()<Cr>]])
+mappings.nnoremap('g/', [[<cmd>lua require('telescope.builtin').live_grep()<Cr>]])
 
 require("telescope").setup({
     defaults = {
