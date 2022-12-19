2700852728276cf191c0c6d80c3d10ca4089a60f
Simon Walker <s.r.walker101@googlemail.com>
2022-12-19 08:50:24 +0000

    add errors window

--- home/nvim/lua/plugins.lua
+++ home/nvim/lua/plugins.lua
@@ -3,11 +3,11 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+    fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
     execute('packadd packer.nvim')
 end
 
-require('packer').startup({function(use)
+require('packer').startup({ function(use)
     use 'wbthomason/packer.nvim'
     use 'christoomey/vim-tmux-runner'
     use 'christoomey/vim-conflicted'
@@ -28,7 +28,7 @@ require('packer').startup({function(use)
         config = function()
             require('octo').setup({
                 file_panel = {
-                    use_icons = false,
+                    icons = false,
                 },
             })
         end,
@@ -37,21 +37,34 @@ require('packer').startup({function(use)
         'VonHeikemen/lsp-zero.nvim',
         requires = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},
-            {'williamboman/mason.nvim'},
-            {'williamboman/mason-lspconfig.nvim'},
+            { 'neovim/nvim-lspconfig' },
+            { 'williamboman/mason.nvim' },
+            { 'williamboman/mason-lspconfig.nvim' },
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},
-            {'hrsh7th/cmp-buffer'},
-            {'hrsh7th/cmp-path'},
-            {'saadparwaiz1/cmp_luasnip'},
-            {'hrsh7th/cmp-nvim-lsp'},
-            {'hrsh7th/cmp-nvim-lua'},
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-buffer' },
+            { 'hrsh7th/cmp-path' },
+            { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'hrsh7th/cmp-nvim-lua' },
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},
+            { 'L3MON4D3/LuaSnip' },
         }
     }
+
+    use {
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+                use_diagnostic_signs = false,
+                auto_preview = false,
+            }
+
+            vim.keymap.set("n", "yot", function() require('trouble').toggle() end, { remap = false })
+        end
+    }
 end,
-config = {}})
+    config = {} })
