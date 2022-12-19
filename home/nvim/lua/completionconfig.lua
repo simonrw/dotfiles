e406a4b532b18bc93a21bdb5e049f8c68133cf5f
Simon Walker <s.r.walker101@googlemail.com>
2022-12-19 20:02:17 +0000

    include prettier as formatter

--- home/nvim/lua/completionconfig.lua
+++ home/nvim/lua/completionconfig.lua
@@ -11,6 +11,7 @@ null_ls.setup({
         null_ls.builtins.diagnostics.mypy,
         null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.prettier
     },
 })
 
