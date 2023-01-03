ba8d4807f95b7d79068c7c6b7ba10ead3eff66f9
Simon Walker <s.r.walker101@googlemail.com>
2023-01-03 09:04:43 +0000

    Install fish system wide
    This is required to set up nix flake completion

--- system/nixos/astoria/configuration.nix
+++ system/nixos/astoria/configuration.nix
@@ -140,6 +140,9 @@
     bashInteractive
   ];
 
+  # enable fish system-wide for completion
+  programs.fish.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
