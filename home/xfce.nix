2efade868effd7fa4526a112d9fcecd660924ef9
Simon Walker <s.r.walker101@googlemail.com>
2023-01-08 00:10:55 +0000

    Start to add keyboard shortcuts

--- home/xfce.nix
+++ home/xfce.nix
@@ -1,9 +1,15 @@
 { config, pkgs, ... }:
+let
+  settings = {
+    xfce4-panel = {
+      "panels/dark-mode" = config.dark-mode;
+    };
+    xfce4-keyboard-shortcuts = {
+      "commands/custom/<Alt><Super>c" = ''${pkgs.wmctrl}/bin/wmctrl -x -a google-chrome || ${pkgs.google-chrome}/bin/google-chrome-stable'';
+    };
+  };
+in
 {
   xfconf.settings =
-    if pkgs.stdenv.isLinux then {
-      xfce4-panel = {
-        "panels/dark-mode" = config.dark-mode;
-      };
-    } else null;
+    if pkgs.stdenv.isLinux then settings else null;
 }
