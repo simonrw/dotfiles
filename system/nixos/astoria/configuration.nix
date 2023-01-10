84d955dcf89eae41b7c5c2ca55f421b6283d17e9
Simon Walker <s.r.walker101@googlemail.com>
2023-01-10 13:19:16 +0000

    Update configuration to match example in the wiki

--- system/nixos/astoria/configuration.nix
+++ system/nixos/astoria/configuration.nix
@@ -84,7 +84,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
