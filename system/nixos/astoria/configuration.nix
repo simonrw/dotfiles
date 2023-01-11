4b27c0b7fec81ee2469c16acfd573cae01340877
Simon Walker <s.r.walker101@googlemail.com>
2023-01-11 11:13:03 +0000

    Revert "Update configuration to match example in the wiki"
    This reverts commit 84d955dcf89eae41b7c5c2ca55f421b6283d17e9.

--- system/nixos/astoria/configuration.nix
+++ system/nixos/astoria/configuration.nix
@@ -84,6 +84,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
+  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
