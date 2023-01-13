95dcb1d37148c95aa5a33a92d543f4e29ea59511
Simon Walker <s.r.walker101@googlemail.com>
2023-01-13 10:06:27 +0000

    Hoist xfce settings to variable
    This will help me implement sway in the future

--- system/nixos/astoria/wm.nix
+++ system/nixos/astoria/wm.nix
@@ -1,13 +1,17 @@
 { pkgs, ... }:
-{
-  services.xserver.displayManager.defaultSession = "xfce";
-  services.xserver.desktopManager.xfce.enable = true;
+let
+  xfce-settings =
+    {
+      services.xserver.displayManager.defaultSession = "xfce";
+      services.xserver.desktopManager.xfce.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    xfce.xfce4-cpufreq-plugin
-    xfce.xfce4-cpugraph-plugin
-    xfce.xfce4-pulseaudio-plugin
-    xfce.xfce4-systemload-plugin
-    xfce.xfce4-xkb-plugin
-  ];
-}
+      environment.systemPackages = with pkgs; [
+        xfce.xfce4-cpufreq-plugin
+        xfce.xfce4-cpugraph-plugin
+        xfce.xfce4-pulseaudio-plugin
+        xfce.xfce4-systemload-plugin
+        xfce.xfce4-xkb-plugin
+      ];
+    };
+in
+xfce-settings
