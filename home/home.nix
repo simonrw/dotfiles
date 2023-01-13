d2b46fdb01e0f25ab657b1c35d3d1c940242bd93
Simon Walker <s.r.walker101@googlemail.com>
2023-01-13 14:41:11 +0000

    Handle kitty colours

--- home/home.nix
+++ home/home.nix
@@ -309,10 +309,20 @@ in
       source = ./karabiner;
       recursive = true;
     };
-    configFile.kitty = {
-      source = ./kitty;
-      recursive = true;
+    configFile."kitty/kitty.conf" = {
+      source = ./kitty/kitty.conf;
     };
+    configFile."kitty/themes/theme.conf" =
+      let
+        theme =
+          if config.dark-mode
+          then "srw.conf"
+          else "papercolor-light.conf";
+
+      in
+      {
+        source = ./kitty/themes/${theme};
+      };
   };
 
   # currently this is broken
