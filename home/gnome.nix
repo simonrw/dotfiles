4ca96ed03efae1e9b38cb4756aa023ca983ecbbe
Simon Walker <s.r.walker101@googlemail.com>
2023-01-19 07:58:22 +0000

    Finish gnome key bindings

--- home/gnome.nix
+++ home/gnome.nix
@@ -11,10 +11,28 @@
       command = "sh -c 'wmctrl -x -a kitty || kitty'";
       name = "Terminal";
     };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      binding = "<Alt><Super>c";
+      command = "sh -c 'wmctrl -x -a chromium || chromium'";
+      name = "Browser";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      binding = "<Alt><Super>e";
+      command = "sh -c 'wmctrl -x -a obsidian || obsidian'";
+      name = "Notes";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+      binding = "<Alt><Super>r";
+      command = "sh -c 'wmctrl -x -a zeal || zeal'";
+      name = "Documentation";
+    };
     "org/gnome/settings-daemon/plugins/media-keys" = {
       custom-keybindings = [
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
       ];
     };
   };
