145b6a030dfb9266b34730dfa788a570c7d84720
Simon Walker <s.r.walker101@googlemail.com>
2023-02-10 17:22:06 +0000

    Don't use sparse rust registries

--- home/fish.nix
+++ home/fish.nix
@@ -40,10 +40,6 @@ in
         set -x PAGER "${pkgs.bat}/bin/bat"
         set -x MANPAGER "sh -c 'col -bx | ${pkgs.bat}/bin/bat -l man -p'"
 
-        # experimental: enable cargo sparse registry for faster downloads
-        set -x CARGO_UNSTABLE_SPARSE_REGISTRY true
-        set -x CARGO_REGISTRIES_CRATES_IO_PROTOCOL sparse
-
         # host-specific configuration
         set host_config ~/.config/fish/conf.d/per-host/config.(hostname).fish
         test -r $host_config; and source $host_config
