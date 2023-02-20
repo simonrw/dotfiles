74b4f7a6ea68ce67d7f15ddb31f448c12e90daf3
Simon Walker <s.r.walker101@googlemail.com>
2023-02-20 10:56:39 +0000

    Add docker compose aliases

--- home/fish.nix
+++ home/fish.nix
@@ -127,6 +127,11 @@ in
       curl = "${pkgs.curlie}/bin/curlie";
       da = "${pkgs.direnv}/bin/direnv allow";
       de = "${pkgs.direnv}/bin/direnv edit";
+      # docker compose
+      dcd = "${pkgs.docker}/bin/docker compose down";
+      dce = "${pkgs.docker}/bin/docker compose exec";
+      dcr = "${pkgs.docker}/bin/docker compose run";
+      dcu = "${pkgs.docker}/bin/docker compose up";
       es = ''exec $SHELL'';
       gpe = "${pkgs.git}/bin/git push && exit";
       gpr = "${pkgs.git}/bin/git pull --rebase";
