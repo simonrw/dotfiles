7227dfc53cf7cd188ff6b17444842c6c88ed7972
Simon Walker <s.r.walker101@googlemail.com>
2023-02-20 11:50:44 +0000

    Configure gh-dash

--- home/gh.nix
+++ home/gh.nix
@@ -10,4 +10,32 @@
       pkgs.gh-dash
     ];
   };
+
+  xdg.configFile."gh-dash/config.yml".text = builtins.toJSON {
+    prSections = [
+      {
+        title = "LS: My Pull Requests";
+        filters = "is:pr is:open author:@me org:localstack";
+        layout.author.hidden = true;
+      }
+      {
+        title = "LS: Review requested";
+        filters = "is:pr is:open review-requested:@me org:localstack";
+      }
+      {
+        title = "LS: Involved";
+        filters = "is:pr is:open involves:@me -author:@me org:localstack";
+      }
+    ];
+    issuesSections = [
+      {
+        title = "LS: Involved";
+        filters = "is:issue is:open involves:@me org:localstack";
+      }
+    ];
+    repoPaths = {
+      "localstack/*" = "~/work/localstack/*";
+    };
+    pager.diff = "delta";
+  };
 }
