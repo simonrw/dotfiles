d6ef07d6dda2abc5638bde0996c766b435cb5f34
Simon Walker <s.r.walker101@googlemail.com>
2023-02-27 10:25:38 +0000

    Add delay to illuminate

--- home/nvim/lua/illuminateconfig.lua
+++ home/nvim/lua/illuminateconfig.lua
@@ -1,3 +1,5 @@
-require('illuminate').configure()
-
+require('illuminate').configure({
+    -- delay appearance a little
+    delay = 750,
+})
 -- highlight configuration is set up in after/plugin/illuminateconfig.vim
