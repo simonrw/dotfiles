dd1bb96839fb51b9c895c4d796c458e65689362f
Simon Walker <s.r.walker101@googlemail.com>
2023-02-27 10:27:55 +0000

    Revert "Add delay to illuminate"
    This reverts commit d6ef07d6dda2abc5638bde0996c766b435cb5f34.

--- home/nvim/lua/illuminateconfig.lua
+++ home/nvim/lua/illuminateconfig.lua
@@ -1,5 +1,3 @@
-require('illuminate').configure({
-    -- delay appearance a little
-    delay = 750,
-})
+require('illuminate').configure()
+
 -- highlight configuration is set up in after/plugin/illuminateconfig.vim
