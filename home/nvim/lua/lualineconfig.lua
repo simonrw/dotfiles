151b766db6cead58004ba523a5554aae2c42b175
Simon Walker <s.r.walker101@googlemail.com>
2023-03-08 16:34:51 +0000

    Use global status bar for vim

--- home/nvim/lua/lualineconfig.lua
+++ home/nvim/lua/lualineconfig.lua
@@ -15,7 +15,7 @@ lualine.setup({
         section_separators = { left = '', right = '' },
         disabled_filetypes = {},
         always_divide_middle = true,
-        globalstatus = false,
+        globalstatus = true,
         refresh = {
             statusline = 100,
         },
