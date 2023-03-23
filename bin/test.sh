457d65fba1b062a0620b0142f0f8268845151570
Simon Walker <s.r.walker101@googlemail.com>
2023-03-23 12:48:20 +0000

    Add test.sh for testing system

---
+++ bin/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+. "$(dirname "$(readlink -f "$0")")/utils.sh"
+
+NIXARCH="$(nixarch)"
+HOSTNAME="$(hostname -s)"
+
+case ${NIXARCH} in
+    *-linux)
+        sudo nixos-rebuild test --flake .
+        ;;
+    *-darwin)
+        nix --extra-experimental-features "nix-command flakes" build ".#darwinConfigurations.${HOSTNAME}.system"
+        ./result/sw/bin/darwin-rebuild test --flake ".#${HOSTNAME}" "$@"
+        ;;
+    *)
+        echo "Unhandled architecture: ${NIXARCH}" >&2
+        exit 1
+    ;;
+esac
