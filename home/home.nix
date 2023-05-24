d6af470d6ac17bd51e969c4e95306ebdbf6161c4
Simon Walker <s.r.walker101@googlemail.com>
2023-05-24 16:35:22 +0100

    Use my theme

--- home/home.nix
+++ home/home.nix
@@ -57,7 +57,7 @@ in
     dark-mode = true;
     font-name = "JetBrains Mono";
     font-size = if isLinux then 10.0 else 12.0;
-    theme = "github";
+    theme = "srw";
     defaults = {
       browser = {
         command = "google-chrome-stable";
