a70a7f439c8ceadb476fa48ee6dd2236da160216
Simon Walker <s.r.walker101@googlemail.com>
2023-06-12 21:43:31 +0100

    Configure dap for tests

--- home/nvim/lua/dapconfig.lua
+++ home/nvim/lua/dapconfig.lua
@@ -1,5 +1,6 @@
-require('dap-python').setup()
-require("dapui").setup()
+local dap_python = require('dap-python')
+
+dap_python.setup()
 
 -- keybindings
 vim.keymap.set('n', '<F8>', ":lua require('dap').step_over()<cr>")
@@ -9,6 +10,7 @@ vim.keymap.set('n', '<Shift><F9>', ":lua require('dap').step_out()<cr>")
 vim.keymap.set('n', '<leader>b', ":lua require('dap').toggle_breakpoint()<cr>")
 
 local dap, dapui = require("dap"), require("dapui")
+dapui.setup()
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
 end
@@ -19,3 +21,5 @@ dap.listeners.before.event_exited["dapui_config"] = function()
   dapui.close()
 end
 
+-- configure dap-python
+dap_python.test_runner = "pytest"
