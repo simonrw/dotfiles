0bd26a97ddb3da37fd52c1f392c5902520bb1680
Simon Walker <s.r.walker101@googlemail.com>
2023-06-12 21:33:32 +0100

    Add dap configuration

--- home/nvim/lua/dapconfig.lua
+++ home/nvim/lua/dapconfig.lua
@@ -1,6 +1,13 @@
 require('dap-python').setup()
 require("dapui").setup()
 
+-- keybindings
+vim.keymap.set('n', '<F8>', ":lua require('dap').step_over()<cr>")
+vim.keymap.set('n', '<F9>', ":lua require('dap').continue()<cr>")
+vim.keymap.set('n', '<F7>', ":lua require('dap').step_into()<cr>")
+vim.keymap.set('n', '<Shift><F9>', ":lua require('dap').step_out()<cr>")
+vim.keymap.set('n', '<leader>b', ":lua require('dap').toggle_breakpoint()<cr>")
+
 local dap, dapui = require("dap"), require("dapui")
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
