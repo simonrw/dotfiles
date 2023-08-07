e0e64c1ed0b33dc37fee93d51cfbdd2cc19e50dd
Simon Walker <s.r.walker101@googlemail.com>
2023-08-07 14:54:00 +0100

    Use light mode

--- home/home.nix
+++ home/home.nix
@@ -55,11 +55,11 @@ in
 
   # custom properties
   me = {
-    dark-mode = true;
+    dark-mode = false;
     font-name = "JetBrains Mono";
     font-style = "Semibold";
     font-size = if isLinux then 10.0 else 12.0;
-    theme = "srw";
+    theme = "solarized";
     vscode-theme = "Dracula";
     defaults = {
       browser = "brave";
@@ -67,8 +67,8 @@ in
     };
   } // (if isLinux then {
     cinnamon = {
-      theme = "Mint-Y-Dark-Aqua";
-      dark-mode = true;
+      theme = "Mint-Y-Aqua";
+      dark-mode = false;
     };
   } else { });
 
