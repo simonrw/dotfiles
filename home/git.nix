21084cec87e17d88a1254dca626d7a06c1de79f9
Simon Walker <s.r.walker101@googlemail.com>
2023-08-29 16:59:00 +0100

    Revert "Add checkout helper script"
    This reverts commit 6b4606ac84208621be19c546db506e650bb95ac4.

--- home/git.nix
+++ home/git.nix
@@ -42,6 +42,7 @@
       # Other aliases
       ff = "!f() { git merge --ff-only origin/$(git branch-name); }; f";
       st = "status";
+      co = "checkout";
       su = "submodule update --init --recursive";
       pr = "pull --rebase --prune";
       mergeff = "merge --ff-only";
