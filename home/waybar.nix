ff23cfa8e47153d0a85a10bdbe2236f735333c22
Simon Walker <s.r.walker101@googlemail.com>
2023-09-03 15:20:22 +0100

    Add waybar configuration

--- home/waybar.nix
+++ home/waybar.nix
@@ -13,8 +13,21 @@
           "DP-2"
         ];
         modules-left = [ "wlr/workspaces" ];
-        modules-center = [ ];
-        modules-right = [ ];
+        modules-center = [ "hyprland/window" ];
+        modules-right = [ "bluetooth" ];
+        # module config
+        "wlr/workspaces" = {
+          "on-click" = "activate";
+        };
+        "bluetooth" = {
+          "format" = " {status}";
+          "format-disabled" = "";
+          "format-connected" = " {num_connections} connected";
+          "tooltip-format" = "{controller_alias}\t{controller_address}";
+          "tooltip-format-connected" = "{controller_alias}\t{controller_address}\n\n{device_enumerate}";
+          "tooltip-format-enumerate-connected" = "{device_alias}\t{device_address}";
+          "on-click" = "blueman-manager";
+        };
       };
     };
   };
