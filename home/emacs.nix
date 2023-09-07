d13cb4a2b8a8ee3d6d2c8066d5fd6c1e384dbf1b
Simon Walker <s.r.walker101@googlemail.com>
2023-09-07 14:57:02 +0100

    Bypass rebuilding emacs every time

--- home/emacs.nix
+++ home/emacs.nix
@@ -1,7 +1,32 @@
 { pkgs, ... }:
 {
-  programs.doom-emacs = {
-    enable = false;
+  programs.doom-emacs = rec {
+    enable = true;
     doomPrivateDir = ./doom.d;
+    # Only init/packages so we only rebuild when those change.
+    # https://github.com/znewman01/dotfiles/blob/be9f3a24c517a4ff345f213bf1cf7633713c9278/emacs/default.nix#L12-L34
+    doomPackageDir =
+      let
+        filteredPath = builtins.path {
+          path = doomPrivateDir;
+          name = "doom-private-dir-filtered";
+          filter = path: type:
+            builtins.elem (baseNameOf path) [ "init.el" "packages.el" ];
+        };
+      in
+      pkgs.linkFarm "doom-packages-dir" [
+        {
+          name = "init.el";
+          path = "${filteredPath}/init.el";
+        }
+        {
+          name = "packages.el";
+          path = "${filteredPath}/packages.el";
+        }
+        {
+          name = "config.el";
+          path = pkgs.emptyFile;
+        }
+      ];
   };
 }
