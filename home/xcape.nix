6a0b45414a96ea442810aaadae875e9889211a8e
Simon Walker <s.r.walker101@googlemail.com>
2023-09-07 14:59:46 +0100

    Do not run xcape by default

--- home/xcape.nix
+++ home/xcape.nix
@@ -1,8 +1,13 @@
-{ ... }:
+{ config, lib, ... }:
+let
+  cfg = config.me.xcape;
+in
 {
+  options.me.xcape.enable = lib.mkEnableOption (lib.mkDoc "");
+
   config = {
-    services.xcape = { 
-      enable = true;
+    services.xcape = {
+      enable = cfg.enable;
       mapExpression = {
         Control_L = "Escape";
       };
