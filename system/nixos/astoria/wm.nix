a8b1ab52ee9f75df18669a8e9e5a6eab120c4a36
Simon Walker <s.r.walker101@googlemail.com>
2023-09-07 14:35:55 +0100

    Comment out overrides from main branch

--- system/nixos/astoria/wm.nix
+++ system/nixos/astoria/wm.nix
@@ -15,9 +15,9 @@
   me.wm.hyprland.enable = true;
 
   # overrides
-  services.xserver.displayManager = {
-    gdm.enable = lib.mkForce false;
-    sddm.enable = lib.mkForce true;
-    defaultSession = lib.mkForce "cinnamon";
-  };
+  # services.xserver.displayManager = {
+  #   gdm.enable = lib.mkForce true;
+  #   sddm.enable = lib.mkForce false;
+  #   defaultSession = lib.mkForce "cinnamon";
+  # };
 }
