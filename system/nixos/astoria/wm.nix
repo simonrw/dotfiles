615868012c4537e1245bf7ecab99cca40184ed92
Simon Walker <s.r.walker101@googlemail.com>
2023-09-21 09:34:14 +0100

    Only use gnome

--- system/nixos/astoria/wm.nix
+++ system/nixos/astoria/wm.nix
@@ -11,15 +11,17 @@
   ];
 
   # enable the window managers I use 
-  me.wm.cinnamon.enable = true;
+  # me.wm.cinnamon.enable = true;
   # me.wm.hyprland.enable = true;
   # me.wm.i3.enable = true;
-  me.wm.sway.enable = true;
+  # me.wm.sway.enable = true;
+  me.wm.gnome.enable = true;
+  me.wm.gnome.wayland = true;
 
   # overrides
-  services.xserver.displayManager = {
-    #   gdm.enable = lib.mkForce true;
-    sddm.enable = lib.mkForce false;
-    #   defaultSession = lib.mkForce "cinnamon";
-  };
+  # services.xserver.displayManager = {
+  #   gdm.enable = lib.mkForce true;
+  #   sddm.enable = lib.mkForce false;
+  #   defaultSession = lib.mkForce "cinnamon";
+  # };
 }
