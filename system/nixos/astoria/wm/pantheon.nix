45c3812a4ebd58cb5827ccaf5b477a2a6da6a334
Simon Walker <s.r.walker101@googlemail.com>
2023-10-17 16:28:11 +0100

    Switch to pantheon

--- system/nixos/astoria/wm/pantheon.nix
+++ system/nixos/astoria/wm/pantheon.nix
@@ -16,5 +16,9 @@ in
   config = mkIf cfg.enable {
     services.xserver.desktopManager.pantheon.enable = true;
     services.pantheon.apps.enable = !cfg.disableApps;
+    services.xserver.displayManager.lightdm.greeters.pantheon.enable = true;
+    environment.pantheon.excludePackages = with pkgs; [
+      orca
+    ];
   };
 }
