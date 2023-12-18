a8f6877e79ce7cb73d3dbe20ac6270a2d930893f
Simon Walker <s.r.walker101@googlemail.com>
2023-12-18 21:50:28 +0000

    Unconditionally run treesitter configuration

--- home/nvim/lua/treesitterconfig.lua
+++ home/nvim/lua/treesitterconfig.lua
@@ -1,35 +1,29 @@
-local function setup()
-    local ts = require('nvim-treesitter.configs')
+local ts = require('nvim-treesitter.configs')
 
-    ts.setup {
-        highlight = {
+ts.setup {
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+        disable = {
+            "gitcommit",
+        },
+    },
+    indent = {
+        enable = false,
+    },
+    textobjects = {
+        select = {
             enable = true,
-            use_languagetree = true,
-            disable = {
-                "gitcommit",
+            lookahead = true,
+            keymaps = {
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                ["ic"] = "@class.inner",
             },
+            include_surrounding_whitespace = function(query_string, selection_mode)
+                return query_string == "@function.outer" or query_string == "@class.outer"
+            end,
         },
-        indent = {
-            enable = false,
-        },
-        textobjects = {
-            select = {
-                enable = true,
-                lookahead = true,
-                keymaps = {
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                },
-                include_surrounding_whitespace = function(query_string, selection_mode)
-                    return query_string == "@function.outer" or query_string == "@class.outer"
-                end,
-            },
-        },
-    }
-end
-
-if vim.fn.has('nvim-0.5') and vim.g.include_treesitter then
-    setup()
-end
+    },
+}
