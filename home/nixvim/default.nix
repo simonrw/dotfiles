a07cfeb568f542e4a9d0dc6a03c4e2e018ca3e3a
Simon Walker <s.r.walker101@googlemail.com>
2024-02-12 21:32:49 +0000

    Add shortcut for toggle quickfix list

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -160,6 +160,10 @@ in {
         key = "<leader>W";
         action = '':mksession!|echo "Session saved"<cr>'';
       })
+      (keymap {
+        key = "Q";
+        action = ":call ToggleList('Quickfix List', 'c')<cr>";
+      })
       # emulate vim-vinegar which stopped working
       (keymap {
         key = "-";
@@ -321,6 +325,36 @@ in {
     extraConfigLua = ''
       -- set up aerial-nvim
       require('aerial').setup()
+    '';
+    extraConfigVim = ''
+      " Add mapping to open/close the quickfix list
+      " Taken from: http://vim.wikia.com/wiki/Toggle_to_open_or_close_the_quickfix_window
+      function! GetBufferList()
+        redir =>buflist
+        silent! ls!
+        redir END
+        return buflist
+      endfunction
+
+      function! ToggleList(bufname, pfx)
+        let buflist = GetBufferList()
+        for bufnum in map(filter(split(buflist, '\n'), 'v:val =~ "'.a:bufname.'"'), 'str2nr(matchstr(v:val, "\\d\\+"))')
+          if bufwinnr(bufnum) != -1
+            exec(a:pfx.'close')
+            return
+          endif
+        endfor
+        if a:pfx == 'l' && len(getloclist(0)) == 0
+            echohl ErrorMsg
+            echo "Location List is Empty."
+            return
+        endif
+        let winnr = winnr()
+        exec(a:pfx.'open')
+        if winnr() != winnr
+          wincmd p
+        endif
+      endfunction
     '';
   };
 }
