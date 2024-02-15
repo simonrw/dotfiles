b0d5c55ce62823e790b8aac3bf1fdd3f9ad61eb7
Simon Walker <s.r.walker101@googlemail.com>
2024-02-15 09:30:07 +0000

    Get system tray to show

--- system/nixos/astoria/wm/bspwm.nix
+++ system/nixos/astoria/wm/bspwm.nix
@@ -7,19 +7,27 @@
 with lib; let
   cfg = config.me.wm.bspwm;
 in {
-  options.me.wm.bspwm.enable = mkEnableOption "BSPWM";
+  options.me.wm.bspwm = {
+    enable = mkEnableOption "BSPWM";
+
+    with-xfce = mkOption {
+      type = types.bool;
+      default = false;
+      description = "Use xfce as a desktop environment";
+    };
+  };
 
   config = mkIf cfg.enable {
     services.xserver = {
       enable = true;
       desktopManager = {
         xfce = {
-          enable = true;
+          enable = cfg.with-xfce;
           noDesktop = true;
           enableXfwm = false;
         };
       };
-      displayManager.defaultSession = "xfce+bspwm";
+      displayManager.defaultSession = "none+bspwm";
       windowManager.bspwm.enable = true;
     };
     # enable bluetooth manager
