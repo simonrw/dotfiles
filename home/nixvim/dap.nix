0f914098e6b78b34cb98d51a5d9914a8cf05d292
Simon Walker <s.r.walker101@googlemail.com>
2024-02-29 23:13:19 +0000

    Update dap configuration

--- home/nixvim/dap.nix
+++ home/nixvim/dap.nix
@@ -17,10 +17,17 @@ in {
       enable = true;
       extensions.dap-go.enable = true;
       extensions.dap-python.enable = true;
-      extensions.dap-ui.enable = true;
+      extensions.dap-ui = {
+        enable = true;
+        # controls.enabled = false;
+      };
       extensions.dap-virtual-text.enable = true;
     };
     keymaps = [
+      (keymap {
+        key = "<f7>";
+        action = ":lua require('dap').step_into()<cr>";
+      })
       (keymap {
         key = "<f8>";
         action = ":lua require('dap').step_over()<cr>";
@@ -28,10 +35,6 @@ in {
       (keymap {
         key = "<F9>";
         action = ":lua require('dap').continue()<cr>";
-      })
-      (keymap {
-        key = "<F10>";
-        action = ":lua require('dap').step_into()<cr>";
       })
       (keymap {
         key = "<Shift><F9>";
@@ -42,5 +45,21 @@ in {
         action = ":lua require('dap').toggle_breakpoint()<cr>";
       })
     ];
+    # launch dap-ui on debugger commands
+    extraConfigLua = ''
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    '';
   };
 }
