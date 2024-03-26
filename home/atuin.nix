4b3c6e687382bcd1db350851f950dfadcd44e11a
Simon Walker <s.r.walker101@googlemail.com>
2024-03-26 09:00:56 +0000

    Remove atuin
    Former-commit-id: 7e27a8927802d635304e59a8ad8642079a1e219f

--- home/atuin.nix
+++ home/atuin.nix
@@ -1,7 +1,7 @@
 # Default configuration file: https://github.com/atuinsh/atuin/blob/803b2fed5ce33cd08bf0c07b376c9a3f1e88f8ba/atuin-client/config.toml#L4
 {
   programs.atuin = {
-    enable = true;
+    enable = false;
     settings = {
       enter_accept = false;
       dialect = "uk";
