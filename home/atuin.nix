e924bdb5c1df124f8577383095d3fa2e45dd8d3e
Simon Walker <s.r.walker101@googlemail.com>
2024-04-04 14:05:31 +0100

    Revert "Remove atuin"
    This reverts commit 4b3c6e687382bcd1db350851f950dfadcd44e11a [formerly 7e27a8927802d635304e59a8ad8642079a1e219f].
    
    
    Former-commit-id: d1fdd63c589bda50da279bec0c942d287713bf64

--- home/atuin.nix
+++ home/atuin.nix
@@ -1,7 +1,7 @@
 # Default configuration file: https://github.com/atuinsh/atuin/blob/803b2fed5ce33cd08bf0c07b376c9a3f1e88f8ba/atuin-client/config.toml#L4
 {
   programs.atuin = {
-    enable = false;
+    enable = true;
     settings = {
       enter_accept = false;
       dialect = "uk";
