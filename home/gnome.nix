bdd58e55c1addc883400dda5e6fb7120aa7479ea
Simon Walker <s.r.walker101@googlemail.com>
2024-04-24 11:37:08 +0100

    Remove slack shortcut

--- home/gnome.nix
+++ home/gnome.nix
@@ -56,26 +56,21 @@ in {
   config = mkIf cfg.enable {
     dconf.settings = {
       "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-        binding = "<Alt><Super>s";
-        command = mkShortcutCommand "slack";
-        name = "Slack";
-      };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
         binding = "<Alt><Super>t";
         command = mkShortcutCommand terminal;
         name = "Terminal";
       };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
         binding = "<Alt><Super>c";
         command = mkShortcutCommand browser;
         name = "Browser";
       };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
         binding = "<Alt><Super>e";
         command = mkShortcutCommand "obsidian";
         name = "Notes";
       };
-      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
         binding = "<Alt><Super>r";
         command = mkShortcutCommand "zeal";
         name = "Documentation";
@@ -95,7 +90,6 @@ in {
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
           "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
-          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
         ];
       };
       "org/gnome/shell/extensions/just-perfection" = {
