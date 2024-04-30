b153f612391e96cb5828a29a329655acd8e65b26
Simon Walker <s.r.walker101@googlemail.com>
2024-04-30 11:47:39 +0100

    Disable diffview

--- home/nixvim/diffview.nix
+++ home/nixvim/diffview.nix
@@ -1,5 +1,17 @@
 {
-  programs.nixvim.plugins.diffview = {
-    enable = true;
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.me.nixvim.diffview;
+in {
+  options.me.nixvim.diffview = {
+    enable = mkEnableOption "DiffView";
+  };
+  config = mkIf cfg.enable {
+    programs.nixvim.plugins.diffview = {
+      enable = true;
+    };
   };
 }
