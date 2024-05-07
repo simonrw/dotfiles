8ecf263a49d7beb9c42e62c38d86b264791913b5
Simon Walker <s.r.walker101@googlemail.com>
2024-05-07 15:03:44 +0100

    Move bindings to nix config
    Former-commit-id: 78e7f712cb2c0a37a58b6734e658f273bbedff49

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -31,6 +31,55 @@ in {
           "$mainMod, space, exec, wofi --show drun"
           "$mainMod, return, exec, ${defaults.terminal}"
           "$mainMod, C, killactive"
+          "$mainMod, c, ${defaults.browser}"
+          # Move focus with mainMod + arrow keys
+          "$mainMod, left, movefocus, l"
+          "$mainMod, right, movefocus, r"
+          "$mainMod, up, movefocus, u"
+          "$mainMod, down, movefocus, d"
+          "$mainMod, h, movefocus, l"
+          "$mainMod, l, movefocus, r"
+          "$mainMod, k, movefocus, u"
+          "$mainMod, j, movefocus, d"
+          # Example binds, see https://wiki.hyprland.org/Configuring/Binds/ for more
+          "$mainMod, Q, exec, alacritty"
+          "$mainMod, C, killactive"
+          "SUPER_SHIFT, M, exit"
+          "$mainMod, V, togglefloating"
+          "$mainMod, R, exec, wofi --show drun"
+          "$mainMod, P, pseudo"
+          "$mainMod, J, togglesplit"
+
+          # Switch workspaces with mainMod + [0-9]
+          "$mainMod, 1, workspace, 1"
+          "$mainMod, 2, workspace, 2"
+          "$mainMod, 3, workspace, 3"
+          "$mainMod, 4, workspace, 4"
+          "$mainMod, 5, workspace, 5"
+          "$mainMod, 6, workspace, 6"
+          "$mainMod, 7, workspace, 7"
+          "$mainMod, 8, workspace, 8"
+          "$mainMod, 9, workspace, 9"
+          "$mainMod, 0, workspace, 10"
+
+          # Move active window to a workspace with mainMod + SHIFT + [0-9]
+          "$mainMod SHIFT, 1, movetoworkspace, 1"
+          "$mainMod SHIFT, 2, movetoworkspace, 2"
+          "$mainMod SHIFT, 3, movetoworkspace, 3"
+          "$mainMod SHIFT, 4, movetoworkspace, 4"
+          "$mainMod SHIFT, 5, movetoworkspace, 5"
+          "$mainMod SHIFT, 6, movetoworkspace, 6"
+          "$mainMod SHIFT, 7, movetoworkspace, 7"
+          "$mainMod SHIFT, 8, movetoworkspace, 8"
+          "$mainMod SHIFT, 9, movetoworkspace, 9"
+          "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+          # switch between previous workspaces
+          "$mainMod, tab, focuscurrentorlast"
+
+          # Scroll through existing workspaces with mainMod + scroll
+          "$mainMod, mouse_down, workspace, e+1"
+          "$mainMod, mouse_up, workspace, e-1"
         ];
       };
 
