55c3a1dbaa77def794416864c899841845abf6f2
Simon Walker <s.r.walker101@googlemail.com>
2024-05-18 09:44:52 +0100

    Use nixpkgs-unstable (#124)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
