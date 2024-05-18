7513cf3bb3b740b151954abda3eedd23e5dbfaba
Simon Walker <s.r.walker101@googlemail.com>
2024-05-18 20:49:31 +0100

    Reapply "Use nord theme"
    This reverts commit d15b152ad2a42250c58671b3002f9292b5c91259.

--- home/home.nix
+++ home/home.nix
@@ -92,13 +92,13 @@ in {
         pkgs.monaspace
       ];
       aws.enable = true;
-      theme = "papercolor";
+      theme = "nord";
       vscode-theme = "Dracula";
       defaults = {
         browser = "brave";
         terminal = "alacritty";
       };
-      is-dark-theme = false;
+      is-dark-theme = true;
     }
     // (
       if isLinux
