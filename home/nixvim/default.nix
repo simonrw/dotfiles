fa1f67b3a80a01bdcf8d8d6b59228afbf2c981b4
Simon Walker <s.r.walker101@googlemail.com>
2024-05-23 15:41:47 +0100

    Switch back to vim-test

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -44,8 +44,8 @@ in {
       context.enable = true;
       octo-nvim.enable = true;
       oil.enable = true;
-      vim-test.enable = false;
-      neotest.enable = true;
+      vim-test.enable = true;
+      neotest.enable = false;
     };
     # defaults
     programs.nixvim = {
