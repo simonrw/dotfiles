5cd816cf08e7d72b66c17b7dbab665f72f1bb0f7
Simon Walker <s.r.walker101@googlemail.com>
2024-06-04 10:48:06 +0100

    Manage darwin wallpaper with home-manager
    Former-commit-id: efef0edc131aef0f704044d206bfa6dde1d6fcab

--- home/wallpaper.nix
+++ home/wallpaper.nix
@@ -1,7 +1,21 @@
-{lib, ...}:
-with lib; {
+{
+  lib,
+  isDarwin,
+  config,
+  ...
+}:
+with lib; let
+  wallpaper = config.me.wallpaper;
+in {
   options.me.wallpaper = mkOption {
     type = types.path;
     description = "Path to a wallpaper";
+  };
+
+  config = mkIf isDarwin {
+    # activate the wallpaper
+    home.activation.setWallpaper = hm.dag.entryAfter ["WriteBoundary"] ''
+      osascript -e "tell application \"System Events\" to tell every desktop to set picture to \"${wallpaper}\" as POSIX file"
+    '';
   };
 }
