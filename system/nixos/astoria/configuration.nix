d6facbfb7dbc6ba638661e63e0062ed2411842e7
Simon Walker <s.r.walker101@googlemail.com>
2024-07-15 11:38:37 +0100

    Remove "sound" key as it is not used
    Former-commit-id: 06b4149d4951a0c5c595cd03a229016ad0e4595a

--- system/nixos/astoria/configuration.nix
+++ system/nixos/astoria/configuration.nix
@@ -182,7 +182,6 @@ in {
     services.printing.enable = true;
 
     # Enable sound with pipewire.
-    sound.enable = true;
     hardware.pulseaudio.enable = false;
     security.rtkit.enable = true;
     services.pipewire = {
