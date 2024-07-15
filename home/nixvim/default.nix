9fef98aa81def747f3d788c45ec89ef4be4c28ae
Simon Walker <s.r.walker101@googlemail.com>
2024-07-15 03:44:16 +0100

    no status bar
    Former-commit-id: 58790198f9c38a76b34d631a8370870f4064d54e

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -54,7 +54,7 @@ in {
       vim-test.enable = true;
       neotest.enable = false;
       notifications.enable = false;
-      statusbar.enable = true;
+      statusbar.enable = false;
       mini.enable = true;
     };
     # defaults
