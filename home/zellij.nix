fb81d5aa39a403910a8cefff7e2583ff4f1b4dba
Simon Walker <s.r.walker101@googlemail.com>
2024-07-19 13:27:39 +0100

    Hide zellij config behind enable flag

--- home/zellij.nix
+++ home/zellij.nix
@@ -1,11 +1,22 @@
-{...}: {
-  programs.zellij = {
-    enable = true;
-    enableFishIntegration = true;
-  };
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.me.zellij;
+in {
+  options.me.zellij.enable = mkEnableOption "zellij";
 
-  xdg.configFile."zellij" = {
-    source = ./zellij;
-    recursive = true;
+  config = mkIf cfg.enable {
+    programs.zellij = {
+      enable = true;
+      enableFishIntegration = true;
+    };
+
+    xdg.configFile."zellij" = {
+      source = ./zellij;
+      recursive = true;
+    };
   };
 }
