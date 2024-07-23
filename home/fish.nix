31e18137a9a7cbd99defddc66571cf108b5c3035
Simon Walker <s.r.walker101@googlemail.com>
2024-07-23 17:39:55 +0100

    Add timing right prompt

--- home/fish.nix
+++ home/fish.nix
@@ -179,6 +179,13 @@ in {
           echo -n -s {$nix_shell_str} {$suffix_color} {$suffix} {$normal} " "
         '';
       };
+      fish_right_prompt = {
+        body = ''
+          set -l fish_color_status_rprompt (set_color '#6e738d')
+          set -l normal (set_color normal)
+          echo -n {$fish_color_status_rprompt} $CMD_DURATION ms{$normal}
+        '';
+      };
     };
   };
 }
