93dd8e1168ba5b6a302c81856a669425b12a011b
Simon Walker <s.r.walker101@googlemail.com>
2024-08-07 05:19:09 +0100

    Add realise-symlink
    https://kokada.capivaras.dev/blog/quick-bits-realise-nix-symlinks/

--- flake.nix
+++ flake.nix
@@ -85,6 +85,31 @@
         wlman = final.callPackage ./derivations/wlman {};
         # temporary until this exists in nixpkgs
         git-spice = final.callPackage ./derivations/git-spice {};
+        # https://kokada.capivaras.dev/blog/quick-bits-realise-nix-symlinks/
+        realise-symlink = final.writeShellApplication {
+            name = "realise-symlink";
+            runtimeInputs = with final; [ coreutils ];
+            text = ''
+              for file in "$@"; do
+                if [[ -L "$file" ]]; then
+                  if [[ -d "$file" ]]; then
+                    tmpdir="''${file}.tmp"
+                    mkdir -p "$tmpdir"
+                    cp --verbose --recursive "$file"/* "$tmpdir"
+                    unlink "$file"
+                    mv "$tmpdir" "$file"
+                    chmod --changes --recursive +w "$file"
+                  else
+                    cp --verbose --remove-destination "$(readlink "$file")" "$file"
+                    chmod --changes +w "$file"
+                  fi
+                else
+                  >&2 echo "Not a symlink: $file"
+                  exit 1
+                fi
+              done
+            '';
+          };
       })
       # override the version of xattr for poetry
       (
