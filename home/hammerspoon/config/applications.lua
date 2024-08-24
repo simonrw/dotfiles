d95c67efd70ac0985f56640e3d431b37f7191313
Simon Walker <s.r.walker101@googlemail.com>
2024-08-24 23:54:03 +0100

    Use zen browser on macos

--- home/hammerspoon/config/applications.lua
+++ home/hammerspoon/config/applications.lua
@@ -3,7 +3,7 @@ local applications = {
         name = "Alacritty",
     },
     browser = {
-        name = "Google Chrome",
+        name = "Zen Browser",
     },
     email = {
         name = "Mail",
