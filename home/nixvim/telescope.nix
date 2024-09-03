1cb07d8ad2d6d80a0614e1ce9018a2ee387d6d04
Simon Walker <s.r.walker101@googlemail.com>
2024-09-03 14:03:42 +0100

    Update telescope config

--- home/nixvim/telescope.nix
+++ home/nixvim/telescope.nix
@@ -1,7 +1,7 @@
 {
   programs.nixvim.plugins.telescope = {
     enable = true;
-    defaults = {
+    settings.defaults = {
       layout_strategy = "horizontal";
       layout_config.prompt_position = "top";
       sorting_strategy = "ascending";
@@ -13,36 +13,34 @@
         override_generic_sorter = true;
         override_file_sorter = true;
         case_mode = "smart_case";
-      };
-    };
-    extraOptions = {
-      pickers = {
-        git_files = {
-          disable_devicons = true;
-        };
-        find_files = {
-          disable_devicons = true;
-        };
-        buffers = {
-          disable_devicons = true;
-        };
-        live_grep = {
-          disable_devicons = true;
-        };
-        current_buffer_fuzzy_find = {
-          disable_devicons = true;
-        };
-        lsp_definitions = {
-          disable_devicons = true;
-        };
-        lsp_references = {
-          disable_devicons = true;
-        };
-        diagnostics = {
-          disable_devicons = true;
-        };
-        lsp_dynamic_workspace_symbols = {
-          disable_devicons = true;
+        pickers = {
+          git_files = {
+            disable_devicons = true;
+          };
+          find_files = {
+            disable_devicons = true;
+          };
+          buffers = {
+            disable_devicons = true;
+          };
+          live_grep = {
+            disable_devicons = true;
+          };
+          current_buffer_fuzzy_find = {
+            disable_devicons = true;
+          };
+          lsp_definitions = {
+            disable_devicons = true;
+          };
+          lsp_references = {
+            disable_devicons = true;
+          };
+          diagnostics = {
+            disable_devicons = true;
+          };
+          lsp_dynamic_workspace_symbols = {
+            disable_devicons = true;
+          };
         };
       };
     };
