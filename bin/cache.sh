a652d8ca376d4fb5cce726d3610e32eebc1e17bd
Simon Walker <s.r.walker101@googlemail.com>
2024-09-06 12:55:20 +0100

    Cache command uses nom
    Former-commit-id: 3053dcfeefd92d4c9af135d12f0c7bc87d953b96

--- bin/cache.sh
+++ bin/cache.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 . "$(dirname "$(readlink -f "$0")")/utils.sh"
 NIXARCH="$(nixarch)"
+NIX=${nom:-nix}
 
 case ${NIXARCH} in
     *-linux)
@@ -11,7 +12,7 @@ case ${NIXARCH} in
         exit 1
         ;;
     *-darwin)
-        nix --extra-experimental-features "nix-command flakes" build ".#darwinConfigurations.${HOSTTARGET}.system" --json \
+        ${NIX} --extra-experimental-features "nix-command flakes" build ".#darwinConfigurations.${HOSTTARGET}.system" --json \
             | jq -r '.[].outputs | to_entries[].value' \
             | cachix push mindriot101-home
         ;;
