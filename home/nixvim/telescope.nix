cc56c23c9c21e5547e4c958596e37b40c8970a29
Simon Walker <s.r.walker101@googlemail.com>
2024-09-16 11:05:43 +0100

    fix devicons disabling for telescope

--- home/nixvim/telescope.nix
+++ home/nixvim/telescope.nix
@@ -1,10 +1,41 @@
 {
   programs.nixvim.plugins.telescope = {
     enable = true;
-    settings.defaults = {
-      layout_strategy = "horizontal";
-      layout_config.prompt_position = "top";
-      sorting_strategy = "ascending";
+    settings = {
+      pickers = {
+        git_files = {
+          disable_devicons = true;
+        };
+        find_files = {
+          disable_devicons = true;
+        };
+        buffers = {
+          disable_devicons = true;
+        };
+        live_grep = {
+          disable_devicons = true;
+        };
+        current_buffer_fuzzy_find = {
+          disable_devicons = true;
+        };
+        lsp_definitions = {
+          disable_devicons = true;
+        };
+        lsp_references = {
+          disable_devicons = true;
+        };
+        diagnostics = {
+          disable_devicons = true;
+        };
+        lsp_dynamic_workspace_symbols = {
+          disable_devicons = true;
+        };
+      };
+      defaults = {
+        layout_strategy = "horizontal";
+        layout_config.prompt_position = "top";
+        sorting_strategy = "ascending";
+      };
     };
     extensions.fzf-native = {
       enable = true;
@@ -13,35 +44,6 @@
         override_generic_sorter = true;
         override_file_sorter = true;
         case_mode = "smart_case";
-        pickers = {
-          git_files = {
-            disable_devicons = true;
-          };
-          find_files = {
-            disable_devicons = true;
-          };
-          buffers = {
-            disable_devicons = true;
-          };
-          live_grep = {
-            disable_devicons = true;
-          };
-          current_buffer_fuzzy_find = {
-            disable_devicons = true;
-          };
-          lsp_definitions = {
-            disable_devicons = true;
-          };
-          lsp_references = {
-            disable_devicons = true;
-          };
-          diagnostics = {
-            disable_devicons = true;
-          };
-          lsp_dynamic_workspace_symbols = {
-            disable_devicons = true;
-          };
-        };
       };
     };
     keymaps = {
