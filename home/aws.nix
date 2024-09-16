f932e55a765fc8213abad5e0e7cf81471547d753
Simon Walker <s.r.walker101@googlemail.com>
2024-09-16 16:59:09 +0100

    Remove fish plugin
    It does not do much

--- home/aws.nix
+++ home/aws.nix
@@ -20,17 +20,5 @@ in {
     home.packages = [
       cfg.package
     ];
-    programs.fish.plugins = mkIf config.programs.fish.enable [
-      # my fork to stop printing on error when aws_completer cannot be found
-      {
-        name = "plugin-aws";
-        src = pkgs.fetchFromGitHub {
-          owner = "simonrw";
-          repo = "plugin-aws";
-          rev = "bdbf81a1260ace6bcafd129dcfef2c628a937925";
-          hash = "sha256-z6hFVk3H5vwxqwolIqWSO8/73hiCHClJeA4gNZw7B4Q=";
-        };
-      }
-    ];
   };
 }
