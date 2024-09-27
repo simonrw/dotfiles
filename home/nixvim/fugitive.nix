9e905b89a280e590215647e14173a0630aca2029
Simon Walker <s.r.walker101@googlemail.com>
2024-09-27 21:03:40 +0100

    Move fugitive code to external module
    Former-commit-id: f2b6601f6b3c9ce8a1a671d253b6e3ef5d3140b3

---
+++ home/nixvim/fugitive.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  config,
+  ...
+}:
+with lib; let
+  cfg = config.me.nixvim.fugitive;
+
+  keymap = {
+    key,
+    action,
+    mode ? "n",
+    lua ? false,
+  }: {
+    inherit action mode key lua;
+    options = {
+      noremap = true;
+      silent = true;
+    };
+  };
+in {
+  options.me.nixvim.fugitive = {
+    enable = mkEnableOption "fugitive";
+  };
+
+  config = mkIf cfg.enable {
+    plugins.fugitive.enable = true;
+    autoCmd = [
+      # support closing fugitive window with 'q'
+      {
+        event = ["FileType"];
+        pattern = ["fugitive"];
+        command = "nmap <buffer> q gq";
+      }
+    ];
+    keymaps = [
+      (keymap {
+        key = "<leader>gc";
+        action = ":Git commit -v<cr>";
+      })
+      (keymap {
+        key = "<leader>gd";
+        action = ":Gvdiff<cr>";
+      })
+      (keymap {
+        key = "<leader>gw";
+        action = ":Gwrite<cr>";
+      })
+      (keymap {
+        key = "<leader>gr";
+        action = ":Gread<cr>";
+      })
+      (keymap {
+        key = "gs";
+        action = ":Git<cr>";
+      })
+      (keymap {
+        key = "<leader>ga";
+        action = ":Git commit -v --amend<cr>";
+      })
+    ];
+  };
+}
