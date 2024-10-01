ef465e60c760396e2fbb06811565b1bac941e7c3
Simon Walker <s.r.walker101@googlemail.com>
2024-10-01 22:05:21 +0100

    Update emacs configuration

--- home/emacs.nix
+++ home/emacs.nix
@@ -1,22 +1,4 @@
-{pkgs, ...}: {
-  programs.emacs = {
-    enable = false;
-    package = pkgs.emacs;
-    extraPackages = epkgs: [
-      epkgs.magit
-      epkgs.vterm
-      epkgs.nix-mode
-      epkgs.rust-mode
-      epkgs.which-key
-      epkgs.company
-      epkgs.projectile
-      epkgs.ripgrep
-      epkgs.blacken
-      epkgs.direnv
-      epkgs.rustic
-      epkgs.nord-theme
-      epkgs.evil
-    ];
-    extraConfig = builtins.readFile ./emacs/init.el;
-  };
+{... }: {
+  home.file.".emacs.d/init.el".source = ./emacs/init.el;
+  home.file.".emacs.d/custom.el".source = ./emacs/custom.el;
 }
---
+++ home/emacs/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("dfab4d4e2904967f208647d14b15015a613c4ab904db491116a7c0968e44a115" default))
+ '(package-selected-packages '(catppuccin-theme smex)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
