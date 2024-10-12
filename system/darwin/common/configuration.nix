e8de74651822d87077ac7a1ce5cdcd1c8b82d5cf
Simon Walker <s.r.walker101@googlemail.com>
2024-10-12 23:37:19 +0100

    Auto-hide dock
    Former-commit-id: fce38904709b7275b2969e40b5060199540e6424

--- system/darwin/common/configuration.nix
+++ system/darwin/common/configuration.nix
@@ -62,7 +62,7 @@
   # https://nixcademy.com/2024/01/15/nix-on-macos/
   system.defaults = {
     dock = {
-      autohide = false;
+      autohide = true;
       mru-spaces = false;
       orientation = "bottom";
       tilesize = {
