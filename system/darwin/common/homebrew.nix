dd8a34a60eb10191170db9d2e58bba202fe04cdf
Simon Walker <s.r.walker101@googlemail.com>
2024-10-15 14:29:29 +0100

    Revert "Use nix-homebrew to manage homebrew installations"
    This reverts commit d58459223eef461ce9dc87416f57d708b6f3e821.

--- system/darwin/common/homebrew.nix
+++ system/darwin/common/homebrew.nix
@@ -53,18 +53,4 @@
       Tailscale = 1475387142;
     };
   };
-  # declarative homebrew configuration
-  nix-homebrew = {
-    # Install Homebrew under the default prefix
-    enable = true;
-
-    # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
-    enableRosetta = true;
-
-    # User owning the Homebrew prefix
-    user = "simon";
-
-    # Automatically migrate existing Homebrew installations
-    autoMigrate = true;
-  };
 }
