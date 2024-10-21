d40f05f1f41942c0f2f70513584881707b67b99b
Simon Walker <s.r.walker101@googlemail.com>
2024-10-21 09:37:53 +0100

    Track ghostty config

---
+++ home/ghostty.nix
@@ -0,0 +1,13 @@
+{ lib, config, ... }:
+with lib;
+let
+  cfg = config.me.ghostty;
+in
+{
+  options.me.ghostty.enable = mkEnableOption "ghostty configuration";
+
+  config = mkIf cfg.enable {
+    # not xdg as this path is fixed even on macos
+    home.file.".config/ghostty/config".source = ./ghostty/config;
+  };
+}
