1f096b008c8fc855fa12304578712f449fdbf311
Simon Walker <s.r.walker101@googlemail.com>
2024-10-21 16:16:24 +0100

    Disable emacs configuration management

--- home/emacs.nix
+++ home/emacs.nix
@@ -1,4 +1,13 @@
-{... }: {
-  home.file.".emacs.d/init.el".source = ./emacs/init.el;
-  home.file.".emacs.d/custom.el".source = ./emacs/custom.el;
+{ lib, config, ... }:
+with lib;
+let
+  cfg = config.me.emacs;
+in
+{
+  options.me.emacs.enable = mkEnableOption "emacs configuration";
+
+  config = mkIf cfg.enable {
+    home.file.".emacs.d/init.el".source = ./emacs/init.el;
+    home.file.".emacs.d/custom.el".source = ./emacs/custom.el;
+  };
 }
