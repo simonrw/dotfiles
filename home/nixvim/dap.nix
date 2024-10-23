c0a7bcca5fd6c63397ffe4da662323e741fc0377
Simon Walker <s.r.walker101@googlemail.com>
2024-10-23 09:51:23 +0100

    Make dap configurable

--- home/nixvim/dap.nix
+++ home/nixvim/dap.nix
@@ -1,4 +1,8 @@
-{...}: let
+{ lib, config, ...}: 
+with lib;
+let
+  cfg = config.me.nixvim.dap;
+
   keymap = {
     key,
     action,
@@ -12,45 +16,48 @@
     };
   };
 in {
-  plugins.dap = {
-    enable = true;
-    extensions.dap-go.enable = true;
-    extensions.dap-python.enable = true;
-    extensions.dap-ui = {
+  options.me.nixvim.dap.enable = mkEnableOption "DAP debugger";
+
+  config = mkIf cfg.enable {
+    plugins.dap = {
       enable = true;
-      controls.enabled = false;
+      extensions.dap-go.enable = true;
+      extensions.dap-python.enable = true;
+      extensions.dap-ui = {
+        enable = true;
+        controls.enabled = false;
+      };
+      extensions.dap-virtual-text.enable = true;
     };
-    extensions.dap-virtual-text.enable = true;
-  };
-  keymaps = [
-    (keymap {
-      key = "<f7>";
-      action = ":lua require('dap').step_into()<cr>";
-    })
-    (keymap {
-      key = "<f8>";
-      action = ":lua require('dap').step_over()<cr>";
-    })
-    (keymap {
-      key = "<F9>";
-      action = ":lua require('dap').continue()<cr>";
-    })
-    (keymap {
-      key = "<Shift><F9>";
-      action = ":lua require('dap').step_out()<cr>";
-    })
-    (keymap {
-      key = "<leader>b";
-      action = ":lua require('dap').toggle_breakpoint()<cr>";
-    })
-    # Eval var under cursor
-    (keymap {
-      key = "<leader>?";
-      action = ":lua require('dapui').eval(nil, { enter = true })<cr>";
-    })
-  ];
-  # launch dap-ui on debugger commands
-  extraConfigLua = ''
+    keymaps = [
+      (keymap {
+        key = "<f7>";
+        action = ":lua require('dap').step_into()<cr>";
+      })
+      (keymap {
+        key = "<f8>";
+        action = ":lua require('dap').step_over()<cr>";
+      })
+      (keymap {
+        key = "<F9>";
+        action = ":lua require('dap').continue()<cr>";
+      })
+      (keymap {
+        key = "<Shift><F9>";
+        action = ":lua require('dap').step_out()<cr>";
+      })
+      (keymap {
+        key = "<leader>b";
+        action = ":lua require('dap').toggle_breakpoint()<cr>";
+      })
+      # Eval var under cursor
+      (keymap {
+        key = "<leader>?";
+        action = ":lua require('dapui').eval(nil, { enter = true })<cr>";
+      })
+    ];
+    # launch dap-ui on debugger commands
+    extraConfigLua = ''
     local dap, dapui = require("dap"), require("dapui")
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
@@ -66,5 +73,6 @@ in {
     end
 
     require('dap-python').test_runner = "pytest"
-  '';
+    '';
+  };
 }
