5bb45e73820fac5565fed9f232c95acb57bb78fc
Simon Walker <s.r.walker101@googlemail.com>
2024-11-01 10:03:33 +0000

    linux: fix CI
    Former-commit-id: 89d663448b993d740c461c8c6f01dc83c8ff6936

--- derivations/zizmor/default.nix
+++ derivations/zizmor/default.nix
@@ -1,7 +1,20 @@
 { rustPlatform
 , fetchFromGitHub
 , darwin
+, stdenv
+, lib
 }:
+let
+    packages = [];
+
+    darwin-packages = with darwin.apple_sdk.frameworks; [
+          SystemConfiguration
+      ];
+
+    linux-packages = [];
+
+    all-packages = packages ++ (lib.optionals stdenv.isDarwin darwin-packages) ++ (lib.optionals stdenv.isLinux linux-packages);
+in
 rustPlatform.buildRustPackage rec {
   pname = "zizmor";
   version = "0.1.4";
@@ -13,10 +26,7 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-S2B4GQAqx4t9AZf3QDUhzku68j0buZdW0cLhmOiRssk=";
   };
 
-  buildInputs = [
-  ] ++ (with darwin.apple_sdk.frameworks; [
-      SystemConfiguration
-  ]);
+  buildInputs = all-packages;
 
   cargoHash = "sha256-hoZXR+zYuK/r4/r3QwIhTmMTCs5M0lMACH4QPEq07ZU=";
 }
