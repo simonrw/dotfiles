afea4d115c2b0ef638ca9ba721c5af4a9e44b3d9
Simon Walker <s.r.walker101@googlemail.com>
2024-11-25 22:05:41 +0000

    direnv: configure
    https://github.com/direnv/direnv/blob/master/man/direnv.toml.1.md
    
    Former-commit-id: 5ce5ce32fb20fad08f2b591b0f03ebf1a56e3380

--- home/direnv.nix
+++ home/direnv.nix
@@ -4,5 +4,14 @@
     nix-direnv = {
       enable = true;
     };
+    silent = true;
+    config = {
+      global = {
+        load_dotenv = true;
+        strict_env = true;
+        warn_timeout = "0";
+        hide_env_diff = true;
+      };
+    };
   };
 }
