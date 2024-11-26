97deb176066192890d290920951a9fad8d0ec07c
Simon Walker <s.r.walker101@googlemail.com>
2024-11-26 21:06:46 +0000

    darwin: add host-specific casks
    Former-commit-id: ddfd6db4a7eedda26c751e92b697aab908682bfa

--- system/darwin/common/homebrew.nix
+++ system/darwin/common/homebrew.nix
@@ -1,50 +1,58 @@
-{...}: {
+{hostname, ...}: let
+  host-specific-casks =
+    {
+      mba = [];
+      mm = ["wacom-tablet"];
+    }
+    .${hostname};
+in {
   homebrew = {
     enable = true;
     # remove any homewbrew packages not installed via nix-darwin
     onActivation.cleanup = "uninstall";
-    casks = [
-      "1password"
-      "1password-cli"
-      "alacritty"
-      "barrier"
-      "brave-browser"
-      "chatgpt"
-      "cursor"
-      "dash"
-      "deskpad"
-      "discord"
-      "docker"
-      "element"
-      "emacs"
-      "firefox"
-      "gimp"
-      "inkscape"
-      "karabiner-elements"
-      "notion"
-      "obs"
-      "obsidian"
-      "pocket-casts"
-      "pycharm-ce"
-      "raycast"
-      "shotcut"
-      "slack"
-      "steam"
-      "sublime-merge"
-      "sublime-text"
-      "tabby"
-      "todoist"
-      "utm"
-      "visual-studio-code"
-      "vlc"
-      "vmware-fusion"
-      "wacom-tablet"
-      "wireshark"
-      "xquartz"
-      "yt-music"
-      "yubico-yubikey-manager"
-      "zed"
-    ];
+    casks =
+      [
+        "1password"
+        "1password-cli"
+        "alacritty"
+        "barrier"
+        "brave-browser"
+        "chatgpt"
+        "cursor"
+        "dash"
+        "deskpad"
+        "discord"
+        "docker"
+        "element"
+        "emacs"
+        "firefox"
+        "gimp"
+        "inkscape"
+        "karabiner-elements"
+        "notion"
+        "obs"
+        "obsidian"
+        "pocket-casts"
+        "pycharm-ce"
+        "raycast"
+        "shotcut"
+        "slack"
+        "steam"
+        "sublime-merge"
+        "sublime-text"
+        "tabby"
+        "todoist"
+        "utm"
+        "visual-studio-code"
+        "vlc"
+        "vmware-fusion"
+        "wireshark"
+        "xquartz"
+        "yt-music"
+        "yubico-yubikey-manager"
+        "zed"
+      ]
+      ++ host-specific-casks;
     brews = [
       "cargo-instruments"
       "cftail"
