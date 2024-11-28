fc31de498a7f14fc8d2bce4b609aee48626c1e2e
Simon Walker <s.r.walker101@googlemail.com>
2024-11-28 16:47:04 +0000

    neovim: disable fancy statusbar

--- home/nixvim/default.nix
+++ home/nixvim/default.nix
@@ -56,7 +56,7 @@
       vim-test.enable = true;
       neotest.enable = false;
       notifications.enable = false;
-      statusbar.enable = true;
+      statusbar.enable = false;
       mini.enable = false;
       which-key.enable = false;
       diffview.enable = false;
