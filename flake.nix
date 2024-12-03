ab4dae3bd72727fc00532336b7f1cd4b57159da7
Simon Walker <s.r.walker101@googlemail.com>
2024-12-03 13:33:36 +0000

    Update and pin to stable release
    Former-commit-id: 59b61a552b3bae5cc4648330941883eb7aac1a50

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "home-manager configuration";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixvim = {
       url = "github:nix-community/nixvim";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
     };
     nur.url = "github:nix-community/NUR";
     darwin = {
