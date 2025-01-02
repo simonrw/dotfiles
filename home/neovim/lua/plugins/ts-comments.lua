801591214be9b51c616f3b0627d2ad0ba22de598
Simon Walker <s.r.walker101@googlemail.com>
2025-01-02 23:23:33 +0000

    neovim: add improved commenting functionality
    Former-commit-id: dca7692547264cc3fb9a12a7d24ee1b4a6e9b1b6

---
+++ home/neovim/lua/plugins/ts-comments.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/ts-comments.nvim",
+    event = "VeryLazy",
+    enabled = vim.fn.has("nvim-0.10.0") == 1,
+    opts = {
+      lang = {
+        terraform = "# %s",
+      },
+    },
+  },
+}
