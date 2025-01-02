4eb618bbb9c571fc56932c3ca4a1e4328cb29da5
Simon Walker <s.r.walker101@googlemail.com>
2025-01-02 16:29:57 +0000

    neovim: fix keymaps for vim-test
    Former-commit-id: 89199674c3c059d9f9a79f7f15d584da3493664f

--- home/neovim/lua/plugins/vim-test.lua
+++ home/neovim/lua/plugins/vim-test.lua
@@ -1,22 +1,32 @@
 return {
-  {
-    "vim-test/vim-test",
-    config = function()
+    {
+        "vim-test/vim-test",
+        config = function()
 
-      vim.g["test#python#runner"] = "pytest"
-      vim.g["test#javascript#reactscripts#options"] = "--watchAll=false"
-      vim.g["test#strategy"] = "basic"
+            vim.g["test#python#runner"] = "pytest"
+            vim.g["test#javascript#reactscripts#options"] = "--watchAll=false"
+            vim.g["test#strategy"] = "basic"
 
-      vim.keymap.set("n", "tf", function() vim.cmd([[ :update|:TestFile<Cr> ]]) end,
-        { noremap = true, silent = true })
-      vim.keymap.set("n", "tl", function() vim.cmd([[ :update|:TestLast<cr> ]]) end,
-        { noremap = true, silent = true })
-      vim.keymap.set("n", "tn", function() vim.cmd([[ :update|:TestNearest<cr> ]]) end,
-        { noremap = true, silent = true })
-      vim.keymap.set("n", "ta", function() vim.cmd([[ :update|:TestSuite<Cr> ]]) end,
-        { noremap = true, silent = true })
-      vim.keymap.set("n", "ts", function() vim.cmd([[ :update|:TestSuite<Cr> ]]) end,
-        { noremap = true, silent = true })
-    end,
-  },
+            vim.keymap.set("n", "tf", function()
+                vim.cmd("update")
+                vim.cmd("TestFile")
+            end, {noremap = true, silent = true})
+            vim.keymap.set("n", "tl", function()
+                vim.cmd("update")
+                vim.cmd("TestLast")
+            end, {noremap = true, silent = true})
+            vim.keymap.set("n", "tn", function()
+                vim.cmd("update")
+                vim.cmd("TestNearest")
+            end, {noremap = true, silent = true})
+            vim.keymap.set("n", "ta", function()
+                vim.cmd("update")
+                vim.cmd("TestSuite")
+            end, {noremap = true, silent = true})
+            vim.keymap.set("n", "ts", function()
+                vim.cmd("update")
+                vim.cmd("TestSuite")
+            end, {noremap = true, silent = true})
+        end
+    }
 }
