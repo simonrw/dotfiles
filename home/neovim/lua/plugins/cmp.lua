cfa09464dd3c2244a6f199f96eb1bc5754349cf6
Simon Walker <s.r.walker101@googlemail.com>
2025-01-03 12:18:38 +0000

    neovim: update cmp configuration

--- home/neovim/lua/plugins/cmp.lua
+++ home/neovim/lua/plugins/cmp.lua
@@ -1,34 +1,39 @@
 return {
-    {
-        "hrsh7th/nvim-cmp",
-        dependencies = {
-            "neovim/nvim-lspconfig",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        config = function()
-            local cmp = require("cmp")
-
-            cmp.setup({
-                autoEnableSources = true,
-                completion = {
-                    completeopt = "menu.menuone,noinsert,noselect",
-                },
-                mapping = {
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                    ["<C-e>"] = cmp.config.disable,
-                    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
-                },
-                snippet = {
-                    expand = function(args) vim.snippet.expand(args.body) end,
-                },
-                sources = cmp.config.sources({
-                    { name = "nvim_lsp" },
-                }, {
-                    { name = "buffer" },
-                }),
-            })
-        end,
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-emoji",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
     },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        autoEnableSources = true,
+        completion = {
+          completeopt = "menu.menuone,noinsert,noselect",
+        },
+        mapping = {
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.config.disable,
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-y>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+        },
+        snippet = {
+          expand = function(args) vim.snippet.expand(args.body) end,
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp_signature_help' },
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+        }, {
+          { name = "emoji" },
+        }),
+      })
+    end,
+  },
 }
