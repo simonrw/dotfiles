aab90ec35b6969c9fc3539b6da6f05483bb6a559
Simon Walker <s.r.walker101@googlemail.com>
2025-01-06 13:55:57 +0000

    neovim: wip changes to blink
    Former-commit-id: 895952c91b2bbb4b7ef43bcc854438cb7d554b96

--- home/neovim/lua/plugins/blink.lua
+++ home/neovim/lua/plugins/blink.lua
@@ -30,6 +30,8 @@ return {
         nerd_font_variant = 'mono'
       },
 
+      signature = { enabled = true },
+
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
