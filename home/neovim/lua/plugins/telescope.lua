9a3caccc05522d2246daa73fd9effaa645399275
Simon Walker <s.r.walker101@googlemail.com>
2025-01-06 22:24:12 +0000

    neovim: add nice telescope bindings
    * jumplist
    * helptags
    
    and rewrite config in lua
    
    
    Former-commit-id: 5e9910e4a7567b5f49eb6d34b5ba57e83a57a55e

--- home/neovim/lua/plugins/telescope.lua
+++ home/neovim/lua/plugins/telescope.lua
@@ -1,90 +1,64 @@
 return {
-{
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      require("telescope").setup({
+        defaults = {
+          layout_config = { prompt_position = "top" },
+          layout_strategy = "horizontal",
+          sorting_strategy = "ascending"
         },
-        config = function()
-            require("telescope").setup({
-                defaults = {
-                    layout_config = {prompt_position = "top"},
-                    layout_strategy = "horizontal",
-                    sorting_strategy = "ascending"
-                },
-                extensions = {
-                    fzf = {
-                        case_mode = "smart_case",
-                        fuzzy = true,
-                        override_file_sorter = true,
-                        override_generic_sorter = true
-                    }
-                },
-                pickers = {
-                    buffers = {disable_devicons = true},
-                    current_buffer_fuzzy_find = {disable_devicons = true},
-                    diagnostics = {disable_devicons = true},
-                    find_files = {disable_devicons = true},
-                    git_files = {disable_devicons = true},
-                    live_grep = {disable_devicons = true},
-                    lsp_definitions = {disable_devicons = true},
-                    lsp_dynamic_workspace_symbols = {disable_devicons = true},
-                    lsp_references = {disable_devicons = true}
-                }
-            })
+        extensions = {
+          fzf = {
+            case_mode = "smart_case",
+            fuzzy = true,
+            override_file_sorter = true,
+            override_generic_sorter = true
+          }
+        },
+        pickers = {
+          buffers = { disable_devicons = true },
+          current_buffer_fuzzy_find = { disable_devicons = true },
+          diagnostics = { disable_devicons = true },
+          find_files = { disable_devicons = true },
+          git_files = { disable_devicons = true },
+          live_grep = { disable_devicons = true },
+          lsp_definitions = { disable_devicons = true },
+          lsp_dynamic_workspace_symbols = { disable_devicons = true },
+          lsp_references = { disable_devicons = true }
+        }
+      })
 
-            -- set up keymaps
-            local __binds = {
-                {
-                    action = "<cmd>Telescope current_buffer_fuzzy_find<cr>",
-                    key = "<leader>/",
-                    mode = "n"
-                },
-                {
-                    action = "<cmd>Telescope live_grep<cr>",
-                    key = "<leader><space>",
-                    mode = "n"
-                },
-                {
-                    action = "<cmd>Telescope find_files<cr>",
-                    key = "<leader>F",
-                    mode = "n"
-                },
-                {
-                    action = "<cmd>Telescope diagnostics<cr>",
-                    key = "<leader>d",
-                    mode = "n"
-                },
-                {
-                    action = "<cmd>Telescope git_files<cr>",
-                    key = "<leader>f",
-                    mode = "n"
-                }, {
-                    action = "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>",
-                    key = "<leader>S",
-                    mode = "n"
-                },
-                {action = "<cmd>Telescope buffers<cr>", key = "gb", mode = "n"},
-                {
-                    action = "<cmd>Telescope lsp_definitions<cr>",
-                    key = "gd",
-                    mode = "n"
-                },
-                {
-                    action = "<cmd>Telescope lsp_references<cr>",
-                    key = "gr",
-                    mode = "n"
-                },
-                {
-                    action = "<cmd>Telescope lsp_document_symbols<cr>",
-                    key = "<leader>s",
-                    mode = "n"
-                }
-            }
-            for i, map in ipairs(__binds) do
-                vim.keymap.set(map.mode, map.key, map.action, map.options)
-            end
-        end
-    }
+      -- set up keymaps
+      vim.keymap.set("n", "<leader>/", function() require("telescope.builtin").current_buffer_fuzzy_find() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader><space>", function() require("telescope.builtin").live_grep() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>F", function() require("telescope.builtin").find_files() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>d", function() require("telescope.builtin").diagnostics() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>f", function() require("telescope.builtin").git_files() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>S", function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "gb", function() require("telescope.builtin").buffers() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "gd", function() require("telescope.builtin").lsp_definitions() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "gr", function() require("telescope.builtin").lsp_references() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>s", function() require("telescope.builtin").lsp_document_symbols() end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>j", function() require("telescope.builtin").jumplist({ show_line = false, }) end,
+        { silent = true, noremap = true })
+      vim.keymap.set("n", "<leader>ht", function() require("telescope.builtin").help_tags() end,
+        { silent = true, noremap = true })
+    end
+  }
 }
