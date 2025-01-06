19367d92351bae66b956b3c735680c8d4157968a
Simon Walker <s.r.walker101@googlemail.com>
2025-01-06 16:15:32 +0000

    neovim: configure treesitter folding

--- home/neovim/lua/plugins/treesitter.lua
+++ home/neovim/lua/plugins/treesitter.lua
@@ -1,16 +1,21 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            local configs = require("nvim-treesitter.configs")
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local configs = require("nvim-treesitter.configs")
 
-            configs.setup({
-                ensure_installed = {"lua", "vim", "rust", "python", "nix", "yaml", "hcl", "terraform"},
-                sync_install = false,
-                highlight = {enable = true},
-                indent = {enable = true}
-            })
-        end
-    },
+      configs.setup({
+        ensure_installed = { "lua", "vim", "rust", "python", "nix", "yaml", "hcl", "terraform" },
+        sync_install = false,
+        highlight = { enable = true, additional_vim_regex_highlighting = false },
+        indent = { enable = false },
+      })
+
+      -- configure folding
+      vim.wo.foldmethod = "expr"
+      vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+      vim.opt.foldlevel = 99
+    end
+  },
 }
