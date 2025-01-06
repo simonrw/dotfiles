27e3cbd411e75681e44d09122c03039c7cb34c1c
Simon Walker <s.r.walker101@googlemail.com>
2025-01-06 11:29:16 +0000

    neovim: use blink instead of cmp
    Former-commit-id: 695ccad2fd0af4467455ffd475580af64cab2a72

--- home/neovim/lua/plugins/lsp.lua
+++ home/neovim/lua/plugins/lsp.lua
@@ -13,7 +13,6 @@ return {
                     }
                 }
             },
-            "hrsh7th/cmp-nvim-lsp",
         },
         config = function()
             local lspServers = {
@@ -62,15 +61,8 @@ return {
                 {name = "terraformls"},
             }
 
-            local capabilities = function()
-                local capabilities = vim.lsp.protocol.make_client_capabilities()
-                capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-                return capabilities
-            end
-
             local setup = {
                 on_attach = function(client, bufnr) end,
-                capabilities = capabilities()
             }
 
             for _, server in ipairs(lspServers) do
