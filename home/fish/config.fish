058fd4f521b2df0eae05bbb5ca21b2a1b00dafc7
Simon Walker <s.r.walker101@googlemail.com>
2025-01-07 13:24:29 +0000

    Revert "Allow tmux in vscode terminal"
    This reverts commit db4b2701779877c109dcc130da327e151a715268 [formerly c2b01e8695cbd3193e1b7b32d1b31aacc8260e6e].
    
    
    Former-commit-id: b64a30aede0a34ce3fdb00288d43bc28acd72591

--- home/fish/config.fish
+++ home/fish/config.fish
@@ -39,6 +39,10 @@ function _not_inside_emacs
     test -z "$INSIDE_EMACS" && test -z "$EMACS"
 end
 
+function _not_inside_vscode_term
+    test "$TERM_PROGRAM" != "vscode"
+end
+
 function _not_inside_zellij
     test -z "$ZELLIJ_SESSION_NAME"
 end
@@ -61,7 +65,7 @@ function _not_disabled
 end
 
 function ensure_tmux_is_running
-    if _not_disabled && _not_inside_tmux && _not_inside_neovim && _not_inside_emacs && _inside_x_session && _not_inside_zellij && _not_inside_pycharm
+    if _not_disabled && _not_inside_tmux && _not_inside_neovim && _not_inside_emacs && _inside_x_session && _not_inside_vscode_term && _not_inside_zellij && _not_inside_pycharm
         tat
     end
 end
