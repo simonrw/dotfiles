28a1807556e02e1db19567215f597d27c1b3f491
Simon Walker <s.r.walker101@googlemail.com>
2025-01-07 17:26:23 +0000

    neovim(lsp): don't add bindings that are overridden by rustacean.nvim

--- home/neovim/lua/plugins/lsp.lua
+++ home/neovim/lua/plugins/lsp.lua
@@ -121,12 +121,16 @@ return {
           vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { noremap = true, silent = true, buffer = buf, desc = "Rename symbol" })
           vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { noremap = true, silent = true, buffer = buf, desc = "Go to next error" })
           vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { noremap = true, silent = true, buffer = buf, desc = "Go to previous error" })
-          vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, { noremap = true, silent = true, buffer = buf, desc = "Get help with function signature" })
-          vim.keymap.set("n", "<leader>a", function()
-            vim.lsp.buf.code_action({
-              source = { organizeImports = true }
-            })
-          end, { noremap = true, silent = true, buffer = buf, desc = "Execute code action" })
+
+          -- only add these mappings if the LSP is not rust-analyzer, as these mappings are set in the rust filetype plugin
+          if c.name ~= "rust-analyzer" then
+            vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, { noremap = true, silent = true, buffer = buf, desc = "Get help with function signature" })
+            vim.keymap.set("n", "<leader>a", function()
+              vim.lsp.buf.code_action({
+                source = { organizeImports = true }
+              })
+            end, { noremap = true, silent = true, buffer = buf, desc = "Execute code action" })
+          end
         end,
       })
     end
