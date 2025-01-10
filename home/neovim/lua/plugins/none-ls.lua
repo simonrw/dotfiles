f97c76d9e59916397a55c6ef0a0e44434be94200
Simon Walker <s.r.walker101@googlemail.com>
2025-01-10 09:35:32 +0000

    neovim: add none-ls configuration
    Former-commit-id: e6c48f02b79d093abea663c19a4256cf61c4c8c1

---
+++ home/neovim/lua/plugins/none-ls.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.opentofu_fmt,
+        },
+      })
+    end,
+  }
+}
