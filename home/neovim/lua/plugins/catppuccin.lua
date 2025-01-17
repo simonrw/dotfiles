cf6350ae4590e9327345c6d6b273911bd7f6cdb8
Simon Walker <s.r.walker101@googlemail.com>
2025-01-17 22:28:31 +0000

    neovim: configure catppuccin

--- home/neovim/lua/plugins/catppuccin.lua
+++ home/neovim/lua/plugins/catppuccin.lua
@@ -1,15 +1,17 @@
 return {
 
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd([[
-                set background=dark
-                colorscheme catppuccin-macchiato
-            ]])
-            vim.cmd.highlight({"TreesitterContextBottom", "gui=none"})
-        end
-    },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+      vim.cmd.background = "dark"
+      vim.cmd.colorscheme "catppuccin-macchiato"
+      vim.cmd.highlight({ "TreesitterContextBottom", "gui=none" })
+    end
+  },
 }
