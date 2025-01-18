0a272dea144b7f50085a704d691a6684cb1872df
Simon Walker <s.r.walker101@googlemail.com>
2025-01-18 20:19:11 +0000

    neovim: include fidget.nvim
    Former-commit-id: a238ebb4fe5e07b5c65aefa6324ebea260bd4e32

---
+++ home/neovim/lua/plugins/fidget.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "j-hui/fidget.nvim",
+    tag = "v1.5.0",
+    opts = {
+      notification = {
+        window = {
+          winblend = 0,
+        },
+      },
+    },
+  }
+}
