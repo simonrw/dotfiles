8f3646ca1a55259e8662b7fad22d456b6c3a1e46
Simon Walker <s.r.walker101@googlemail.com>
2025-01-19 20:50:02 +0000

    neovim: swap snacks for telescope and oil
    Former-commit-id: 701aabc83230ecb1651b0b821e11acb42d5722fe

--- home/neovim/lua/plugins/snacks.lua
+++ home/neovim/lua/plugins/snacks.lua
@@ -14,27 +14,5 @@ return {
       quickfile = { enabled = true },
       words = { enabled = true },
     },
-    keys = {
-      { "<leader>f", function() require("snacks.picker").git_files() end, desc = "Find git files" },
-      { "<leader>F", function() require("snacks.picker").files() end, desc = "Find files" },
-      { "<leader>d", function() require("snacks.picker").diagnostics() end, desc = "LSP diagnostics" },
-      { "<leader>S", function() require("snacks.picker").lsp_symbols() end, desc = "LSP symbols" },
-      { "gb", function() require("snacks.picker").buffers() end, desc = "Open buffers" },
-      { "gd", function() require("snacks.picker").lsp_definitions() end, desc = "LSP definitions" },
-      { "gr", function() require("snacks.picker").lsp_references() end, desc = "LSP references" },
-      { "gi", function() require("snacks.picker").lsp_implementations() end, desc = "LSP implementations" },
-      { "gy", function() require("snacks.picker").lsp_type_definitions() end, desc = "LSP type definition" },
-      { "<leader>j", function() require("snacks.picker").jumps() end, desc = "Jump list" },
-      { "<leader>ht", function() require("snacks.picker").help() end, desc = "Help tags" },
-      { "<leader><space>", function() require("snacks.picker").grep() end, desc = "Perform a live grep over the project" },
-    },
-    config = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "MiniFilesActionRename",
-        callback = function(event)
-          Snacks.rename.on_rename_file(event.data.from, event.data.to)
-        end,
-      })
-    end,
   }
 }
