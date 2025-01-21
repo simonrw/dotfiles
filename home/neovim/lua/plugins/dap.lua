fd530d8a97db384099a1405f1ec10e9f284befb5
Simon Walker <s.r.walker101@googlemail.com>
2025-01-21 23:05:10 +0000

    neovim: set up dap
    Former-commit-id: cf3a4f4dbe28252b385de276627ca8ccee3b975c

---
+++ home/neovim/lua/plugins/dap.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    'mfussenegger/nvim-dap-python',
+    dependencies = {
+      {
+        'mfussenegger/nvim-dap',
+        config = function()
+          local opts = { noremap = true, silent = true }
+
+          vim.keymap.set("n", "<leader>b", function() require("dap").toggle_breakpoint() end, opts)
+
+          -- map pycharm bindings
+          vim.keymap.set("n", "<F7>", function() require("dap").step_into() end, opts)
+          vim.keymap.set("n", "<F8>", function() require("dap").step_over() end, opts)
+          vim.keymap.set("n", "<S-F8>", function() require("dap").step_out() end, opts)
+          vim.keymap.set("n", "<F9>", function() require("dap").continue() end, opts)
+        end,
+      },
+    },
+    config = function()
+      require("dap-python").setup("python")
+    end,
+  },
+  {
+    'rcarriga/nvim-dap-ui',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      'nvim-neotest/nvim-nio',
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+}
