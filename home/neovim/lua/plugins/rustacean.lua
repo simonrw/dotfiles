595fe93102436ab622cd24fea1d5bc0487cfc690
Simon Walker <s.r.walker101@googlemail.com>
2025-01-22 14:06:19 +0000

    neovim: ignore common big dirs

--- home/neovim/lua/plugins/rustacean.lua
+++ home/neovim/lua/plugins/rustacean.lua
@@ -3,5 +3,18 @@ return {
     'mrcjkb/rustaceanvim',
     version = '^5',
     lazy = false,
+    config = function()
+      vim.g.rustaceanvim = {
+        server = {
+          default_settings = {
+            ['rust-analyzer'] = {
+              files = {
+                excludeDirs = { ".venv", "venv", ".direnv", "node_modules" },
+              },
+            },
+          },
+        },
+      }
+    end,
   }
 }
