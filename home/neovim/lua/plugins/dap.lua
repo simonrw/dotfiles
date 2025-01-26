95977b9451417981e18efe526cb010e7b46a1268
Simon Walker <s.r.walker101@googlemail.com>
2025-01-26 23:26:57 +0000

    neovim(dap): add localstack code workspace configurations
    Former-commit-id: dfb66a4a95032a6566d5fd1535d988430664c2a4

--- home/neovim/lua/plugins/dap.lua
+++ home/neovim/lua/plugins/dap.lua
@@ -1,3 +1,109 @@
+--- @param m table
+--- @param workspace_path string
+--- @return table
+local function expand_path_mapping(m, workspace_path)
+  local out = {}
+  for _, mapping in ipairs(m) do
+    local transformed = {}
+    if mapping.localRoot then
+      transformed.localRoot = expand_path(mapping.localRoot, workspace_path)
+    end
+    if mapping.remoteRoot then
+      transformed.remoteRoot = expand_path(mapping.remoteRoot, workspace_path)
+    end
+    table.insert(out, transformed)
+  end
+  return out
+end
+
+--- @param s string
+--- @param workspace_path string
+--- @return string
+local function expand_path(s, workspace_path)
+  if string.find(s, "${workspaceFolder:") then
+    local startIndex, endIndex = string.find(s, "${workspaceFolder:")
+    local endReplaceIndex = string.find(s, '}', endIndex)
+    local subPath = string.sub(s, endIndex + 1, endReplaceIndex - 1)
+    local replacee = workspace_path .. "/" .. subPath
+    local pattern = string.sub(s, startIndex, endReplaceIndex)
+    return string.sub(s, 0, startIndex - 1) .. replacee .. string.sub(s, endReplaceIndex + 1)
+  elseif string.find(s, "${workspaceFolder}") then
+    -- should not really happen but straightforward
+    local replaced = string.gsub(s, "${workspaceFolder}", workspace_path)
+    return replaced
+  else
+    -- no transform needed
+    return s
+  end
+end
+
+--- @param mappings table
+--- @param workspace_path string
+--- @return table
+local function expand_path_mappings(mappings, workspace_path)
+  local expanded_mappings = {}
+  for _, mapping in ipairs(mappings) do
+    table.insert(expanded_mappings, expand_path_mapping(mapping, workspace_path))
+  end
+  return expanded_mappings
+end
+
+--- @param env table
+--- @param workspace_path string
+--- @return table
+local function expand_env(env, workspace_path)
+  local out = {}
+  for name, value in pairs(env) do
+    local res = expand_path(value, workspace_path)
+    out[name] = res
+  end
+  return out
+end
+
+--- @param config table
+--- @param workspace_path string
+--- @return table
+local function expand_paths(config, workspace_path)
+  local cwd = config.cwd
+  if cwd then
+    config.cwd = expand_path(cwd, workspace_path)
+  end
+
+  local pathMappings = config.pathMappings
+  if pathMappings then
+    config.pathMappings = expand_path_mappings(pathMappings, workspace_path)
+  end
+
+  local env = config.env
+  if env then
+    local res = expand_env(env, workspace_path)
+    config.env = res
+  end
+
+  return config
+end
+
+--- @param jsonstr string
+--- @param workspace_path string
+--- @return table
+local function load_workspace_json(jsonstr, workspace_path)
+  local ok, obj = pcall(vim.json.decode, jsonstr, { luanil = { object = true } })
+  if not ok then
+    error("error decoding launch json")
+  end
+
+  assert(type(obj) == "table", "workspace must contain an object")
+
+  local workspace_dir = vim.fs.normalize(vim.fs.dirname(workspace_path))
+
+  local configs = {}
+  for _, config in ipairs(obj.launch.configurations or {}) do
+    local transformed = expand_paths(config, workspace_dir)
+    table.insert(configs, transformed)
+  end
+  return configs
+end
+
 return {
   {
     'mfussenegger/nvim-dap-python',
@@ -5,13 +111,14 @@ return {
       {
         'mfussenegger/nvim-dap',
         config = function()
+          local dap = require("dap")
           local opts = function(desc)
             return { noremap = true, silent = true, desc = desc }
           end
 
-          vim.keymap.set("n", "<leader>bb", function() require("dap").toggle_breakpoint() end, opts("Toggle breakpoint"))
-          vim.keymap.set("n", "<leader>br", function() require("dap").repl.toggle() end, opts("Toggle repl"))
-          vim.keymap.set("n", "<leader>bl", function() require("dap").run_last() end, opts("Run last debugging session"))
+          vim.keymap.set("n", "<leader>bb", function() dap.toggle_breakpoint() end, opts("Toggle breakpoint"))
+          vim.keymap.set("n", "<leader>br", function() dap.repl.toggle() end, opts("Toggle repl"))
+          vim.keymap.set("n", "<leader>bl", function() dap.run_last() end, opts("Run last debugging session"))
           vim.keymap.set("n", "<leader>bf", function()
             local widgets = require('dap.ui.widgets')
             widgets.centered_float(widgets.frames)
@@ -22,12 +129,12 @@ return {
           end, opts("Show scopes"))
 
           -- map pycharm bindings
-          vim.keymap.set({ "i", "n" }, "<F7>", function() require("dap").step_into() end, opts("Step into"))
-          vim.keymap.set({ "i", "n" }, "<F8>", function() require("dap").step_over() end, opts("Step over"))
-          vim.keymap.set({ "i", "n" }, "<S-F8>", function() require("dap").step_out() end, opts("Step out"))
-          vim.keymap.set({ "i", "n" }, "<F9>", function() require("dap").continue() end, opts("Continue"))
+          vim.keymap.set({ "i", "n" }, "<F7>", function() dap.step_into() end, opts("Step into"))
+          vim.keymap.set({ "i", "n" }, "<F8>", function() dap.step_over() end, opts("Step over"))
+          vim.keymap.set({ "i", "n" }, "<S-F8>", function() dap.step_out() end, opts("Step out"))
+          vim.keymap.set({ "i", "n" }, "<F9>", function() dap.continue() end, opts("Continue"))
 
-          require("dap").adapters.codelldb = {
+          dap.adapters.codelldb = {
             type = "server",
             port = "${port}",
             executable = {
@@ -35,6 +142,23 @@ return {
               args = { "--port", "${port}" },
             },
           }
+
+          -- support parsing LocalStack code workspace file
+          dap.providers.configs["localstack-workspace"] = function(bufnr)
+            local resolved_path = vim.fn.getcwd() .. '/../localstack.code-workspace'
+            if not vim.loop.fs_stat(resolved_path) then
+              return {}
+            end
+
+            local lines = {}
+            for line in io.lines(resolved_path) do
+              if not vim.startswith(vim.trim(line), '//') then
+                table.insert(lines, line)
+              end
+            end
+            local contents = table.concat(lines, '\n')
+            return load_workspace_json(contents, resolved_path)
+          end
         end,
       },
     },
