0dbf3a3652c92ed4f64e917dd45ec9de1116e8a6
Simon Walker <s.r.walker101@googlemail.com>
2025-01-26 22:12:31 +0000

    neovim: update dap mappings
    Former-commit-id: 77b3480354706e5c6062dda2e1670ae587d05004

--- home/neovim/lua/plugins/dap.lua
+++ home/neovim/lua/plugins/dap.lua
@@ -5,22 +5,34 @@ return {
       {
         'mfussenegger/nvim-dap',
         config = function()
-          local opts = { noremap = true, silent = true }
+          local opts = function(desc)
+            return { noremap = true, silent = true, desc = desc }
+          end
 
-          vim.keymap.set("n", "<leader>b", function() require("dap").toggle_breakpoint() end, opts)
+          vim.keymap.set("n", "<leader>bb", function() require("dap").toggle_breakpoint() end, opts("Toggle breakpoint"))
+          vim.keymap.set("n", "<leader>br", function() require("dap").repl.toggle() end, opts("Toggle repl"))
+          vim.keymap.set("n", "<leader>bl", function() require("dap").run_last() end, opts("Run last debugging session"))
+          vim.keymap.set("n", "<leader>bf", function()
+            local widgets = require('dap.ui.widgets')
+            widgets.centered_float(widgets.frames)
+          end, opts("Show frames"))
+          vim.keymap.set("n", "<leader>bs", function()
+            local widgets = require('dap.ui.widgets')
+            widgets.centered_float(widgets.scopes)
+          end, opts("Show scopes"))
 
           -- map pycharm bindings
-          vim.keymap.set({ "i", "n" }, "<F7>", function() require("dap").step_into() end, opts)
-          vim.keymap.set({ "i", "n" }, "<F8>", function() require("dap").step_over() end, opts)
-          vim.keymap.set({ "i", "n" }, "<S-F8>", function() require("dap").step_out() end, opts)
-          vim.keymap.set({ "i", "n" }, "<F9>", function() require("dap").continue() end, opts)
+          vim.keymap.set({ "i", "n" }, "<F7>", function() require("dap").step_into() end, opts("Step into"))
+          vim.keymap.set({ "i", "n" }, "<F8>", function() require("dap").step_over() end, opts("Step over"))
+          vim.keymap.set({ "i", "n" }, "<S-F8>", function() require("dap").step_out() end, opts("Step out"))
+          vim.keymap.set({ "i", "n" }, "<F9>", function() require("dap").continue() end, opts("Continue"))
 
           require("dap").adapters.codelldb = {
             type = "server",
             port = "${port}",
             executable = {
               command = "codelldb",
-              args = {"--port", "${port}"},
+              args = { "--port", "${port}" },
             },
           }
         end,
