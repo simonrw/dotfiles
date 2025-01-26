8b342bc3290c120554648ca4b58de5c7f3da8600
Simon Walker <s.r.walker101@googlemail.com>
2025-01-26 21:12:19 +0000

    neovim: bind debug keys in insert mode as well
    Former-commit-id: 607dd10cd2eece3dfd7dbab0e844d108c83e89a5

--- home/neovim/lua/plugins/dap.lua
+++ home/neovim/lua/plugins/dap.lua
@@ -10,10 +10,10 @@ return {
           vim.keymap.set("n", "<leader>b", function() require("dap").toggle_breakpoint() end, opts)
 
           -- map pycharm bindings
-          vim.keymap.set("n", "<F7>", function() require("dap").step_into() end, opts)
-          vim.keymap.set("n", "<F8>", function() require("dap").step_over() end, opts)
-          vim.keymap.set("n", "<S-F8>", function() require("dap").step_out() end, opts)
-          vim.keymap.set("n", "<F9>", function() require("dap").continue() end, opts)
+          vim.keymap.set({ "i", "n" }, "<F7>", function() require("dap").step_into() end, opts)
+          vim.keymap.set({ "i", "n" }, "<F8>", function() require("dap").step_over() end, opts)
+          vim.keymap.set({ "i", "n" }, "<S-F8>", function() require("dap").step_out() end, opts)
+          vim.keymap.set({ "i", "n" }, "<F9>", function() require("dap").continue() end, opts)
 
           require("dap").adapters.codelldb = {
             type = "server",
@@ -28,30 +28,6 @@ return {
     },
     config = function()
       require("dap-python").setup("python")
-    end,
-  },
-  {
-    'rcarriga/nvim-dap-ui',
-    dependencies = {
-      'mfussenegger/nvim-dap',
-      'nvim-neotest/nvim-nio',
-    },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-      dapui.setup()
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
     end,
   },
 }
