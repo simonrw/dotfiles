19c6367ccdc47409c24c4a94e66b3c002f4e0416
Simon Walker <s.r.walker101@googlemail.com>
2025-01-28 23:00:25 +0000

    neovim(treesitter): install typescript and javascript
    Former-commit-id: 75ece090509b5921feb5f4516aa79a888bbfbb03

--- home/neovim/lua/plugins/treesitter.lua
+++ home/neovim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = { "lua", "vim", "rust", "python", "nix", "yaml", "hcl", "terraform" },
+        ensure_installed = { "lua", "vim", "rust", "python", "nix", "yaml", "hcl", "terraform", "typescript", "javascript" },
         sync_install = false,
         highlight = { enable = true, additional_vim_regex_highlighting = false },
         indent = { enable = false },
