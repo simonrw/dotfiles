d660e5d4ea8695a11a6db4faae4fcddc49ae8c7c
Simon Walker <s.r.walker101@googlemail.com>
2025-02-03 21:23:10 +0000

    neovim(telescope): use 0.1.x branch
    Former-commit-id: 4f6d1ff9a52b388302980a893db076cde72fa002

--- home/neovim/lua/plugins/telescope.lua
+++ home/neovim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
