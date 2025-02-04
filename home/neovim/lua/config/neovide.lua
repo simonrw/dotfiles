f68abd806d0384d771c5672cf31b386700cd727b
Simon Walker <s.r.walker101@googlemail.com>
2025-02-04 09:51:06 +0000

    neovim: add neovide configuration
    Former-commit-id: 4c108d4ee2e1009f145341966d297ed86c9300d9

---
+++ home/neovim/lua/config/neovide.lua
@@ -0,0 +1,6 @@
+-- neovide specific settings
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_vfx_mode = ""
+  vim.g.neovide_scroll_animation_length = 0
+end
