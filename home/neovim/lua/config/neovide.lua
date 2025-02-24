1f188e6a13b79632a49036ecd70d198d99fdd30f
Simon Walker <s.r.walker101@googlemail.com>
2025-02-24 20:50:04 +0000

    neovide:add font size mappings

--- home/neovim/lua/config/neovide.lua
+++ home/neovim/lua/config/neovide.lua
@@ -1,6 +1,13 @@
+local map = vim.keymap
+
 -- neovide specific settings
 if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_vfx_mode = ""
   vim.g.neovide_scroll_animation_length = 0
+
+  -- bindings to change font size
+  map.set({ "n", "v" }, "<C-+>", function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1 end)
+  map.set({ "n", "v" }, "<C-->", function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1 end)
+  map.set({ "n", "v" }, "<C-0>", function() vim.g.neovide_scale_factor = 1 end)
 end
