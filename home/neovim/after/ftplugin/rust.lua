1bc93537cbb7c375531421c40ec11eeb0d4e771e
Simon Walker <s.r.walker101@googlemail.com>
2025-02-27 09:40:35 +0000

    neovim: use K binding for hover
    Former-commit-id: 7b087cf2b5a81bce7e50691058aea91ee2afac45

--- home/neovim/after/ftplugin/rust.lua
+++ home/neovim/after/ftplugin/rust.lua
@@ -12,7 +12,7 @@ vim.keymap.set(
 
 vim.keymap.set(
   "n",
-  "<leader>k", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+  "K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
   function()
     vim.cmd.RustLsp({ 'hover', 'actions' })
   end,
