548f3cb003390e39c14e15eb89ef33c11d2bed7a
Simon Walker <s.r.walker101@googlemail.com>
2025-03-16 17:21:22 +0000

    neovim: configure diffview

--- home/neovim/lua/plugins/diffview.lua
+++ home/neovim/lua/plugins/diffview.lua
@@ -1,5 +1,14 @@
 return {
   {
-    "sindrets/diffview.nvim"
+    "sindrets/diffview.nvim",
+    opts = {
+      use_icons = false,
+      hooks = {
+        diff_buf_read = function(bufnr)
+          vim.opt_local.number = false
+          vim.opt_local.relativenumber = false
+        end,
+      },
+    },
   }
 }
