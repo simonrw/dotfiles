142401a4dd13b5f93d8b2ee9da3ecc63a37a5c86
Simon Walker <s.r.walker101@googlemail.com>
2025-03-18 09:17:19 +0000

    neovim: update error indicators
    * Remove warn/info/hint indicators
    * Make the styling nicer
    
    neovim: remove warn/hint/info indicators
    
    I don't care about these

--- home/neovim/lua/plugins/lualine.lua
+++ home/neovim/lua/plugins/lualine.lua
@@ -1,10 +1,8 @@
-local indicator_symbol = "●"
-
 local function diagnostic(level)
   if (vim.diagnostic.count(0)[level] or 0) > 0 then
-    return indicator_symbol
+    return "x"
   else
-    return "○"
+    return ""
   end
 end
 
@@ -24,22 +22,22 @@ return {
         section_separators = "",
       },
       sections = {
-        lualine_a = {"mode"},
-        lualine_b = {"filename"},
+        lualine_a = { "mode" },
+        lualine_b = { "filename" },
         lualine_c = {
-          { error_ind, color = { fg = "#FF0000" } },
-          { warn_ind, color = { fg = "#FFAA00" } },
-          { info_ind, color = { fg = "#229922" } },
-          { note_ind, color = { fg = "#005599" } },
+          {
+            error_ind,
+            color = { fg = "#8aadf4" },
+          },
         },
         lualine_x = {},
-        lualine_y = {"progress"},
-        lualine_z = {"location"},
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = { },
+        lualine_c = {},
         lualine_x = {},
         lualine_y = {},
         lualine_z = {},
