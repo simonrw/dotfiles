c026bbb49be9e5711efc548229942436fd1ddd65
Simon Walker <s.r.walker101@googlemail.com>
2025-05-05 20:19:55 +0100

    fish: use neovim as my man page viewer
    Former-commit-id: 248cb171d0c8ea82942f55673dff29dc0b5ebfdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -82,6 +82,7 @@ status is-interactive; and begin
     set -x PAGER bat
     set -x PYTHONUNBUFFERED 1
     set -x SHELL (command -v fish)
+    set -x MANPAGER "nvim +Man!"
 
     set -x BUILD_PREFIX {$HOME}/.local
     set -x PATH {$BUILD_PREFIX}/bin {$HOME}/.bin  /usr/local/bin {$HOME}/.cargo/bin {$HOME}/bin {$GOPATH}/bin {$HOME}/.npm-packages/bin {$PATH}
