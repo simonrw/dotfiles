72dafeec621845d70ac8fdae6bddd98107956725
Simon Walker <s.r.walker101@googlemail.com>
2025-05-26 21:49:53 +0100

    neovim: add tsx to default tree-sitter installations

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -9,7 +9,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = { "lua", "vim", "rust", "python", "nix", "yaml", "hcl", "terraform", "typescript", "javascript" },
+        ensure_installed = { "lua", "vim", "rust", "python", "nix", "yaml", "hcl", "terraform", "typescript", "javascript", "tsx" },
         sync_install = false,
         highlight = { enable = true, additional_vim_regex_highlighting = false },
         indent = { enable = false },
