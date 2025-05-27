25ba72ec52790a143a01f854e0bb69be5795f620
Simon Walker <s.r.walker101@googlemail.com>
2025-05-27 13:00:19 +0100

    neovim: add mapping to complete word with copilot
    Former-commit-id: 8652a5d82550fa588c4719cf9bfe5530227e6d35

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -8,7 +8,7 @@ return {
                     -- Remap completion keys
                     vim.g.copilot_no_tab_map = true
                     -- control + w (accept word)
-                    -- vim.keymap.set({ "i" }, "<C-w>", "<Plug>(copilot-accept-word)", { silent = true, desc = "Toggle CodeCompanion Chat" })
+                    vim.keymap.set({ "i" }, "<C-l>", "<Plug>(copilot-accept-word)", { silent = true, desc = "Toggle CodeCompanion Chat" })
                     -- control + e (accept line)
                     vim.keymap.set({ "i" }, "<C-e>", "<Plug>(copilot-accept-line)",
                         { silent = true, desc = "Toggle CodeCompanion Chat" })
