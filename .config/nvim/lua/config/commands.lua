c6b80f96d6e7ce7b088d8a651fa3af05fb227aa0
Simon Walker <s.r.walker101@googlemail.com>
2025-06-13 10:18:43 +0100

    nvim: add Mkdir function
    Former-commit-id: 88b6a149bad66fbca3fd0d0c945453969ec630da

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -5,18 +5,34 @@ vim.api.nvim_create_user_command("T", "split | resize 30 | term <args>", {
 })
 
 vim.api.nvim_create_user_command("ToggleList", function()
-  local qf_exists = false
-  for _, win in pairs(vim.fn.getwininfo()) do
-    if win["quickfix"] == 1 then
-      qf_exists = true
+    local qf_exists = false
+    for _, win in pairs(vim.fn.getwininfo()) do
+        if win["quickfix"] == 1 then
+            qf_exists = true
+        end
     end
-  end
 
-  if qf_exists then
-    vim.cmd "cclose"
-  else
-    vim.cmd "copen"
-  end
+    if qf_exists then
+        vim.cmd "cclose"
+    else
+        vim.cmd "copen"
+    end
 end, {})
 
 vim.api.nvim_create_user_command("W", "write", {})
+
+vim.api.nvim_create_user_command("Mkdir", function()
+    -- full path of current file
+    local filepath = vim.fn.expand('%:p')
+    if filepath == '' then
+        return
+    end
+
+    -- parent directory
+    local dir = vim.fn.fnamemodify(filepath, ':h')
+
+    -- mkdir -p behavior: only create if not exists
+    if vim.fn.isdirectory(dir) == 0 then
+        vim.fn.mkdir(dir, 'p')
+    end
+end, {})
