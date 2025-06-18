6bb22e40c90ca3f79a29edcb9f3ea9177f8f58a8
Simon Walker <s.r.walker101@googlemail.com>
2025-06-18 09:24:33 +0100

    nvim: add octo plugin
    Former-commit-id: 09fcd09328ecd56118b372804afa6ceebc35345b

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        'pwntester/octo.nvim',
+        requires = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'nvim-tree/nvim-web-devicons',
+        },
+        config = function()
+            require "octo".setup({
+                use_local_fs = false,
+            })
+        end
+    },
+}
