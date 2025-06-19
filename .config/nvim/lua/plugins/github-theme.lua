05ae0e177eede8a4e567e8ab3469faa751753928
Simon Walker <s.r.walker101@googlemail.com>
2025-06-19 08:05:25 +0100

    nvim: correct dark mode detection for github theme

--- .config/nvim/lua/plugins/github-theme.lua
+++ .config/nvim/lua/plugins/github-theme.lua
@@ -3,7 +3,7 @@ return {
         "projekt0n/github-nvim-theme",
         priority = 1000,
         lazy = false,
-        enabled = not vim.g.is_dark_theme,
+        enabled = not vim.g.is_dark_mode,
         config = function()
             require("github-theme").setup({})
             vim.cmd.colorscheme "github_light"
