817fba02a57d288aa4cc182bff2f9c824b3ab727
Simon Walker <s.r.walker101@googlemail.com>
2025-06-23 15:25:52 +0100

    brew: swap docker for orbstack
    Former-commit-id: f5af62a885202f103351c9a7be7e92dd0b919499

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -169,3 +169,7 @@ status is-interactive; and begin
         fish_config theme choose Tomorrow
     end
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
