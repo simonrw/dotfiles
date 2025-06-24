268821e58eef80f9ec2086aa64a724f56302c0f9
Simon Walker <s.r.walker101@googlemail.com>
2025-06-24 08:15:14 +0100

    Revert "brew: swap docker for orbstack"
    This reverts commit f5af62a885202f103351c9a7be7e92dd0b919499.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -169,7 +169,3 @@ status is-interactive; and begin
         fish_config theme choose Tomorrow
     end
 end
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
