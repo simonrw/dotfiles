2fb20ba0c8a9f8aa3568de31dc59e590c3478f66
Simon Walker <s.r.walker101@googlemail.com>
2025-07-10 21:06:53 +0100

    nvim: auto install packages
    Former-commit-id: 363a99f958613b24bb1560bc28ed562452989524

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -5,12 +5,34 @@ return {
             { "mason-org/mason.nvim", opts = {} },
             "neovim/nvim-lspconfig",
         },
-        opts = {
-            ensure_installed = {
-                -- Don't include rust_analyzer as it's usually installed by rustup
-                "basedpyright",
-                "lua_ls",
-            },
-        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    -- Don't include rust_analyzer as it's usually installed by rustup
+                    "basedpyright",
+                    "lua_ls",
+                },
+            })
+
+            -- install additional requirements that are not allowed with the ensure_installed key
+            local a = require("plenary.async")
+            local reg = require("mason-registry")
+
+            local packages = { "debugpy", "stylua", "shellcheck", "ruff" }
+
+            for _, package_name in ipairs(packages) do
+                a.run(function()
+                    if not reg.is_installed(package_name) then
+                        Snacks.notify('Installing ' .. package_name)
+                        local package = reg.get_package(package_name)
+                        package:install({}, function(success, error)
+                            if not success then
+                                Snacks.notify.error('Error installing ' .. package_name .. ': ' .. error)
+                            end
+                        end)
+                    end
+                end)
+            end
+        end,
     }
 }
