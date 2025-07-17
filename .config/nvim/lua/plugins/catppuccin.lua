6aa0aa643fdc17154bb232200d878a33d9af26a0
Simon Walker <s.r.walker101@googlemail.com>
2025-07-17 15:24:50 +0100

    nvim: support reloading themes
    Former-commit-id: 04bd411c21da68d3ad0d69474589ea2ea975df1f

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -5,14 +5,8 @@ return {
     name = "catppuccin",
     priority = 1000,
     lazy = false,
-    enabled = vim.g.is_dark_mode,
     config = function()
       require("catppuccin").setup()
-
-      vim.cmd.colorscheme "catppuccin-macchiato"
-      vim.cmd.highlight({ "TreesitterContextBottom", "gui=none" })
-      vim.cmd.highlight({ "CursorLine", "guibg=#303347" })
-      vim.cmd.highlight({ "CursorColumn", "guibg=#303347" })
     end
   },
 }
--- .config/nvim/lua/plugins/github-theme.lua
+++ .config/nvim/lua/plugins/github-theme.lua
@@ -3,25 +3,8 @@ return {
         "projekt0n/github-nvim-theme",
         priority = 1000,
         lazy = false,
-        enabled = not vim.g.is_dark_mode,
         config = function()
             require("github-theme").setup({})
-            vim.cmd.colorscheme "github_light"
-
-            vim.cmd.highlight({ "DiagnosticError", "guifg=Red" })
-            vim.cmd.highlight({ "DiagnosticHint", "guifg=Orange" })
-            vim.cmd.highlight({ "DiagnosticWarn", "guifg=Orange" })
-            vim.cmd.highlight({ "DiagnosticInfo", "guifg=LightBlue" })
-            vim.cmd.highlight({ "DiagnosticFloatingError", "guifg=Red" })
-            vim.cmd.highlight({ "DiagnosticFloatingHint", "guifg=Orange" })
-            vim.cmd.highlight({ "DiagnosticFloatingInfo", "guifg=LightBlue" })
-            vim.cmd.highlight({ "DiagnosticFloatingWarn", "guifg=Orange" })
-            vim.cmd.highlight({ "DiagnosticVirtualTextError", "guifg=Red" })
-            vim.cmd.highlight({ "DiagnosticVirtualTextHint", "guifg=Orange" })
-            vim.cmd.highlight({ "DiagnosticVirtualTextInfo", "guifg=LightBlue" })
-            vim.cmd.highlight({ "DiagnosticVirtualTextWarn", "guifg=Orange" })
-            vim.cmd.highlight({ "Comment", "guifg=#e69340" })
-            vim.cmd.highlight({ "TreesitterContext", "guibg=#f0f0f0" })
         end,
     }
 }
