b6961ea2e43d3789e544047bbdd6aba7558a1d82
Simon Walker <s.r.walker101@googlemail.com>
2025-08-06 00:00:39 +0100

    font: use monaspace for zed, emacs and ghostty
    Former-commit-id: 513f9525eae9094b5d38ebae5188926892c5acd7

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,7 +21,7 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "JetBrainsMonoNL NF" :size 16 :weight 'semi-bold)
+(setq doom-font (font-spec :family "Monaspace Neon" :size 16 :weight 'semi-bold)
       doom-variable-pitch-font (font-spec :family "Helvetica Neue" :size 13))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
