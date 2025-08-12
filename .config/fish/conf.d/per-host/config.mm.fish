71028680d3b1eb24e7fe099d5c75c629f29380b8
Simon Walker <s.r.walker101@googlemail.com>
2025-08-12 14:17:26 +0100

    fish: move rustup_home and cargo_home to external hdd

--- .config/fish/conf.d/per-host/config.mm.fish
+++ .config/fish/conf.d/per-host/config.mm.fish
@@ -1,7 +1,9 @@
 # use my external HDD for cargo builds and go package downloads
 if test -d /Volumes/External
     set -x CARGO_TARGET_DIR /Volumes/External/cargo-target
+    set -x CARGO_HOME /Volumes/External/cargo-home
+    set -x RUSTUP_HOME /Volumes/External/rustup-home
 
     set -x GOPATH /Volumes/External/gocode
-    set -x PATH {$PATH} {$GOPATH}/bin
+    set -x PATH {$CARGO_HOME}/bin {$GOPATH}/bin {$PATH}
 end
