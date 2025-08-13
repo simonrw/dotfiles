2751dcc7cabaa0f8e624eb349dc1ee453f387f07
Simon Walker <s.r.walker101@googlemail.com>
2025-08-13 23:02:32 +0100

    emacs: fix exec-path-from-shell ordering

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -88,11 +88,12 @@
   (setq! auto-dark-themes '((catppuccin) (doom-one-light)))
   (auto-dark-mode))
 
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
-
+(require 'exec-path-from-shell)
 (dolist (var '("SSH_AUTH_SOCK" "SSH_AGENT_PID" "GPG_AGENT_INFO" "LANG" "LC_CTYPE"))
   (add-to-list 'exec-path-from-shell-variables var))
+
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
 
 ;; start the server by default
 (server-start)
