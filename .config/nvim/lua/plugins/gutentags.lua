839fd478266e0952171ae5c78cfec1ca2a778adb
Simon Walker <s.r.walker101@googlemail.com>
2025-08-19 09:59:07 +0100

    nvim: more gutentags ignoring

--- .config/nvim/lua/plugins/gutentags.lua
+++ .config/nvim/lua/plugins/gutentags.lua
@@ -3,7 +3,7 @@ return {
         "ludovicchabant/vim-gutentags",
         config = function()
             vim.g.gutentags_ctags_tagfile = ".tags"
-            vim.g.gutentags_ctags_exclude = {".venv", "node_modules", "target"}
+            vim.g.gutentags_ctags_exclude = {".venv", "node_modules", "target", ".filesystem", ".mypy_cache"}
             vim.keymap.set("n", "gd", "<c-]>", { silent = true, noremap = true, desc = "Go to tag definitioc" })
         end,
     },
