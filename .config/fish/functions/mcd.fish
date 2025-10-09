286260d264d71117c70a856a5ad12fe43ad77289
Simon Walker <s.r.walker101@googlemail.com>
2025-10-09 10:12:06 +0100

    chore: cleanup old unused config
    Former-commit-id: 5b23c07edf9c9419973fd08fb7c964082cc959e1

--- .config/fish/functions/mcd.fish
+++
@@ -1,3 +0,0 @@
-function mcd --description="Create and move into new directory"
-    mkdir -p $argv[1]; and cd $argv[1]
-end
--- .config/fish/functions/ssh-notmux.fish
+++
@@ -1,7 +0,0 @@
-function ssh-notmux --description="SSH into a host disabling automatic tmux on the destination host" --wraps="ssh"
-    if test (count $argv) -gt 1
-        ssh -t $argv[1] env TMUX_DISABLED=1 $argv[2..-1]
-    else
-        ssh -t $argv[1] env TMUX_DISABLED=1 fish
-    end
-end
