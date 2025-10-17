fcef15fe74e17a5e031996495c5223180a35bce2
Simon Walker <s.r.walker101@googlemail.com>
2025-10-17 14:11:35 +0100

    nvim: configure theming
    Former-commit-id: e9250a90a42fc2486a24ef60e6bd92b4c8bc9a2e

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -360,8 +360,29 @@ vim.lsp.enable({
 
 local function load_theme()
     if vim.g.is_dark_mode then
+        vim.cmd([[set background=dark ]])
+
+        vim.cmd.highlight({ "TreesitterContextBottom", "gui=none" })
+        vim.cmd.highlight({ "CursorLine", "guibg=#303347" })
+        vim.cmd.highlight({ "CursorColumn", "guibg=#303347" })
+        vim.cmd.highlight({ "LineNr", "guifg=#6c7086" })
     else
         vim.cmd([[set background=light ]])
+
+        vim.cmd.highlight({ "DiagnosticError", "guifg=Red" })
+        vim.cmd.highlight({ "DiagnosticHint", "guifg=Orange" })
+        vim.cmd.highlight({ "DiagnosticWarn", "guifg=Orange" })
+        vim.cmd.highlight({ "DiagnosticInfo", "guifg=LightBlue" })
+        vim.cmd.highlight({ "DiagnosticFloatingError", "guifg=Red" })
+        vim.cmd.highlight({ "DiagnosticFloatingHint", "guifg=Orange" })
+        vim.cmd.highlight({ "DiagnosticFloatingInfo", "guifg=LightBlue" })
+        vim.cmd.highlight({ "DiagnosticFloatingWarn", "guifg=Orange" })
+        vim.cmd.highlight({ "DiagnosticVirtualTextError", "guifg=Red" })
+        vim.cmd.highlight({ "DiagnosticVirtualTextHint", "guifg=Orange" })
+        vim.cmd.highlight({ "DiagnosticVirtualTextInfo", "guifg=LightBlue" })
+        vim.cmd.highlight({ "DiagnosticVirtualTextWarn", "guifg=Orange" })
+        vim.cmd.highlight({ "Comment", "guifg=#e69340" })
+        vim.cmd.highlight({ "TreesitterContext", "guibg=#f0f0f0" })
     end
 end
 
@@ -369,7 +390,7 @@ vim.api.nvim_create_autocmd("Signal", {
     pattern = "SIGUSR1",
     callback = function()
         vim.g.is_dark_mode = vim.g.get_is_dark_mode()
-        load_theme()
+        -- load_theme()
     end,
 })
 vim.cmd('set completeopt+=noselect')
@@ -576,7 +597,7 @@ vim.api.nvim_create_autocmd('User', {
 })
 
 
-load_theme()
+-- load_theme()
 
 -- finally emit the config loaded event for lazy initialization
 vim.schedule(function()
