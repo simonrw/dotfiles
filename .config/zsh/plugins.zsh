9a265fd61567c95467c9467ecb276c27d51872b7
Simon Walker <s.r.walker101@googlemail.com>
2025-12-28 10:02:53 +0000

    zsh: correct autocomplete
    Former-commit-id: 0584da948dce868742274a0eae9b325a5a0fedb3

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -3,4 +3,5 @@ test -f /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh 
 test -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # zsh autosuggestions
+test -f /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 test -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh && source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
