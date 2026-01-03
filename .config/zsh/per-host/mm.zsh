237ab8f160a0fff71a225fb13271404a2549f340
Simon Walker <s.r.walker101@googlemail.com>
2026-01-03 21:46:35 +0000

    Revert "zsh: per-shell cargo target dir"
    This reverts commit 24c78fc8d968689b7c44ccea40818c0a20ffb9d2 [formerly 9e15c48b7207a0e021350103041733dbc4103df5].
    
    I thought an issue with compiling dap-gui was related to out of date
    build artifacts, but it's not the case. Thie also undid a performance
    increase
    
    
    Former-commit-id: 460ee75c13e032ca11b82ee9cbd3e5979e0f71df

--- .config/zsh/per-host/mm.zsh
+++ .config/zsh/per-host/mm.zsh
@@ -1,6 +1,6 @@
 # use my external HDD for cargo builds and go package downloads
 if test -d /Volumes/External; then
-    export CARGO_TARGET_DIR=/Volumes/External/cargo-target/$(cargo_target_dir)
+    export CARGO_TARGET_DIR=/Volumes/External/cargo-target
     export CARGO_HOME=/Volumes/External/cargo-home
     export RUSTUP_HOME=/Volumes/External/rustup-home
     export GOPATH=/Volumes/External/gocode
