7a73ca0317a6ff5a5587821a4a9ce258a573b7db
Simon Walker <s.r.walker101@googlemail.com>
2026-03-06 21:59:20 +0000

    emacs: speed up tramp
    Former-commit-id: dd48cd6a511681d3761632e824879a790a1a739e

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -215,3 +215,23 @@
 
 (use-package rg
   :ensure t)
+
+;; speed up tramp
+;; https://coredumped.dev/2025/06/18/making-tramp-go-brrrr./
+(setq remote-file-name-inhibit-locks t
+      tramp-use-scp-direct-remote-copying t
+      remote-file-name-inhibit-auto-save-visited t)
+
+(connection-local-set-profile-variables
+ 'remote-direct-async-process
+ '((tramp-direct-async-process . t)))
+
+(connection-local-set-profiles
+ '(:application tramp :protocol "scp")
+ 'remote-direct-async-process)
+
+(setq magit-tramp-pipe-stty-settings 'pty)
+
+(with-eval-after-load 'tramp
+  (with-eval-after-load 'compile
+    (remove-hook 'compilation-mode-hook #'tramp-compile-disable-ssh-controlmaster-options)))
