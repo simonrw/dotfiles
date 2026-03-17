966043235bd4830ba954e5924a68dad0b16cc66a
Simon Walker <s.r.walker101@googlemail.com>
2026-03-17 09:21:21 +0000

    fish: don't submit openspec telemetry
    Former-commit-id: 3a1f51bb3ed801c8126a8acc46d9ce53519beb17

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -24,6 +24,7 @@ set -gx XDG_CACHE_HOME $HOME/.cache
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_STATE_HOME $HOME/.local/state
+set -gx OPENSPEC_TELEMETRY 0
 
 if test "$__IS_DARK_THEME" = 1
     set -gx BAT_CONFIG_PATH $HOME/.config/bat/dark/config
