a7f2459602bd5881b9c4263c052b0fd2ec97e0ae
Simon Walker <s.r.walker101@googlemail.com>
2026-04-02 17:18:54 +0100

    emacs: use straight
    Former-commit-id: 295372aa2ce1fc4d2528341e9a455814280259ff

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,5 +1,22 @@
 ;; -*- lexical-binding: t; -*-
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-use-package-by-default t)
 
 (setq package-archives
       '(("melpa" . "https://melpa.org/packages/")
