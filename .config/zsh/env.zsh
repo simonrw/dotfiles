fe19a1349135338aa634548656edc0b3e3e1e9b9
Simon Walker <s.r.walker101@googlemail.com>
2026-04-16 17:51:06 +0100

    editor: use code -w for EDITOR
    Former-commit-id: fa766646ea62d4075a158ec23a1245401dd2bb85

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -63,7 +63,7 @@ export CARGO_TARGET_DIR=${HOME}/.cargo-target
 
 # centralise where python puts its .pyc files
 export PYTHONPYCACHEPREFIX=${HOME}/.python-cache
-export EDITOR=nvim
+export EDITOR="code -w"
 
 function isatty() {
     [ -t 0 ] && [ -t 1 ]
