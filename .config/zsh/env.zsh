3d3db676433b16c2e3602b28cbf051b021c9a811
Simon Walker <s.r.walker101@googlemail.com>
2026-04-17 11:30:15 +0100

    fish: revert using code as EDITOR

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -63,7 +63,7 @@ export CARGO_TARGET_DIR=${HOME}/.cargo-target
 
 # centralise where python puts its .pyc files
 export PYTHONPYCACHEPREFIX=${HOME}/.python-cache
-export EDITOR="code -w"
+export EDITOR=nvim
 
 function isatty() {
     [ -t 0 ] && [ -t 1 ]
