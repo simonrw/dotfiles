5c92e06727ed45ee69203e43ba6bed7714c6d661
Simon Walker <s.r.walker101@googlemail.com>
2026-05-01 15:27:01 +0100

    Revert "zsh: defer plugin startup work"
    This reverts commit ad7eac915f8c46f69354d9a8291f61a29e8e6490.

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,28 +1,10 @@
 # repo-managed zsh plugins
-test -f ~/.config/zsh/defer.zsh && source ~/.config/zsh/defer.zsh
 test -f ~/.config/zsh/lightweight-abbr.zsh && source ~/.config/zsh/lightweight-abbr.zsh
 
-_source_first_deferred() {
-    local file
-    for file in "$@"; do
-        if [[ -r "$file" ]]; then
-            zsh-defer source "$file"
-            return
-        fi
-    done
-}
+# zsh syntax highlighting
+test -f /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+test -f /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh && source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # zsh autosuggestions
-# Use an explicit grey rather than the plugin default (fg=8), because some terminal
-# themes map ANSI bright black to the normal foreground colour.
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=244'
-_source_first_deferred \
-    /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh \
-    /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# zsh syntax highlighting must be loaded after other plugins that add ZLE widgets.
-_source_first_deferred \
-    /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh \
-    /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
-unfunction _source_first_deferred
+test -f /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh && source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+test -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh && source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
