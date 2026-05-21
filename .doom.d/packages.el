7bbca31ff61b853a00b407b5ba2b6cb284c27004
Simon Walker <s.r.walker101@googlemail.com>
2026-05-21 07:42:47 +0100

    Revert "emacs: switch from vanilla emacs to doom emacs with evil mode"
    This reverts commit 11ca8ff16e82a7ba5912a91a0585b51c7b5cf7fe.

--- .doom.d/packages.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-
-(package! catppuccin-theme)
-(package! auto-dark)
-(package! mise)
-(package! flymake-ruff)
-(package! agent-shell)
-(package! consult-eglot)
