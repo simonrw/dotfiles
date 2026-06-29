548f04b905eb2548b73b2ebbdecb54b8add6ee29
Simon Walker <s.r.walker101@googlemail.com>
2026-06-29 20:34:26 +0100

    hs: configure default browser

---
+++ .hammerspoon/config/browser.lua
@@ -0,0 +1,17 @@
+local browser_name = "Helium"
+
+-- set default browser
+local function handleBrowse(scheme, host, params, fullURL, senderPID)
+    hs.application.launchOrFocus(browser_name)
+    local app = hs.application.get(browser_name)
+    if app == nil then
+        hs.application.launchOrFocus(browser_name)
+        app = hs.application.get(browser_name)
+    end
+    local bundle_id = app:bundleID()
+
+    hs.urlevent.openURLWithBundle(fullURL, bundle_id)
+end
+
+hs.application.enableSpotlightForNameSearches(true)
+hs.urlevent.httpCallback = handleBrowse
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,2 +1,11 @@
+-- Disable all window animations when changing
+hs.window.animationDuration = 0
+
+for file in io.popen('find ~/.hammerspoon/config -name "*.lua" -exec basename {} \\; | sed "s/\\.lua$//g"'):lines() do
+    require('config/' .. file)
+end
+
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
+
+hs.alert.show('Config reloaded')
