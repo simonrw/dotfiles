f20645027d42964a990ab1b06d9dad8a726612ed
Simon Walker <s.r.walker101@googlemail.com>
2026-06-29 20:17:29 +0100

    hamemrspoon: use reloadconfiguration spoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,14 +1,2 @@
-local function reloadConfig(files)
-    local doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
