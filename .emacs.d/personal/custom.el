192f76b60de6efee7c318d6f6fe494d0b000a90e
Simon Walker <s.r.walker101@googlemail.com>
2026-07-16 15:25:07 +0100

    emacs: customizations

--- .emacs.d/personal/custom.el
+++ .emacs.d/personal/custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("b0cedf3c6d8fbbf65934e2045dddacff0a031992f2f389215adcb0ca741347c3" default))
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(evil-leader vterm auto-dark batppuccin yaml-mode web-mode elisp-slime-nav rainbow-delimiters rainbow-mode cmake-mode company consult marginalia orderless vertico exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring ag ace-window)))
+   '(evil-collection evil-leader vterm auto-dark batppuccin yaml-mode web-mode elisp-slime-nav rainbow-delimiters rainbow-mode cmake-mode company consult marginalia orderless vertico exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree super-save smartrep smartparens operate-on-number move-text magit projectile imenu-anywhere hl-todo guru-mode git-modes git-timemachine gist flycheck expand-region editorconfig easy-kill diminish diff-hl discover-my-major crux browse-kill-ring ag ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
