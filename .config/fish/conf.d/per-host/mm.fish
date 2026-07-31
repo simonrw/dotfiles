d61ad9f7c28734f375b55c0685dcac09ea17d648
Simon Walker <s.r.walker101@googlemail.com>
2026-07-31 10:40:09 +0100

    fish: don't use external for rust
    Performance is definitely worse with this drive, so just use it for large file
    storage rather than Go and Rust compile dirs

--- .config/fish/conf.d/per-host/mm.fish
+++ .config/fish/conf.d/per-host/mm.fish
@@ -2,11 +2,6 @@
 set -l __LOCATION /Volumes/External
 
 if test -d $__LOCATION; and test -r $__LOCATION; and test -w $__LOCATION
-    set -gx CARGO_TARGET_DIR $__LOCATION/cargo-target
-    set -gx CARGO_HOME $__LOCATION/cargo-home
-    set -gx RUSTUP_HOME $__LOCATION/rustup-home
-    set -gx GOPATH $__LOCATION/gocode
-    fish_add_path $CARGO_HOME/bin $GOPATH/bin
     set -gx OLLAMA_MODELS $__LOCATION/ollama
     set -gx COLIMA_HOME $__LOCATION/colima
 end
