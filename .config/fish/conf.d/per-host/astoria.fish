aebc3dbcb9a0e9903fe5758a83e8093880af216d
Simon Walker <s.r.walker101@googlemail.com>
2026-09-01 16:40:08 +0100

    fish: use normal cargo target dir
    This should speed up performance

--- .config/fish/conf.d/per-host/astoria.fish
+++ .config/fish/conf.d/per-host/astoria.fish
@@ -2,10 +2,5 @@
 set -l __LOCATION /mnt/data
 
 if test -d $__LOCATION; and test -r $__LOCATION; and test -w $__LOCATION
-    set -gx CARGO_TARGET_DIR $__LOCATION/cargo-target
-    set -gx CARGO_HOME $__LOCATION/cargo-home
-    set -gx RUSTUP_HOME $__LOCATION/rustup-home
-    set -gx GOPATH $__LOCATION/gocode
-    fish_add_path $CARGO_HOME/bin $GOPATH/bin
     set -gx OLLAMA_MODELS $__LOCATION/ollama
 end
